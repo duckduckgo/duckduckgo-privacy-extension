aecabb0f87a64368770558033bef6d9f634c278e
mr.Shu <mr@shu.io>
2013-08-22 10:46:56 +0200

    fixed uninstall

--- js/uninstall.js
+++ js/uninstall.js
@@ -15,8 +15,15 @@
  */
 
 
-document.getElementsById("uninstall").click = function() {
-    chrome.extension.sendMessage({options: "get"});
-}
+window.onload = function() {
+    document.getElementById("uninstall").href = 'javascript:void(0);';
+    document.getElementById("uninstall").onclick = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
 
+        chrome.extension.sendMessage({uninstall: true});
+
+        return false;
+    }
+}
 
