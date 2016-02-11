022ac7f9e33788b11dc38e04adfc422fabf7c07f
Sayan Chakraborty <mail.sayanc@gmail.com>
2016-02-11 23:32:17 +0530

    Removing console.log and refactoring backgroundjs

--- js/background.js
+++ js/background.js
@@ -15,84 +15,86 @@
  */
 
 
-function Background()
-{
-    $this = this;
+function Background() {
+  $this = this;
 
-    // clearing last search on borwser startup
-    localStorage['last_search'] = '';
+  // clearing last search on borwser startup
+  localStorage['last_search'] = '';
 
-    chrome.extension.onMessage.addListener(function(request, sender, callback){
-        console.log(request);
-        if(request.query)
-            return $this.query(request.query, callback);
-        if (request.options) {
-            callback(localStorage);
-        }
-
-        if (request.selection) {
-        
-        }
-
-        if (request.current_url) {
-            chrome.tabs.getSelected(function(tab) {
-                console.log(tab);
-                var url = tab.url;
-                callback(url);
-            });
-        }
-
-        return true;
-    });
-
-//  this.menuID = chrome.contextMenus.create({
-//       "title" : "Ask the duck",
-//       "type" : "normal",
-//       "contexts" : ["selection"],
-//       "onclick" : function() {
-//          console.log('clicked!!!'); 
-//       }
-//  });
-}
-
-Background.prototype.query = function(query, callback) 
-{
-    console.log('got a query', query);
-    var req = new XMLHttpRequest();
-    console.log(localStorage['zeroclickinfo']);
-    if (localStorage['zeroclickinfo'] === 'false') {
-        callback(null);
-        return;
-    } else {
-        req.open('GET', 'https://chrome.duckduckgo.com?q=' + encodeURIComponent(query) + '&format=json&d=1', true);
+  chrome.extension.onMessage.addListener(function(request, sender, callback) {
+    if (request.query)
+      return $this.query(request.query, callback);
+    if (request.options) {
+      callback(localStorage);
     }
 
-    req.onreadystatechange = function(data) {
-        if (req.readyState != 4)  { return; } 
-        var res = JSON.parse(req.responseText);
-        console.log('res:', res);
-        callback(res);
+    if (request.current_url) {
+      chrome.tabs.getSelected(function(tab) {
+        console.log(tab);
+        var url = tab.url;
+        callback(url);
+      });
     }
 
-    req.send(null);
     return true;
+  });
+
+  //  this.menuID = chrome.contextMenus.create({
+  //       "title" : "Ask the duck",
+  //       "type" : "normal",
+  //       "contexts" : ["selection"],
+  //       "onclick" : function() {
+  //          console.log('clicked!!!');
+  //       }
+  //  });
 }
+
+Background.prototype.query = function(query, callback) {
+  console.log('got a query', query);
+  var req = new XMLHttpRequest();
+  if (localStorage['zeroclickinfo'] === 'false') {
+    callback(null);
+    return;
+  } else {
+    req.open('GET', 'https://chrome.duckduckgo.com?q=' + encodeURIComponent(query) +
+      '&format=json&d=1', true);
+  }
+
+  req.onreadystatechange = function(data) {
+    if (req.readyState != 4) {
+      return;
+    }
+    var res = JSON.parse(req.responseText);
+    console.log('res:', res);
+    callback(res);
+  };
+
+  req.send(null);
+  return true;
+};
 
 var background = new Background();
 
-chrome.omnibox.onInputEntered.addListener( function(text) {
-    chrome.tabs.query({'currentWindow': true, 'active': true}, function(tabs) {
-        chrome.tabs.update(tabs[0].id, {url: "https://duckduckgo.com/?q="+encodeURIComponent(text)});
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  chrome.tabs.query({
+    'currentWindow': true,
+    'active': true
+  }, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {
+      url: "https://duckduckgo.com/?q=" + encodeURIComponent(text)
     });
+  });
 });
 
 //This adds Context Menu when user select some text.
 //create context menu
 chrome.contextMenus.create({
-    title : 'Search DuckDuckGo for "%s"',
-    contexts : ["selection"],
-    onclick : function(info){
-        var queryText = info.selectionText;
-        chrome.tabs.create({url:"https://duckduckgo.com/?q="+queryText});
-    }
+  title: 'Search DuckDuckGo for "%s"',
+  contexts: ["selection"],
+  onclick: function(info) {
+    var queryText = info.selectionText;
+    chrome.tabs.create({
+      url: "https://duckduckgo.com/?q=" + queryText
+    });
+  }
 });
