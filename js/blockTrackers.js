1a87fbc5bfc15e219783fa8b1a7271baba137aef
Maria Grazia Alastra <maria@duckduckgo.com>
2017-03-29 19:57:06 -0400

    check if host is whitelisted

--- js/blockTrackers.js
+++ js/blockTrackers.js
@@ -27,17 +27,19 @@ require.scopes.blockTrackers = (function() {
             if (formerSocialBlocking !== bg.isSocialBlockingEnabled) {
                 formerSocialBlocking = bg.isSocialBlockingEnabled;
                 trackers = load.processMozillaBlockList(blockList);
-                
-                chrome.tabs.query({
-                  'currentWindow': true,
-                  'active': true
-                }, function(currentTabs) {
+            }
+            
+            chrome.tabs.query({
+              'currentWindow': true,
+              'active': true
+            }, function(currentTabs) {
+                if (currentTabs[0]) {
                    var tabId = currentTabs[0].id;
                    if ((tabs[tabId] && tabs[tabId].whitelist) && (tabs[tabId].whitelist.indexOf(host) !== -1)) {
                        isWhiteListed = true;
                    }
-                });
-            }
+                }
+            });
 
 
             if (trackers[host] && (!isWhiteListed)) {
