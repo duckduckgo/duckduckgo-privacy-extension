b9006769ecef754aa3e2c41aa7b6cefc313bbdc5
jdorweiler <jason@duckduckgo.com>
2017-04-26 12:49:25 -0600

    more tests

--- test/tests/stats.js
+++ test/tests/stats.js
@@ -1,5 +1,5 @@
 (function() {
-  QUnit.module("utils");
+  QUnit.module("stats");
 
   var utils = require('stats');
   var trackers = [
@@ -7,20 +7,19 @@
   ];
 
   QUnit.test("add tracker to stats", function (assert) {
-      var testData = { "parent": "Google", 'currURL': "reddit.com", "tracker": "doubleclick.net"};
+      var testData = { "parent": "Google", 'currURL': "reddit.com", "tracker" : {"count": 1, "url": "doubleclick.net"}};
       // add a bunch of data to stats
-      for(var i = 0; i < 10; i++){
+      var entriesToAdd = 10;
+      for(var i = 0; i < entriesToAdd; i++){
           stats.update(testData.parent, testData.currURL, testData.tracker);
       }
       var topBlocked = stats.getTopBlocked();
       assert.ok(topBlocked.length !== 0, 'topBlocked has data');
 
-      // add another tracker
       stats.update("SomeOtherCompany", testData.currURL, testData.tracker);
-
       topBlocked = stats.getTopBlocked();
-
       assert.ok(topBlocked[0].name === testData.parent, "has correct top blocked company");
+      assert.ok(topBlocked[0].count === entriesToAdd, "has correct count for top blocked company");
   });
 
 })();
