40c3d47f86fba00cebb5380c405a02c4d751804c
Russell Holt <russell@duckduckgo.com>
2017-05-02 13:31:58 -0400

    removed separate function

--- js/templates/trackerlist.es6.js
+++ js/templates/trackerlist.es6.js
@@ -2,16 +2,11 @@ const bel = require('./../../node_modules/bel');
 
 module.exports = function () {
 
-    var f = function(list) {
-        return list.map((site) => bel`<li> ${site.domain} = ${site.blocked} </li>`);
-    };
-
     return bel`<div class="js-trackerlist js-menu-section">
         <div class="js-menu-title">${this.model.heading}</div>
         <ul class="js-menu-item-list">
-            ${f(this.model.testList)}
+            ${this.model.testList.map((site) => bel`<li> ${site.domain}: ${site.blocked} </li>`)}
         </ul>
     </div>`
-    
 }
 
