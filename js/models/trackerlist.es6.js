e0bf2db9d8dacbd5f0bf3a1e2a2171e044b9b4b9
Russell Holt <russell@duckduckgo.com>
2017-05-03 00:40:23 -0400

    cleanup

--- js/models/trackerlist.es6.js
+++ js/models/trackerlist.es6.js
@@ -4,26 +4,8 @@ var backgroundPage = chrome.extension.getBackgroundPage();
 
 function TrackerList (attrs) {
 
-
     Parent.call(this, attrs);
     this.companyList = backgroundPage.Companies.getTopBlocked(5);
-
-    // test data for now
-    // this might reference chrome.extension.getBackgroundPage();
-    // to get the parent company data
-    // this.testList = [
-
-    //     {  domain: "google.com", blocked: 100 },
-    //     {  domain: "facebook.com", blocked: 20 },
-    //     {  domain: "twitter.com", blocked: 10 },
-    //     {  domain: "amazon.com", blocked: 5 },
-    //     {  domain: "adnexus.com", blocked: 4 }
-
-    // ];
-
-    // this.extensionIsEnabled = this.bg.settings.getSetting("extensionIsEnabled");
-    // console.log("extension is enabled: ",  this.bg.settings.getSetting("extensionIsEnabled"));
-
 
 };
 
