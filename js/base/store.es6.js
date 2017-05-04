864f343014cfe3c64d9ddf490cbf187d4ac77121
Lauren Garcia <lauren.garcia@gmail.com>
2017-05-04 21:11:52 +0300

    stub out dummy model properties object

--- js/base/store.es6.js
+++ js/base/store.es6.js
@@ -7,8 +7,8 @@ const deepFreeze = require('deep-freeze');
 // all this "reducer" stuff from developer
 // and swap this out for model._emit()
 function exampleReducer (state, action) {
-    if (action.type === 'example') {
-        return { example: true };
+    if (action.type === 'SET_EXAMPLEREDUCER') {
+        return action.properties || {};
     }
 }
 
@@ -19,6 +19,9 @@ function exampleReducer (state, action) {
 // after init phase!
 const store = minidux.createStore (exampleReducer, { example: false })
 
+console.log('default state:')
+console.log(store.getState());
+
 // store.subscribe will need to be broadcast out to each model
 // each model will need to subscribe
 // remember: minidux only allows one store subscriber(!)
@@ -27,11 +30,15 @@ store.subscribe((state) => {
   console.log(state)
   // make state immutable before broadcasting out to models!
   state = deepFreeze(state);
-})
+});
 
 // this will happen when models call model.set()
 window.setTimeout(() => {
-  store.dispatch({ type: 'example' });
+  store.dispatch({ type: 'SET_EXAMPLEREDUCER', properties: {
+    foo: 'foo',
+    bar: { baz: 'baz' }
+  }
+  });
   console.log('state after dispatch call:')
   console.log(store.getState());
 }, 1000)
