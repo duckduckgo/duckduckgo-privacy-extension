6b3b1d4ea0f72f979084ba555bc8d73cc821038f
Russell Holt <russell@duckduckgo.com>
2017-05-04 10:45:57 -0400

    class rename

--- js/templates/trackerlist.es6.js
+++ js/templates/trackerlist.es6.js
@@ -5,7 +5,7 @@ module.exports = function () {
     return bel`<div class="js-trackerlist">
         <div class="js-menu-title">${this.model.heading}</div>
         <ul class="js-menu-item-list">
-            ${this.model.companyListM.map((site) => bel`<li class="js-site-co"> <div class="js-site-blocker-bar" style="width:${site.p}px"></div>
+            ${this.model.companyListM.map((site) => bel`<li class="site-co"> <div class="js-site-blocker-bar" style="width:${site.p}px"></div>
                                           ${site.name} <div class="js-site-blocker-count">${site.count}</div></li>`)}
         </ul>
     </div>`
