9e294721eac6dab19f85809a5e320b7a499092cd
jdorweiler <jason@duckduckgo.com>
2017-05-08 14:38:48 -0600

    start atb module

---
+++ js/atb.js
@@ -0,0 +1,15 @@
+var atb = (() => {
+    var majorVersion;
+    var minorVersion;
+    var setAtb;
+
+    return {
+        updateSetAtb: () => {
+
+        },
+
+        getRewrittenURL: () => {
+
+        }
+    }
+}
