2996870448b15cb362ccbaef59728f10cf707765
Lauren Garcia <lauren.garcia@gmail.com>
2017-05-16 18:01:11 +0300

    fix site domain display so it doesn't break on large domain names

--- js/templates/site.es6.js
+++ js/templates/site.es6.js
@@ -7,7 +7,7 @@ module.exports = function () {
     return bel`<section class="site-info divider-bottom">
         <ul class="menu-list">
             <li>
-                <span class="js-site-domain">${domain}</span>
+                <h1 class="site-info__domain">${domain}</h1>
                 <div class="js-site-toggle js-toggle-bg js-toggle-bg-${!this.model.isWhitelisted}">
                     <div class="js-site-toggle js-toggle-fg js-toggle-fg-${!this.model.isWhitelisted}"></div>
                 </div>
