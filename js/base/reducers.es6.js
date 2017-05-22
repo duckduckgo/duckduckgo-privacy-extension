eadab0f00673a3f06ab7cc9f7851abc3f1340409
Lauren Garcia <lauren.garcia@gmail.com>
2017-05-22 13:16:12 +0300

    remove minidux dependency for combineReduers

--- js/base/reducers.es6.js
+++ js/base/reducers.es6.js
@@ -1,4 +1,4 @@
-const minidux = require('minidux');
+// const minidux = require('minidux');
 const asyncReducers = {}
 
 function addReducer (reducerName, reducerFn) {
@@ -33,7 +33,23 @@ function getActionType (reducerName) {
 }
 
 function combine () {
-    return minidux.combineReducers(asyncReducers);
+    // return minidux.combineReducers(asyncReducers);
+    var keys = Object.keys(asyncReducers)
+
+    return function combination (state, action) {
+      var hasChanged = false
+      var nextState = {}
+
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i]
+        if (typeof asyncReducers[key] !== 'function') throw new Error('reducer ' + key + 'must be a function')
+        nextState[key] = asyncReducers[key](state[key], action)
+        hasChanged = hasChanged || nextState[key] !== state[key]
+      }
+
+      return hasChanged ? nextState : state
+    }
+
 }
 
 function removeReducer (reducerName) {
