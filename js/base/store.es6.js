372bd5100445d2da06ca83ffae8d0ce9d9dd8e9d
Lauren Garcia <lauren.garcia@gmail.com>
2017-05-22 13:52:41 +0300

    remove minidux dependency for createStore

--- js/base/store.es6.js
+++ js/base/store.es6.js
@@ -1,7 +1,7 @@
 // TODO: README at js/base directory level, point to it from main README
 // TODO: create a state injector for test mocks
 
-const minidux = require('minidux');
+// const minidux = require('minidux');
 const deepFreeze = require('deep-freeze');
 const reducers = require('./reducers.es6.js');
 const EventEmitter2 = require('eventemitter2');
@@ -30,7 +30,7 @@ function register (modelName) {
     const combinedReducers = reducers.combine();
 
     if (!_store) {
-        _store = minidux.createStore(combinedReducers, {});
+        _store = _createStore(combinedReducers);
         _store.subscribe((state) => {
             state = deepFreeze(state); // make state immutable before publishing
             _publishChange(state); // publish changes to subscribers
@@ -104,3 +104,66 @@ module.exports = {
   subscribe: _publisher,
   remove: remove
 };
+
+
+
+
+
+const isPlainObject = require('is-plain-object');
+
+function _createStore (reducer) {
+  if (!reducer || typeof reducer !== 'function') throw new Error('reducer must be a function')
+
+  // if (typeof initialState === 'function' && typeof enhancer === 'undefined') {
+  //   enhancer = initialState
+  //   initialState = undefined
+  // }
+
+  // if (typeof enhancer !== 'undefined') {
+  //   if (typeof enhancer !== 'function') {
+  //     throw new Error('enhancer must be a function.')
+  //   }
+
+  //   return enhancer(createStore)(reducer, initialState)
+  // }
+
+  var initialState = initialState || {}
+  var state = initialState
+  var listener = null
+  var isEmitting = false
+
+  function dispatch (action) {
+    if (!action || !isPlainObject(action)) throw new Error('action parameter is required and must be a plain object')
+    if (!action.type || typeof action.type !== 'string') throw new Error('type property of action is required and must be a string')
+    if (isEmitting) throw new Error('modifiers may not emit actions')
+
+    isEmitting = true
+    state = reducer(state, action)
+    if (listener) listener(state)
+    isEmitting = false
+    return action
+  }
+
+  function subscribe (cb) {
+    if (!cb || typeof cb !== 'function') throw new Error('listener must be a function')
+    listener = cb
+  }
+
+  function replaceReducer (next) {
+    if (typeof next !== 'function') throw new Error('new reducer must be a function')
+    reducer = next
+  }
+
+  // function getState () {
+  //   return state
+  // }
+
+  dispatch({ type: '@@createStore/INIT' })
+
+  return {
+    dispatch: dispatch,
+    subscribe: subscribe,
+    // getState: getState,
+    replaceReducer: replaceReducer
+  }
+}
