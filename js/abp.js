851770dbc541e7208c358c68a93acb8af7bab490
jdorweiler <jason@duckduckgo.com>
2017-06-07 07:56:01 -0600

    add to grunt

--- js/abp.js
+++ js/abp.js
@@ -1,4 +1,12 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+abp = require('abp-filter-parser');
+var easyList = load.loadExtensionFile('data/tracker_lists/easyprivacy.txt');
+parsedEasyList = {};
+abp.parse(easyList, parsedEasyList);
+
+},{"abp-filter-parser":2}],2:[function(require,module,exports){
 (function (global, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['exports', 'bloom-filter-js', './badFingerprints.js'], factory);
@@ -650,7 +658,7 @@
 
 // console.log('checking again for str, no result');
 
-},{"./badFingerprints.js":2,"bloom-filter-js":3}],2:[function(require,module,exports){
+},{"./badFingerprints.js":3,"bloom-filter-js":4}],3:[function(require,module,exports){
 (function (global, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['exports'], factory);
@@ -675,7 +683,7 @@
   exports.badSubstrings = badSubstrings;
 });
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 (function (global, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['exports'], factory);
@@ -922,10 +930,4 @@
   exports.BloomFilter = BloomFilter;
 });
 
-},{}],4:[function(require,module,exports){
-abp = require('abp-filter-parser');
-var easyList = load.loadExtensionFile('data/tracker_lists/easyprivacy.txt');
-parsedEasyList = {};
-abp.parse(easyList, parsedEasyList);
-
-},{"abp-filter-parser":1}]},{},[4]);
+},{}]},{},[1]);
