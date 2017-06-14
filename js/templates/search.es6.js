3ad0396ffb50721f69408a1c5c51f3db476aa700
Lauren Garcia <lauren.garcia@gmail.com>
2017-06-14 03:19:43 +0200

    no inline styles

--- js/templates/search.es6.js
+++ js/templates/search.es6.js
@@ -5,7 +5,7 @@ module.exports = function () {
         <form class="search-form js-search-form card" name="x">
           <input type="text" autocomplete="off" placeholder="Search DuckDuckGo" name="q" class="search-form__input js-search-input" value="${this.model.searchText}" />
           <input class="search-form__go js-search-go" tabindex="2" value="" type="button" />
-          <input type="submit" style="display: none;" />
+          <input type="submit" class="search-form__submit" />
           <span class="ddg-logo"></span>
         </form>
     </section>`;
