59ef287d7ec9d09fdd11447d90d0d747bf9e2f50
Lauren Garcia <lauren.garcia@gmail.com>
2017-06-19 21:01:01 +0100

    create reusables for sliding subviews

--- js/templates/trackerlist-tabbed.es6.js
+++ js/templates/trackerlist-tabbed.es6.js
@@ -2,8 +2,10 @@ const bel = require('bel');
 
 module.exports = function () {
 
-    return bel`<section class="sliding-subview">
-        Hello World!
+    return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
+        <nav class="sliding-subview__header card">
+          <a href="#">Trackers</a>
+        </nav>
     </section>`
 }
 
