bc65f3ccfa59150e939309761ff69473cf6fe9cb
Lauren Garcia <lauren.garcia@gmail.com>
2017-06-20 01:23:30 +0100

    style tabbed nav on subview

--- js/templates/trackerlist-tabbed.es6.js
+++ js/templates/trackerlist-tabbed.es6.js
@@ -4,7 +4,13 @@ module.exports = function () {
 
     return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
         <nav class="sliding-subview__header card">
-          <a href="#" class="js-sliding-subview-close">Trackers</a>
+          <a href="#" class="sliding-subview__header__title js-sliding-subview-close">
+            Trackers
+          </a>
+          <ol class="sliding-subview__header__tabbed-nav">
+            <a href="#">Page</a>
+            <a href="#" class="active">All Time</a>
+          </ol>
         </nav>
     </section>`
 }
