2095f8a70b01f8ab9fff2d5bfb617ff85b8dbf40
Lauren Garcia <lauren.garcia@gmail.com>
2017-06-21 01:20:26 +0100

    remove inline javascript that violates ext. CSPs

--- js/templates/linkable.es6.js
+++ js/templates/linkable.es6.js
@@ -1,7 +1,9 @@
 const bel = require('bel');
 
 module.exports = function () {
-    return bel`<a class="linkable ${this.model.klass}" id="js-linkable-${this.model.id}" href="javascript:void(0)">
+    return bel`<a class="linkable ${this.model.klass}"
+                  id="js-linkable-${this.model.id}"
+                  href="#">
             ${this.model.text}
             <span class="${this.model.spanClass}"></div>
         </a>`;
