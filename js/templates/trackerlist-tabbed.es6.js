9f98ac933b524bf6b0732d8ac9488e5fa24408c3
Lauren Garcia <lauren.garcia@gmail.com>
2017-07-03 19:28:41 +0100

    tabbed nav label:  ->

--- js/templates/trackerlist-tabbed.es6.js
+++ js/templates/trackerlist-tabbed.es6.js
@@ -11,7 +11,7 @@ module.exports = function () {
                     <span class="icon icon__arrow icon__arrow--left pull-left"></span>
                 </a>
                 <ul class="sliding-subview__header__tabbed-nav">
-                    <a href="#" class="js-nav-tab js-nav-tab-page">Page</a>
+                    <a href="#" class="js-nav-tab js-nav-tab-page">This Page</a>
                     <a href="#" class="js-nav-tab js-nav-tab-all">All Time</a>
                 </ul>
             </nav>
