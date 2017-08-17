17859f85cbe21357e3b90931a0f94a85fb02b941
Lauren Garcia <lauren.garcia@gmail.com>
2017-08-16 17:49:24 -0700

    httpse.testPipeRequestUrl() method for console cmdline testing

--- js/httpse.js
+++ js/httpse.js
@@ -21,27 +21,29 @@ class HTTPSE {
                 console.warn('HTTPSE: .pipeRequestUrl() this.db is not ready')
                 return resolve(reqUrl)
             }
-
+            reqUrl = reqUrl.toLowerCase()
             const protocol = URLParser.extractProtocol(reqUrl).protocol
             if (!protocol.indexOf('http') === 0) resolve(reqUrl)
 
-            const host = utils.extractHostFromURL(reqUrl.toLowerCase())
+            const host = utils.extractHostFromURL(reqUrl)
             this.db.get(this.dbObjectStore, host).then(
                 (record) => {
                     if (record && record.simpleUpgrade) {
-                        const upgrade = reqUrl.toLowerCase().replace(/^(http|https):\/\//, 'https://')
+                        // TODO: deal with wildcard records in db
+                        const upgrade = reqUrl.replace(/^(http|https):\/\//, 'https://')
                         return resolve(upgrade)
                     }
                     return resolve(reqUrl)
                 },
                 () => {
-                    console.warn('HTTPSE: pipeRequestUrl() encountered a db error.')
+                    console.warn('HTTPSE: pipeRequestUrl() encountered a db error')
                     return resolve(reqUrl)
                 }
             )
         })
     }
 
+    /* For debugging/development/test purposes only */
     getHostRecord (host) {
         return new Promise ((resolve, reject) => {
             if (!this.isReady) {
@@ -75,6 +77,7 @@ class HTTPSE {
             'yts.ag'
         ]
 
+        // Test that host records exist after db install from server
         testHosts.forEach((host) => {
             this.getHostRecord(host).then(
                 (record) => {
@@ -87,7 +90,31 @@ class HTTPSE {
                 },
                 () => console.error('HTTPSE: testDBKeys() encountered a db error for host: ' + host)
             )
-        })             
+        })        
+    }
+
+    testPipeRequestUrl () {
+        // These hosts should always have records that were xhr'd 
+        // into the client-side db from server
+        const testUrls = [
+            'http://1337x.to/foo',
+            'http://submit.pandora.com/foo/bar',
+            'http://foo.api.roblox.com/sit?stand=false',
+            'http://thump.vice.com',
+            'http://yts.ag'
+        ]
+
+        function _handleDone (r, i) {
+            // console.log(r)
+            if (i === (testUrls.length - 1)) console.timeEnd('testPipeRequestUrlTimer')
+        }
+
+        console.time('testPipeRequestUrlTimer')
+        testUrls.forEach((url, i) => {
+            this.pipeRequestUrl(url)
+                .then((r) => _handleDone(r, i),
+                      (r) => _handleDone(r, i))
+        })     
     }
 
     /* For debugging/development/test purposes only */
