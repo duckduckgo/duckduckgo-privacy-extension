c1f3d9eb51a44cf716bb85790f61b445e40fa703
Lauren Garcia <lauren.garcia@gmail.com>
2017-09-05 16:44:15 -0700

    more verbose error messages on test failures

--- test/tests/https.js
+++ test/tests/https.js
@@ -4,6 +4,7 @@
   QUnit.test('test https upgrade rules', function (assert) {
 
       var done = assert.async()
+      
       bkg.db.ready().then(() => {
           bkg.https.testGetHostRecord(function(e) { 
             assert.ok(e === undefined, 'https.testGetHostRecord() found all records')
@@ -11,14 +12,16 @@
             const testUrl = 'http://foo.api.roblox.com/sit?stand=false'
             bkg.https.pipeRequestUrl(testUrl, { site: {} } ).then(
                 (upgradedUrl) => {
-                    assert.ok(upgradedUrl === 'https://foo.api.roblox.com/sit?stand=false', 'https.pipeRequestUrl() upgraded request')
+                    assert.ok(upgradedUrl === 'https://foo.api.roblox.com/sit?stand=false', 'https.pipeRequestUrl() upgraded wildcarded subdomain request')
                     done()
                 },
-                () => assert.ok(0 === 1, 'https.pipeRequestUrl() upgraded request')
+                () => assert.ok(0 === 1, 'https.pipeRequestUrl() upgraded wildcarded subdomain request')
             )
 
           })
-      })
+      },
+      () => assert.ok(0 === 1, 'db encountered an error during init')
+      )
 
   })
 })()
