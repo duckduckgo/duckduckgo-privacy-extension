f738a3a5f12640927fb2017949b4d60333eb3a49
Lauren Garcia <lauren.garcia@gmail.com>
2017-09-05 16:32:29 -0700

    test https upgrades

--- test/tests/https.js
+++ test/tests/https.js
@@ -6,7 +6,17 @@
       var done = assert.async()
       bkg.db.ready().then(() => {
           bkg.https.testGetHostRecord(function(e) { 
-            assert.ok(e === undefined, 'https.testGetHostRecord() works')
+            assert.ok(e === undefined, 'https.testGetHostRecord() found all records')
+          
+            const testUrl = 'http://foo.api.roblox.com/sit?stand=false'
+            bkg.https.pipeRequestUrl(testUrl, { site: {} } ).then(
+                (upgradedUrl) => {
+                    assert.ok(upgradedUrl === 'https://foo.api.roblox.com/sit?stand=false', 'https.pipeRequestUrl() upgraded request')
+                    done()
+                },
+                () => assert.ok(0 === 1, 'https.pipeRequestUrl() upgraded request')
+            )
+
           })
       })
 
