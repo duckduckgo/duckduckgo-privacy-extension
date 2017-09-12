77deb26eaa0034fceeefbec47c4e29e7bed8db4b
Lauren Garcia <lauren.garcia@gmail.com>
2017-09-12 14:12:26 -0700

    set focus() on search box on popup render
    (cherry picked from commit e79def1b5c21dfffa5e1214be22a44812b5748ba)

--- js/ui/templates/search.es6.js
+++ js/ui/templates/search.es6.js
@@ -3,7 +3,9 @@ const bel = require('bel');
 module.exports = function () {
     return bel`<section>
         <form class="sliding-subview__header card search-form js-search-form" name="x">
-          <input type="text" autocomplete="off" placeholder="Search DuckDuckGo" name="q" class="search-form__input js-search-input" value="${this.model.searchText}" />
+          <input type="text" autocomplete="off" placeholder="Search DuckDuckGo" 
+                 name="q" class="search-form__input js-search-input" 
+                 value="${this.model.searchText}" />
           <input class="search-form__go js-search-go" tabindex="2" value="" type="button" />
           <input type="submit" class="search-form__submit" />
           <span class="ddg-logo"></span>
