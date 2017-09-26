ec6ffe7071d5b4dd9f32f562589c73bea598ddd5
Zaahir Moolla <moollaza@duckduckgo.com>
2017-09-26 16:01:43 -0400

    testing chromedriver + extension loading

---
+++ selenium-test/testRatings.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const webdriver = require('selenium-webdriver');
+const By = webdriver.By;
+const until = webdriver.until;
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+const EXT_PATH = '../build/chrome-zeroclick-latest.crx',
+    BASE_URL = 'chrome-extension://ifojdgcpkiaobdlcoalkcahfjfkhjnhm/html/';
+
+const chromeCapabilities = webdriver.Capabilities.chrome();
+chromeCapabilities.set('chromeOptions', {args: ['--headless']});
+
+const options =  new chrome.Options().addExtensions(EXT_PATH);
+
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .withCapabilities(chromeCapabilities)
+  .setChromeOptions(options)
+  .build();
+
+// Navigate to google.com, enter a search.
+driver.get('https://www.duckduckgo.com/');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.id('search_button_homepage')).click();
+driver.wait(until.titleIs('webdriver at DuckDuckGo'), 1000);
+
+// Take screenshot of results page. Save to disk.
+driver.takeScreenshot().then(base64png => {
+  fs.writeFileSync('screenshot.png', new Buffer(base64png, 'base64'));
+});
+
+driver.quit();
