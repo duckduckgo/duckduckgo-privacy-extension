9d37655ff9af4916d216f598960b50b4a26335a1
jdorweiler <jason@duckduckgo.com>
2017-09-29 11:18:50 -0600

    fix companylist importer

--- scripts/buildLists.js
+++ scripts/buildLists.js
@@ -9,12 +9,13 @@ glob.sync( './scripts/importers/*.js' ).forEach( function(file) {
 var trackerListData = require('./tracker_list_data.json');
 
 trackerListData.forEach( function(listData) {
-    var processedList = global[listData.type](listData);
-    if(processedList){
-        fs.writeFile(__dirname + "/tracker_lists/" + processedList.name, JSON.stringify(processedList.data, null, 4), function(err) {
-            if(err) {
-                    return console.log(err);
-             }
-         });
-    }
+    global[listData.type](listData).then( (processedList) => {
+        if(processedList){
+            fs.writeFile(__dirname + "/tracker_lists/" + processedList.name, JSON.stringify(processedList.data, null, 4), function(err) {
+                if(err) {
+                        return console.log(err);
+                }
+            });
+        }
+    });
 });
