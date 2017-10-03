f5bd4e3cb4b59ad4cfb77b93fd53989df010cc00
Lauren Garcia <lauren.garcia@gmail.com>
2017-10-02 20:36:49 -0700

    standard-4

--- js/ui/models/whitelist.es6.js
+++ js/ui/models/whitelist.es6.js
@@ -1,30 +1,29 @@
 const Parent = window.DDG.base.Model;
 
 function Whitelist (attrs) {
-    // placeholder until we get whitelist data from background
-    this.list = {}
+    attrs.list = {}
     Parent.call(this, attrs)
-};
+}
 
 
 Whitelist.prototype = $.extend({},
-  Parent.prototype,
-  {
+      Parent.prototype,
+      {
 
-        modelName: 'whitelist',
+            modelName: 'whitelist',
 
-        removeDomain(itemIndex) {
-            var domain = this.list[itemIndex];
-            console.log(`whitelist: remove ${domain}`);
+            removeDomain (itemIndex) {
+                let domain = this.list[itemIndex];
+                console.log(`whitelist: remove ${domain}`)
 
-            this.fetch({'whitelisted': {
-                list: 'whitelisted',
-                domain: domain,
-                value: false
+                this.fetch({'whitelisted': {
+                    list: 'whitelisted',
+                    domain: domain,
+                    value: false
+                }
+                })
             }
-            })
-        }
-  }
-);
+      }
+)
 
-module.exports = Whitelist;
+module.exports = Whitelist
