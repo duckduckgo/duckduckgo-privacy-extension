196cf0f224e1bd95d648371b68c7d1a0deda0f9f
Lauren Garcia <lauren.garcia@gmail.com>
2017-10-05 10:00:58 -0700

    move options link temporarily

--- js/ui/pages/trackers.es6.js
+++ js/ui/pages/trackers.es6.js
@@ -1,25 +1,25 @@
-const Parent = window.DDG.base.Page;
-const mixins = require('./mixins/index.es6.js');
-const TrackerListView = require('./../views/trackerlist-truncated.es6.js');
-const TrackerListModel = require('./../models/trackerlist-top-blocked.es6.js');
-const trackerListTemplate = require('./../templates/trackerlist-truncated.es6.js');
-const SiteView = require('./../views/site.es6.js');
-const SiteModel = require('./../models/site.es6.js');
-const siteTemplate = require('./../templates/site.es6.js');
-const SearchView = require('./../views/search.es6.js');
-const SearchModel = require('./../models/search.es6.js');
-const searchTemplate = require('./../templates/search.es6.js');
-const LinkableView = require('./../views/linkable.es6.js');
-const LinkableModel = require('./../models/linkable.es6.js');
-const linkableTemplate = require('./../templates/linkable.es6.js');
-const AutocompleteView = require('./../views/autocomplete.es6.js');
-const AutocompleteModel = require('./../models/autocomplete.es6.js');
-const autocompleteTemplate = require('./../templates/autocomplete.es6.js');
-const BackgroundMessageModel = require('./../models/backgroundMessage.es6.js');
+const Parent = window.DDG.base.Page
+const mixins = require('./mixins/index.es6.js')
+const TrackerListView = require('./../views/trackerlist-truncated.es6.js')
+const TrackerListModel = require('./../models/trackerlist-top-blocked.es6.js')
+const trackerListTemplate = require('./../templates/trackerlist-truncated.es6.js')
+const SiteView = require('./../views/site.es6.js')
+const SiteModel = require('./../models/site.es6.js')
+const siteTemplate = require('./../templates/site.es6.js')
+const SearchView = require('./../views/search.es6.js')
+const SearchModel = require('./../models/search.es6.js')
+const searchTemplate = require('./../templates/search.es6.js')
+const LinkableView = require('./../views/linkable.es6.js')
+const LinkableModel = require('./../models/linkable.es6.js')
+const linkableTemplate = require('./../templates/linkable.es6.js')
+const AutocompleteView = require('./../views/autocomplete.es6.js')
+const AutocompleteModel = require('./../models/autocomplete.es6.js')
+const autocompleteTemplate = require('./../templates/autocomplete.es6.js')
+const BackgroundMessageModel = require('./../models/backgroundMessage.es6.js')
 
 function Trackers (ops) {
-    this.$parent = $('#trackers-container');
-    Parent.call(this, ops);
+    this.$parent = $('#trackers-container')
+    Parent.call(this, ops)
 };
 
 Trackers.prototype = $.extend({},
@@ -51,37 +51,37 @@ Trackers.prototype = $.extend({},
 
             this.views.search = new SearchView({
                 pageView: this,
-                model: new SearchModel({searchText:''}), // TODO proper location of remembered query
+                model: new SearchModel({searchText: ''}), // TODO proper location of remembered query
                 appendTo: this.$parent,
                 template: searchTemplate
-            });
+            })
+
+            this.views.options = new LinkableView({
+                pageView: this,
+                model: new LinkableModel({
+                    text: '',
+                    id: 'options-link',
+                    link: this.openOptionsPage,
+                    klass: 'link-secondary',
+                    spanClass: 'icon icon__settings pull-right'
+                }),
+                appendTo: this.$parent.find('.search-form'),
+                template: linkableTemplate
+            })
 
             this.views.site = new SiteView({
                 pageView: this,
                 model: new SiteModel(),
                 appendTo: this.$parent,
                 template: siteTemplate
-            });
+            })
 
             this.views.trackerlist = new TrackerListView({
                 pageView: this,
                 model: new TrackerListModel({ numCompanies: 3 }),
                 appendTo: this.$parent,
                 template: trackerListTemplate,
-            });
-
-            this.views.options = new LinkableView({
-                pageView: this,
-                model: new LinkableModel({
-                    text: 'Settings',
-                    id: 'options-link',
-                    link: this.openOptionsPage,
-                    klass: 'link-secondary',
-                    spanClass: 'icon icon__settings pull-right'
-                }),
-                appendTo: this.$parent,
-                template: linkableTemplate
-            });
+            })
 
             // TODO: hook up model query to actual ddg ac endpoint.
             // For now this is just here to demonstrate how to
@@ -93,12 +93,12 @@ Trackers.prototype = $.extend({},
                 // appendTo: this.views.search.$el,
                 appendTo: null,
                 template: autocompleteTemplate
-            });
+            })
 
         }
     }
 );
 
 // kickoff!
-window.DDG = window.DDG || {};
-window.DDG.page = new Trackers();
+window.DDG = window.DDG || {}
+window.DDG.page = new Trackers()
