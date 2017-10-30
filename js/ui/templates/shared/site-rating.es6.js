da75234964e9c9a4554e4c45c83cda1161ea4cdd
Lauren Garcia <lauren.garcia@gmail.com>
2017-10-30 14:41:38 -0700

    unified design: null rating state

--- js/ui/templates/shared/site-rating.es6.js
+++ js/ui/templates/shared/site-rating.es6.js
@@ -1,9 +1,10 @@
 const bel = require('bel')
 
 module.exports = function (rating, isWhitelisted) {
+    const _rating = rating ? rating.toLowerCase() : 'null'
     const isActive = !isWhitelisted ? 'is-active' : ''
     return bel`<div class="site-info__rating
-        site-info__rating--${rating.toLowerCase()}
+        site-info__rating--${_rating}
         ${isActive}
         js-rating"></div>`
 }
