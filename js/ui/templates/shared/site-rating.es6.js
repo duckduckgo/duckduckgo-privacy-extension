2c0306f50865d497443fcdaa9a1d2cfea65016dc
Lauren Garcia <lauren.garcia@gmail.com>
2017-10-30 15:17:24 -0700

    unified design: site rating icon update in grade details

--- js/ui/templates/shared/site-rating.es6.js
+++ js/ui/templates/shared/site-rating.es6.js
@@ -2,7 +2,7 @@ const bel = require('bel')
 
 module.exports = function (rating, isWhitelisted) {
     const _rating = rating ? rating.toLowerCase() : 'null'
-    const isActive = !isWhitelisted ? 'is-active' : ''
+    const isActive = isWhitelisted ? '' : 'is-active'
     return bel`<div class="site-info__rating
         site-info__rating--${_rating}
         ${isActive}
