28d1e78fd7e6f180bc582fbed0686bb42cb5d9c4
Lauren Garcia <lauren.garcia@gmail.com>
2017-10-31 16:22:00 -0700

    site rating explanation: pass model isWhitelisted status into template

--- js/ui/templates/shared/site-rating-explainer.es6.js
+++ js/ui/templates/shared/site-rating-explainer.es6.js
@@ -3,13 +3,14 @@ const bel = require('bel')
 module.exports = function (ratings, isWhitelisted) {
     let msg = ``
     let _rating = isWhitelisted ? ratings.before : ratings.after
-    
+
     if (_rating) {
         msg = `This received a "${_rating.toUpperCase()}" Privacy Grade
           for the reasons below.`
     }
+
     return bel`<p class="site-info--details__explainer
         js-rating-explainer border--bottom">
             ${msg}
-    </p>`
+        </p>`
 }
