0b8a284fa683bd0cdf39646ab42328cb3f96e3ab
Lauren Garcia <lauren.garcia@gmail.com>
2017-11-08 13:07:47 -0800

    clean up name normalization, also single quotes are the devils work

--- scripts/importers/companyList.js
+++ scripts/importers/companyList.js
@@ -3,12 +3,13 @@ global.companyList = function(listData){
      * https://raw.githubusercontent.com/mozilla-services/shavar-prod-lists/master/disconnect-blacklist.json
      * "<tracker host>" : { "c": <company name>, "u": "company url" }
      */
-    var companyListLoc = 'https://raw.githubusercontent.com/mozilla-services/shavar-prod-lists/master/disconnect-blacklist.json';
+    var companyListLoc = 'https://raw.githubusercontent.com/mozilla-services/shavar-prod-lists/master/disconnect-blacklist.json'
     var remapDataLoc = 'https://raw.githubusercontent.com/mozilla-services/shavar-prod-lists/master/google_mapping.json'
-    var trackerList = { TopTrackerDomains:{}};
-    var trackerTypes = ['Advertising', 'Analytics', 'Disconnect', 'Social'];
-    var request = require('request');
-    var remapData, companyList;
+    var trackerList = { TopTrackerDomains:{} }
+    var trackerTypes = ['Advertising', 'Analytics', 'Disconnect', 'Social']
+    var request = require('request')
+    var remapData
+    var companyList
 
     // FIXME -- use settings data in the future.
     // (but settings data needs to be read in a different way than it currently can.)
@@ -31,19 +32,16 @@ global.companyList = function(listData){
                 trackerTypes.forEach((type) => {
                     companyList.categories[type].forEach((entry) => {
 
-                        for(var name in entry){
-                            let fixedName = name;
+                        for (var name in entry) {
+                            let normalizedName = name
+                            if (name === 'Amazon.com') normalizedName = 'Amazon'
 
-                            // Fix Amazon
-                            if (name === "Amazon.com")
-                                fixedName = "Amazon";
-
-                            // itisAtracker is not a real entry in the list
+                            // ItIsAtracker is not a real entry in the list
                             if (name !== 'ItIsATracker') {
-                                for( var domain in entry[name]){
+                                for (var domain in entry[name]){
                                     if (entry[name][domain].length) {
                                         entry[name][domain].forEach((trackerURL) => {
-                                            addToList(type, trackerURL, {'c': fixedName, 'u': domain});
+                                            addToList(type, trackerURL, {'c': normalizedName, 'u': domain});
                                         });
                                     }
                                 }
@@ -52,7 +50,7 @@ global.companyList = function(listData){
                     });
                 });
 
-                resolve({"name": 'trackersWithParentCompany', "data": trackerList})
+                resolve({'name': 'trackersWithParentCompany', 'data': trackerList})
             })
         })
     })
@@ -69,31 +67,31 @@ global.companyList = function(listData){
     }
 
     function applyRemapping(type, name, url) {
-        if(type === 'Disconnect'){
-            var socialRemap = remapSocial(type,name,url);
+        if (type === 'Disconnect'){
+            var socialRemap = remapSocial(type, name, url);
             if(socialRemap){
                 return socialRemap;
             }
 
-            var googleReMap = remapGoogle(type,name,url);
-            if(googleReMap){
-                return googleReMap;
+            var googleReMap = remapGoogle(type, name, url);
+            if (googleReMap) {
+                return googleReMap
             }
         }
         return type;
     }
 
     function remapSocial(type, name, url){
-        var newType;
-        if(name === 'Facebook' || name === 'Twitter'){
-            newType = "Social";
+        var newType
+        if (name === 'Facebook' || name === 'Twitter') {
+            newType = 'Social'
         }
         return newType;
     }
 
     function remapGoogle(type, name, url){
         var newType;
-        if(name === "Google"){
+        if(name === 'Google'){
             Object.keys(remapData).some( function(category) {
                 if(remapData[category][0]['Google']['http://www.google.com/'].indexOf(url) !== -1){
                     return newType = category;
