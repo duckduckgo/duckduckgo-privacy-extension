053603045eb02aa26b4c2fae90570442d29fac5a
Russell Holt <russell@duckduckgo.com>
2017-11-09 14:17:22 -0500

    update email addresses

--- js/ui/templates/hamburger-menu.es6.js
+++ js/ui/templates/hamburger-menu.es6.js
@@ -19,14 +19,14 @@ module.exports = function () {
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:firefox@duckduckgo.com?subject=Firefox%20Extension%20Feedback&body=Help%20us%20improve%20by%20sharing%20a%20little%20info%20about%20the%20issue%20you%27ve%20encountered%2E%0A%0ATell%20us%20which%20features%20or%20functionality%20your%20feedback%20refers%20to%2E%20What%20do%20you%20love%3F%20What%20isn%27t%20working%3F%20How%20could%20it%20be%20improved%3F"
+                    <a href="mailto:extension-feedback@duckduckgo.com?subject=Firefox%20Extension%20Feedback&body=Help%20us%20improve%20by%20sharing%20a%20little%20info%20about%20the%20issue%20you%27ve%20encountered%2E%0A%0ATell%20us%20which%20features%20or%20functionality%20your%20feedback%20refers%20to%2E%20What%20do%20you%20love%3F%20What%20isn%27t%20working%3F%20How%20could%20it%20be%20improved%3F"
                         class="menu-title">
                         Send feedback
                         <span>Got issues or suggestions? Let us know!</span>
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:firefox@duckduckgo.com?subject=Firefox%20Extension%20Broken%20Site%20Report&body=Help%20us%20improve%20by%20sharing%20a%20little%20info%20about%20the%20issue%20you%27ve%20encountered%2E%0A%0A1%2E%20Which%20website%20is%20broken%3F%20%28copy%20and%20paste%20the%20URL%29%0A%0A2%2E%20Describe%20the%20issue%2E%20%28What%27s%20breaking%20on%20the%20page%3F%20Attach%20a%20screenshot%20if%20possible%2E%29%0A"
+                    <a href="mailto:extension-brokensites@duckduckgo.com?subject=Firefox%20Extension%20Broken%20Site%20Report&body=Help%20us%20improve%20by%20sharing%20a%20little%20info%20about%20the%20issue%20you%27ve%20encountered%2E%0A%0A1%2E%20Which%20website%20is%20broken%3F%20%28copy%20and%20paste%20the%20URL%29%0A%0A2%2E%20Describe%20the%20issue%2E%20%28What%27s%20breaking%20on%20the%20page%3F%20Attach%20a%20screenshot%20if%20possible%2E%29%0A"
                         class="menu-title">
                         Report broken site
                         <span>If a site's not working, please tell us.</span>
