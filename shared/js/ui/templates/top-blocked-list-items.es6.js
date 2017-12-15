287815b1010437ccde6dfdb5ec93afdff45fddec
jdorweiler <jason@duckduckgo.com>
2017-12-15 15:05:17 -0700

    move templates over

---
+++ shared/js/ui/templates/top-blocked-list-items.es6.js
@@ -0,0 +1,19 @@
+const bel = require('bel')
+
+module.exports = function (companyListMap) {
+    return companyListMap.map((data) => {
+
+        return bel`<li class="top-blocked__li">
+          <div class="top-blocked__li__company-name">${data.name}</div>
+          <div class="top-blocked__li__blocker-bar">
+              <div class="top-blocked__li__blocker-bar__fg
+                  js-top-blocked-graph-bar-fg"
+                  style="width: 0px" data-width="${data.px}px">
+              </div>
+          </div>
+          <div class="top-blocked__li__blocker-pct js-top-blocked-pct">
+              ${data.percent}%
+          </div>
+        </li>`
+    })
+}
---
+++ shared/js/ui/templates/top-blocked-truncated-list-items.es6.js
@@ -0,0 +1,15 @@
+const bel = require('bel')
+
+module.exports = function (companyListMap) {
+    return companyListMap.map((data) => {
+        return bel`<li class="top-blocked__li top-blocked__li--truncated">
+            <div class="top-blocked__pill">
+                <div class="top-blocked__li-site__icon ${data.name.replace('.', '').toLowerCase()}"></div>
+                <div class="top-blocked__li__blocker-pct js-top-blocked-pct">
+                    ${data.percent}%
+                </div>
+            </div>
+      </li>`
+    })
+}
+
