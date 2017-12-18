d58335bec588c9db0bad06e7cd9d9f9ec502e776
MariagraziaAlastra <maria@duckduckgo.com>
2017-12-18 16:36:20 -0500

    fall back to generic icon if it's not a major tracker network

--- shared/js/ui/templates/top-blocked-truncated-list-items.es6.js
+++ shared/js/ui/templates/top-blocked-truncated-list-items.es6.js
@@ -1,10 +1,11 @@
 const bel = require('bel')
+const bkg = chrome.extension.getBackgroundPage()
 
 module.exports = function (companyListMap) {
     return companyListMap.map((data) => {
         return bel`<li class="top-blocked__li top-blocked__li--truncated">
             <div class="top-blocked__pill">
-                <div class="top-blocked__li-site__icon ${data.name.toLowerCase()}"></div>
+                <div class="top-blocked__li-site__icon ${bkg.utils.getScssTrackerName(data.name.toLowerCase())}"></div>
                 <div class="top-blocked__li__blocker-pct js-top-blocked-pct">
                     ${data.percent}%
                 </div>
