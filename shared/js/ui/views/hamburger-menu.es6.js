db05d7fcbf00638330d30415d1c95b057d8eddd4
Lauren Garcia <lauren.garcia@gmail.com>
2017-12-19 15:34:50 -0800

    standard: shared/js/ui/views/**/*.js

--- shared/js/ui/views/hamburger-menu.es6.js
+++ shared/js/ui/views/hamburger-menu.es6.js
@@ -1,64 +1,62 @@
 const Parent = window.DDG.base.View
 
 function HamburgerMenu (ops) {
-    this.model = ops.model
-    this.template = ops.template
-    Parent.call(this, ops)
+  this.model = ops.model
+  this.template = ops.template
+  Parent.call(this, ops)
 
-    this._setup()
+  this._setup()
 }
 
-HamburgerMenu.prototype = $.extend({},
-    Parent.prototype,
-    {
+HamburgerMenu.prototype = window.$.extend({},
+  Parent.prototype,
+  {
 
-        _setup: function () {
+    _setup: function () {
+      this._cacheElems('.js-hamburger-menu', [
+        'close',
+        'options-link'
+      ])
+      this.bindEvents([
+        [this.$close, 'click', this._closeMenu],
+        [this.$optionslink, 'click', this._openOptionsPage],
+        [this.model.store.subscribe, 'action:search', this._handleAction],
+        [this.model.store.subscribe, 'change:site', this._handleSiteUpdate]
+      ])
+    },
 
-            this._cacheElems('.js-hamburger-menu', [
-                'close',
-                'options-link'
-            ])
+    _handleAction: function (notification) {
+      if (notification.action === 'burgerClick') this._openMenu()
+    },
 
-            this.bindEvents([
-                [this.$close, 'click', this._closeMenu],
-                [this.$optionslink, 'click', this._openOptionsPage],
-                [this.model.store.subscribe, 'action:search', this._handleAction],
-                [this.model.store.subscribe, 'change:site', this._handleSiteUpdate]
-            ])
-        },
+    _openMenu: function (e) {
+      this.$el.removeClass('is-hidden')
+    },
 
-        _handleAction: function (notification) {
-            if (notification.action === 'burgerClick') this._openMenu()
-        },
+    _closeMenu: function (e) {
+      if (e) e.preventDefault()
+      this.$el.addClass('is-hidden')
+    },
 
-        _openMenu: function (e) {
-            this.$el.removeClass('is-hidden')
-        },
+    _handleSiteUpdate: function (notification) {
+      if (notification && notification.change.attribute === 'tab') {
+        this.model.tabUrl = notification.change.value.url
+        this._rerender()
+        this._setup()
+      }
+    },
 
-        _closeMenu: function (e) {
-            if (e) e.preventDefault()
-            this.$el.addClass('is-hidden')
-        },
-
-        _handleSiteUpdate: function (notification) {
-              if (notification && notification.change.attribute === 'tab') {
-                  this.model.tabUrl = notification.change.value.url
-                  this._rerender()
-                  this._setup()
-              }
-        },
-
-        _openOptionsPage: function () {
-            this.model.fetch({getBrowser: true}).then(browser => {
-                if (browser === 'moz') {
-                    chrome.tabs.create({url: chrome.extension.getURL("/html/options.html")})
-                    window.close()
-                } else {
-                    chrome.runtime.openOptionsPage()
-                }
-            })
+    _openOptionsPage: function () {
+      this.model.fetch({getBrowser: true}).then(browser => {
+        if (browser === 'moz') {
+          window.chrome.tabs.create({url: window.chrome.extension.getURL('/html/options.html')})
+          window.close()
+        } else {
+          window.chrome.runtime.openOptionsPage()
         }
+      })
     }
+  }
 )
 
 module.exports = HamburgerMenu
--- shared/js/ui/views/top-blocked-truncated.es6.js
+++ shared/js/ui/views/top-blocked-truncated.es6.js
@@ -4,54 +4,54 @@ const TopBlockedFullView = require('./top-blocked.es6.js')
 const topBlockedFullTemplate = require('./../templates/top-blocked.es6.js')
 
 function TruncatedTopBlocked (ops) {
-    this.model = ops.model
-    this.pageView = ops.pageView
-    this.template = ops.template
-    Parent.call(this, ops)
+  this.model = ops.model
+  this.pageView = ops.pageView
+  this.template = ops.template
+  Parent.call(this, ops)
 
-    this.model.getTopBlocked().then(() => {
-        this.rerenderList()
-    })
+  this.model.getTopBlocked().then(() => {
+    this.rerenderList()
+  })
 
-    this.bindEvents([
-        [this.model.store.subscribe, 'action:backgroundMessage', this.handleBackgroundMsg]
-    ])
+  this.bindEvents([
+    [this.model.store.subscribe, 'action:backgroundMessage', this.handleBackgroundMsg]
+  ])
 }
 
-TruncatedTopBlocked.prototype = $.extend({},
-    Parent.prototype,
-    animateGraphBars,
-    {
+TruncatedTopBlocked.prototype = window.$.extend({},
+  Parent.prototype,
+  animateGraphBars,
+  {
 
-        _seeAllClick: function () {
-            this.views.slidingSubview = new TopBlockedFullView({
-                template: topBlockedFullTemplate,
-                numItems: 10
-            })
-        },
+    _seeAllClick: function () {
+      this.views.slidingSubview = new TopBlockedFullView({
+        template: topBlockedFullTemplate,
+        numItems: 10
+      })
+    },
 
-        _setup: function () {
-            this._cacheElems('.js-top-blocked', ['graph-bar-fg', 'pct', 'see-all'])
-            this.bindEvents([
-                [this.$seeall, 'click', this._seeAllClick]
-            ]);
-        },
+    _setup: function () {
+      this._cacheElems('.js-top-blocked', ['graph-bar-fg', 'pct', 'see-all'])
+      this.bindEvents([
+        [this.$seeall, 'click', this._seeAllClick]
+      ])
+    },
 
-        rerenderList: function () {
-            this._rerender()
-            this._setup()
-            this.animateGraphBars()
-        },
+    rerenderList: function () {
+      this._rerender()
+      this._setup()
+      this.animateGraphBars()
+    },
 
-        handleBackgroundMsg: function (message) {
-            if (!message || !message.action) return
+    handleBackgroundMsg: function (message) {
+      if (!message || !message.action) return
 
-            if (message.action === 'didResetTrackersData') {
-                this.model.reset()
-                this.rerenderList()
-            }
-        }
+      if (message.action === 'didResetTrackersData') {
+        this.model.reset()
+        this.rerenderList()
+      }
     }
-);
+  }
+)
 
 module.exports = TruncatedTopBlocked
