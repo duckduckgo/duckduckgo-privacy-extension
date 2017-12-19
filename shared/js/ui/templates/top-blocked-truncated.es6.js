af92e024c2e5f12320adb1e5c24871e98c5ff3ca
MariagraziaAlastra <maria@duckduckgo.com>
2017-12-19 15:38:34 -0500

    padding for top blocked card

--- shared/js/ui/templates/top-blocked-truncated.es6.js
+++ shared/js/ui/templates/top-blocked-truncated.es6.js
@@ -5,19 +5,19 @@ const noData = require('./shared/top-blocked-no-data.es6.js')
 module.exports = function () {
 
     if (this.model.companyListMap && this.model.companyListMap.length > 0) {
-        return bel`<section class="top-blocked card">
+        return bel`<section class="top-blocked top-blocked--truncated card">
             <h3 class="padded uppercase text--center">
                 Tracker Networks Top Offenders
             </h3>
             <ol class="default-list top-blocked__list top-blocked__list--truncated">
                 ${listItems(this.model.companyListMap)}
-                <li class="top-blocked__li top-blocked__li--see-all border--top">
-                    <a href="#" class="link-secondary js-top-blocked-see-all">
-                        <span class="icon icon__arrow pull-right"></span>
-                        All Tracker Networks
-                    </a>
-                </li>
             </ol>
+            <div class="top-blocked__see-all border--top">
+                <a href="#" class="link-secondary js-top-blocked-see-all">
+                    <span class="icon icon__arrow pull-right"></span>
+                    All Tracker Networks
+                </a>
+            </div>
         </section>`
     } else {
         return bel`<section class="top-blocked card card--transparent">
