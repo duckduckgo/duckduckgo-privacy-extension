36ed5add4dc13aa11719e4dd461d80308f03c652
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-19 13:43:45 +0000

    add some extra styling/markup

--- shared/js/ui/templates/privacy-practices.es6.js
+++ shared/js/ui/templates/privacy-practices.es6.js
@@ -2,20 +2,28 @@ const bel = require('bel')
 const header = require('./shared/sliding-subview-header.es6.js')
 
 module.exports = function () {
-
     return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
         ${header('Privacy Practices')}
-        <div class="site-info site-info--details card card--no-top-margin">
-            <h1 class="site-info__domain">dodgysite.tk</h1>
-        </div>
-        <div class="site-info site-info--details card card--no-top-margin">
-            Privacy practices indicate how much the personal information
-            that you share with a website is protected.
-        </div>
-        <div class="site-info site-info--details card card--no-top-margin">
-            <ul>
-                <li><i>X</i>You must provide your legal name</li>
+        <div class="privacy-practices site-info site-info--details
+            card card--no-top-margin">
+            <div class="privacy-practices__overview padded border--bottom">
+                <h1>dodgysite.tk</h1>
+            </div>
+            <div class="privacy-practices__explainer padded border--bottom">
+                Privacy practices indicate how much the personal information
+                that you share with a website is protected.
+            </div>
+            <ul class="privacy-practices__details padded border--bottom">
+                <li class="privacy-practices__detail-item
+                    privacy-practices__detail-item--bad
+                    ">You must provide your legal name</li>
+                <li class="privacy-practices__detail-item
+                    privacy-practices__detail-item--good
+                    ">Your personal information is used for limited purposes</li>
             </ul>
+            <div class="privacy-practices__attrib padded">
+                Privacy Practice results from <a href="https://tosdr.org/">TOSDR</a>
+            </div>
         </div>
      </section>`
 }
