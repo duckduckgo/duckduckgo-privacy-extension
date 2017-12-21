71e691d470f99f5b926854ee598b297d369a147f
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-21 18:18:18 +0000

    fix detail for no privacy practices

--- shared/js/ui/templates/shared/privacy-practices-details.es6.js
+++ shared/js/ui/templates/shared/privacy-practices-details.es6.js
@@ -1,25 +1,19 @@
 const bel = require('bel')
 
 module.exports = function (tosdr) {
-    if (!tosdr || !tosdr.reasons) {
-        return bel`<div class="text--center">
-            <div class="privacy-practices__details__no-detail-icon"></div>
-            <h1 class="privacy-practices__details__title">
-                No Privacy Practices Found
-            </h1>
-            <div class="privacy-practices__details__msg">
-                The Privacy practices of this website have not been reviewed
-            </div>
-        </div>`
+    if (tosdr && tosdr.reasons) {
+        let good = tosdr.reasons.good || []
+        let bad = tosdr.reasons.bad || []
+
+        if (good.length || bad.length) {
+            return bel`<ul>
+                ${good.map(renderItem.bind(null, 'good'))}
+                ${bad.map(renderItem.bind(null, 'bad'))}
+            </ul>`
+        }
     }
 
-    let good = tosdr.reasons.good || []
-    let bad = tosdr.reasons.bad || []
-
-    return bel`<ul>
-        ${good.map(renderItem.bind(null, 'good'))}
-        ${bad.map(renderItem.bind(null, 'bad'))}
-    </ul>`
+    return renderNoDetails()
 }
 
 function renderItem (modifier, item) {
@@ -27,4 +21,16 @@ function renderItem (modifier, item) {
         privacy-practices__detail-item--${modifier} bold">
         ${item}
     </li>`
+}
+
+function renderNoDetails () {
+    return bel`<div class="text--center">
+        <div class="privacy-practices__details__no-detail-icon"></div>
+        <h1 class="privacy-practices__details__title">
+            No Privacy Practices Found
+        </h1>
+        <div class="privacy-practices__details__msg">
+            The Privacy practices of this website have not been reviewed
+        </div>
+    </div>`
 }
