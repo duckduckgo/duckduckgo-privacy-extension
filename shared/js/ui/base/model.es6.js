0eb3cdbdf11bebe2308c19c73ddad95ba602dbef
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-22 14:28:53 +0000

    whitespace fixes

--- shared/js/ui/base/model.es6.js
+++ shared/js/ui/base/model.es6.js
@@ -1,160 +1,151 @@
-const $ = require('jquery');
-const mixins = require('./mixins/index.es6.js');
-const store = require('./store.es6.js');
+const $ = require('jquery')
+const mixins = require('./mixins/index.es6.js')
+const store = require('./store.es6.js')
 
 function BaseModel (attrs) {
+  // attributes are applied directly
+  // onto the instance:
+  $.extend(this, attrs)
 
-    // attributes are applied directly
-    // onto the instance:
-    $.extend(this, attrs)
-
-    // register model with `store` of
-    // global notifications
-    // (after checking `modelName` property)
-    if (!this.modelName || typeof this.modelName !== 'string') {
-        throw new Error ('cannot init model without `modelName` property')
-    } else {
-        // make sure there's no collisions between different instances
-        // of the same model
-        if (this.uniqueModelName) {
-            this.modelName += `-${this._getUUID()}`
-        }
-
-        this.store = store
-        this.store.register(this.modelName)
+  // register model with `store` of
+  // global notifications
+  // (after checking `modelName` property)
+  if (!this.modelName || typeof this.modelName !== 'string') {
+    throw new Error('cannot init model without `modelName` property')
+  } else {
+    // make sure there's no collisions between different instances
+    // of the same model
+    if (this.uniqueModelName) {
+      this.modelName += `-${this._getUUID()}`
     }
 
-};
+    this.store = store
+    this.store.register(this.modelName)
+  }
+}
 
 BaseModel.prototype = $.extend({},
-    mixins.events,
-    {
+  mixins.events,
+  {
 
-        /**
-         * Setter method for modifying attributes
-         * on the model. Since the attributes
-         * are directly accessible + mutable on the object
-         * itself, you don't *have* to use the set method.
-         *
-         * However, the benefit of using the set method
-         * is that changes are broadcast out via store
-         * to any UI components that might want to observe
-         * changes and update their state.
-         *
-         * @param {string or object} attr
-         * @param {*} val
-         * @api public
-         */
-        set: function (attr, val) {
-
-            // support passing a hash of values to set instead of
-            // single attribute/value pair, i.e.:
-            //
-            // this.set({
-            //   title: 'something',
-            //   description: 'something described'
-            // })
-            if (typeof attr === 'object') {
-                for (var key in attr) {
-                    this.set(key, attr[key], val);
-                }
-                return;
-            }
-
-            const lastValue = this[attr] || null;
-            this[attr] = val;
-
-            this.store.publish({
-                notifierName: this.modelName,
-                change: {attribute: attr, value: val, lastValue: lastValue},
-                attributes: this._toJSON()
-            });
-        },
-
-
-        /**
-         * Convenience method for code clarity
-         * so we can explicitly call clear()
-         * instead of doing null sets.
-         * Using .clear() broadcasts the change
-         * out to the rest of the app via this.set()
-         * which calls this.store.publish()
-         */
-        clear: function(attr) {
-            this.set(attr, null);
-        },
-
-
-        /**
-         * Destroy any of this model's bound events
-         * and remove its reducer from store so
-         * there is no memeory footprint left.
-         * Mostly used when view.destroy() is called.
-         */
-         destroy: function () {
-             this.unbindEvents();
-             this.store.remove(this.modelName);
-         },
-
-
-        /**
-         * Fetch data from background
-         * this.model.fetch({'messageName': messageValue}).then((response) ..
-         **/
-        fetch: function (message) {
-            return new Promise((resolve, reject) => {
-                chrome.runtime.sendMessage(message, ((result) => {
-                        resolve(result)
-                    })
-                )
-            })
-        },
-
-        /**
-         * Send a user action
-         * Broadcasts an action to other UI components
-         * via notification store
-         * @param action {string}
-         * @param data {could be a jquery event or other data; is optional}
-         */
-        send: function (action, data) {
-            if (!action) throw new Error('model.send() requires an action argument')
-            data = data || null
-            this.store.publish({
-                notifierName: this.modelName,
-                action: action,
-                data: data,
-                attributes: this._toJSON()
-            })
-        },
-
-
-        /**
-         * Private method for turning `this` into a
-         * JSON object before sending to application store.
-         * Basically just weeds out properties that
-         * are functions.
-         */
-        _toJSON: function () {
-            let attributes = Object.assign({}, Object.getPrototypeOf(this), this);
-            if (attributes.store) delete attributes.store;
-            return JSON.parse(JSON.stringify(attributes));
-        },
-
-        /**
-         * Generate unique identifier
-         * (from http://stackoverflow.com/a/2117523)
-         */
-        _getUUID: function () {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                let r = Math.random() * 16 | 0
-                let v = c == 'x' ? r : (r & 0x3 | 0x8)
-
-                return v.toString(16)
-            })
+    /**
+     * Setter method for modifying attributes
+     * on the model. Since the attributes
+     * are directly accessible + mutable on the object
+     * itself, you don't *have* to use the set method.
+     *
+     * However, the benefit of using the set method
+     * is that changes are broadcast out via store
+     * to any UI components that might want to observe
+     * changes and update their state.
+     *
+     * @param {string or object} attr
+     * @param {*} val
+     * @api public
+     */
+    set: function (attr, val) {
+      /*
+      * support passing a hash of values to set instead of
+      * single attribute/value pair, i.e.:
+      *
+      * this.set({
+      *   title: 'something',
+      *   description: 'something described'
+      * })
+      */
+      if (typeof attr === 'object') {
+        for (var key in attr) {
+          this.set(key, attr[key], val)
         }
+        return
+      }
 
+      const lastValue = this[attr] || null
+      this[attr] = val
+
+      this.store.publish({
+        notifierName: this.modelName,
+        change: {attribute: attr, value: val, lastValue: lastValue},
+        attributes: this._toJSON()
+      })
+    },
+
+    /**
+     * Convenience method for code clarity
+     * so we can explicitly call clear()
+     * instead of doing null sets.
+     * Using .clear() broadcasts the change
+     * out to the rest of the app via this.set()
+     * which calls this.store.publish()
+     */
+    clear: function (attr) {
+      this.set(attr, null)
+    },
+
+    /**
+     * Destroy any of this model's bound events
+     * and remove its reducer from store so
+     * there is no memeory footprint left.
+     * Mostly used when view.destroy() is called.
+     */
+    destroy: function () {
+      this.unbindEvents()
+      this.store.remove(this.modelName)
+    },
+
+    /**
+     * Fetch data from background
+     * this.model.fetch({'messageName': messageValue}).then((response) ..
+     **/
+    fetch: function (message) {
+      return new Promise((resolve, reject) => {
+        window.chrome.runtime.sendMessage(message, (result) => resolve(result))
+      })
+    },
+
+    /**
+     * Send a user action
+     * Broadcasts an action to other UI components
+     * via notification store
+     * @param action {string}
+     * @param data {could be a jquery event or other data is optional}
+     */
+    send: function (action, data) {
+      if (!action) throw new Error('model.send() requires an action argument')
+      data = data || null
+      this.store.publish({
+        notifierName: this.modelName,
+        action: action,
+        data: data,
+        attributes: this._toJSON()
+      })
+    },
+
+     /**
+      * Private method for turning `this` into a
+      * JSON object before sending to application store.
+      * Basically just weeds out properties that
+      * are functions.
+      */
+    _toJSON: function () {
+      let attributes = Object.assign({}, Object.getPrototypeOf(this), this)
+      if (attributes.store) delete attributes.store
+      return JSON.parse(JSON.stringify(attributes))
+    },
+
+    /**
+     * Generate unique identifier
+     * (from http://stackoverflow.com/a/2117523)
+     */
+    _getUUID: function () {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = Math.random() * 16 | 0
+        let v = c == 'x' ? r : (r & 0x3 | 0x8)
+
+        return v.toString(16)
+      })
     }
-);
+  }
+)
 
-module.exports = BaseModel;
+module.exports = BaseModel
--- shared/js/ui/views/top-blocked.es6.js
+++ shared/js/ui/views/top-blocked.es6.js
@@ -3,66 +3,67 @@ const animateGraphBars = require('./mixins/animate-graph-bars.es6.js')
 const TopBlockedModel = require('./../models/top-blocked.es6.js')
 
 function TopBlocked (ops) {
-    // model data is async
-    this.model = null
-    this.numItems = ops.numItems
-    this.template = ops.template
-    ParentSlidingSubview.call(this, ops)
+  // model data is async
+  this.model = null
+  this.numItems = ops.numItems
+  this.template = ops.template
+  ParentSlidingSubview.call(this, ops)
 
-    this.setupClose()
-    this.renderAsyncContent()
+  this.setupClose()
+  this.renderAsyncContent()
 
-    this.bindEvents([
-        [this.model.store.subscribe, 'action:backgroundMessage', this.handleBackgroundMsg]
-    ])
+  this.bindEvents([
+    [this.model.store.subscribe, 'action:backgroundMessage', this.handleBackgroundMsg]
+  ])
 }
 
-TopBlocked.prototype = $.extend({},
-    ParentSlidingSubview.prototype,
-    animateGraphBars,
-    {
+TopBlocked.prototype = window.$.extend({},
+  ParentSlidingSubview.prototype,
+  animateGraphBars,
+  {
 
-        setup: function () {
-            this.$content = this.$el.find('.js-top-blocked-content')
-            // listener for reset stats click
-            this.$reset = this.$el.find('.js-reset-trackers-data')
-            this.bindEvents([
-                [this.$reset, 'click', this.resetTrackersStats]
-            ])
-        },
+    setup: function () {
+      this.$content = this.$el.find('.js-top-blocked-content')
+      // listener for reset stats click
+      this.$reset = this.$el.find('.js-reset-trackers-data')
+      this.bindEvents([
+        [this.$reset, 'click', this.resetTrackersStats]
+      ])
+    },
 
-        renderAsyncContent: function () {
-            this.model = new TopBlockedModel({
-                uniqueModelName: true,
-                numCompanies: this.numItems
-            })
-            this.model.getTopBlocked().then(() => {
-                const content = this.template()
-                this.$el.append(content)
-                this.setup()
+    renderAsyncContent: function () {
+      const random = Math.round(Math.random() * 100000)
+      this.model = new TopBlockedModel({
+        uniqueModelName: true,
+        numCompanies: this.numItems
+      })
+      this.model.getTopBlocked().then(() => {
+        const content = this.template()
+        this.$el.append(content)
+        this.setup()
 
-                // animate graph bars and pct
-                this.$graphbarfg = this.$el.find('.js-top-blocked-graph-bar-fg')
-                this.$pct = this.$el.find('.js-top-blocked-pct')
-                this.animateGraphBars()
-            })
-        },
+        // animate graph bars and pct
+        this.$graphbarfg = this.$el.find('.js-top-blocked-graph-bar-fg')
+        this.$pct = this.$el.find('.js-top-blocked-pct')
+        this.animateGraphBars()
+      })
+    },
 
-        resetTrackersStats: function (e) {
-            if (e) e.preventDefault()
-            this.model.fetch({resetTrackersData: true})
-        },
+    resetTrackersStats: function (e) {
+      if (e) e.preventDefault()
+      this.model.fetch({resetTrackersData: true})
+    },
 
-        handleBackgroundMsg: function (message) {
-            if (!message || !message.action) return
+    handleBackgroundMsg: function (message) {
+      if (!message || !message.action) return
 
-            if (message.action === 'didResetTrackersData') {
-                this.model.reset(message.data)
-                const content = this.template()
-                this.$content.replaceWith(content)
-            }
-        }
+      if (message.action === 'didResetTrackersData') {
+        this.model.reset(message.data)
+        const content = this.template()
+        this.$content.replaceWith(content)
+      }
     }
+  }
 )
 
 module.exports = TopBlocked
