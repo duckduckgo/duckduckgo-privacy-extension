1657f38043c76143fd31a28c9deb4c706888f1e2
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-28 14:55:08 +0000

    fill in hero section of the scorecard view

--- shared/js/ui/templates/grade-scorecard.es6.js
+++ shared/js/ui/templates/grade-scorecard.es6.js
@@ -1,7 +1,27 @@
 const bel = require('bel')
+const siteRating = require('./shared/site-rating.es6.js')
+const siteRatingSubtitle = require('./shared/site-rating-subtitle.es6.js')
 
 module.exports = function () {
   return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
-    hi!
+   <div class="site-info card">
+     <div class="hero border--bottom">
+      <a href="#" class="hero__close js-sliding-subview-close">
+        <span class="icon icon__arrow icon__arrow--left">
+        </span>
+      </a>
+      ${siteRating(
+        this.model.isCalculatingSiteRating,
+        this.model.siteRating,
+        this.model.isWhitelisted
+      )}
+      <h1 class="hero__title">${this.model.domain}</h1>
+      ${siteRatingSubtitle(
+        this.model.isCalculatingSiteRating,
+        this.model.siteRating,
+        this.model.isWhitelisted
+      )}
+    </div>
+  </div>
   </section>`
 }
