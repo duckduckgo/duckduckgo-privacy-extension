bfc11f5cb01607b3aa01b72881a844556bcd13c6
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-28 18:21:05 +0000

    style up all the items

--- shared/js/ui/templates/grade-scorecard.es6.js
+++ shared/js/ui/templates/grade-scorecard.es6.js
@@ -3,6 +3,13 @@ const siteRating = require('./shared/site-rating.es6.js')
 const siteRatingSubtitle = require('./shared/site-rating-subtitle.es6.js')
 
 module.exports = function () {
+  let detailItems = []
+
+  detailItems.push(renderItem('good', 'Encrypted Connection'))
+  detailItems.push(renderItem('bad', '10 Tracker Networks Blocked'))
+  detailItems.push(renderItem('bad', '3 Major Trackers Blocked'))
+  detailItems.push(renderItem('mixed', 'Mixed privacy practices'))
+
   return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
    <div class="site-info card">
      <div class="hero border--bottom">
@@ -22,6 +29,20 @@ module.exports = function () {
         this.model.isWhitelisted
       )}
     </div>
+    <ul class="status-items status-items--right padded border--bottom--inner">
+      ${detailItems}
+    </ul>
+    <ul class="status-items status-items--right padded">
+      ${renderItem('c', 'Privacy Grade')}
+      ${renderItem('a', 'Enhanced Grade')}
+    </ul>
   </div>
   </section>`
+}
+
+function renderItem (modifier, item) {
+  return bel`<li class="status-items__item
+      status-items__item--${modifier} bold">
+    ${item}
+  </li>`
 }
