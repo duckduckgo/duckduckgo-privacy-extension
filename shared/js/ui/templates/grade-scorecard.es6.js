0272fa00260aa54a3cfce2bd1d26bac7d084c468
Andrey Pissantchev <pissantchev@gmail.com>
2017-12-29 12:22:55 +0000

    plug in data from model

--- shared/js/ui/templates/grade-scorecard.es6.js
+++ shared/js/ui/templates/grade-scorecard.es6.js
@@ -3,13 +3,6 @@ const siteRating = require('./shared/site-rating.es6.js')
 const siteRatingSubtitle = require('./shared/site-rating-subtitle.es6.js')
 
 module.exports = function () {
-  let detailItems = []
-
-  detailItems.push(renderItem('good', 'Encrypted Connection'))
-  detailItems.push(renderItem('bad', '10 Tracker Networks Blocked'))
-  detailItems.push(renderItem('bad', '3 Major Trackers Blocked'))
-  detailItems.push(renderItem('mixed', 'Mixed privacy practices'))
-
   return bel`<section class="sliding-subview sliding-subview--has-fixed-header">
    <div class="site-info site-info--full-height card">
      <div class="hero border--bottom">
@@ -29,15 +22,78 @@ module.exports = function () {
         this.model.isWhitelisted
       )}
     </div>
-    <ul class="status-items status-items--right padded border--bottom--inner">
-      ${detailItems}
-    </ul>
-    <ul class="status-items status-items--right padded">
-      ${renderItem('c', 'Privacy Grade')}
-      ${renderItem('a', 'Enhanced Grade')}
-    </ul>
+    ${getReasons(this.model)}
+    ${getGrades(this.model)}
   </div>
   </section>`
+}
+
+function getReasons (model) {
+  let detailItems = []
+
+  // grab all the data from the items and create a status item for each of them
+
+  // encryption status
+  if (model.httpsStatusText) {
+    let connectionMsg = 'Unncrypted'
+    let modifier = 'bad'
+
+    if (model.httpsStatusText === 'Secure') {
+      connectionMsg = 'Encrypted'
+      modifier = 'good'
+    }
+
+    detailItems.push(renderItem(modifier, `${connectionMsg} Connection`))
+  }
+
+  // tracking networks blocked,
+  // only show a message if there's trackers blocked
+  if (model.trackersBlockedCount) {
+    detailItems.push(renderItem('bad', `${model.trackersBlockedCount} Tracker Networks Blocked`))
+  }
+
+  // major tracking networks,
+  // only show a message if it's bad
+  if (model.trackerNetworks.length) {
+    detailItems.push(renderItem('bad', `${model.trackerNetworks.length} Major Tracker Networks Blocked`))
+  }
+
+  // TODO handle case where the site is a major tracker network
+
+  // privacy practices from tosdr
+  if (model.tosdr &&
+      model.tosdr.message &&
+      model.tosdr.message !== 'Unknown') {
+    let msg = model.tosdr.message
+    let modifier = msg.toLowerCase()
+
+    detailItems.push(renderItem(modifier, `${msg} Privacy Practices`))
+  }
+
+  return bel`<ul class="status-items status-items--right padded border--bottom--inner">
+    ${detailItems}
+  </ul>`
+}
+
+function getGrades (model) {
+  let before = model.siteRating.before
+  let after = model.siteRating.after
+
+  if (!before || !after) {
+    return
+  }
+
+  let detailItems = []
+
+  detailItems.push(renderItem(before.toLowerCase(), 'Privacy Grade'))
+
+  if (before !== after) {
+    detailItems.push(renderItem(after.toLowerCase(), 'Enhanced Grade'))
+  }
+
+  return bel`<ul class="status-items status-items--right padded">
+    ${detailItems}
+  </ul>`
 }
 
 function renderItem (modifier, item) {
