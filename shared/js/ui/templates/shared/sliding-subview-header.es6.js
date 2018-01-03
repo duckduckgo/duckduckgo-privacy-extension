3e586bb497ea728e0aee226e11e07e721daa7970
jdorweiler <jason@duckduckgo.com>
2018-01-03 16:29:51 -0700

    fix links

--- shared/js/ui/templates/shared/sliding-subview-header.es6.js
+++ shared/js/ui/templates/shared/sliding-subview-header.es6.js
@@ -3,7 +3,7 @@ const hamburgerButton = require('./hamburger-button.es6.js')
 
 module.exports = function (title) {
   return bel`<nav class="sliding-subview__header card">
-    <a href="#" class="sliding-subview__header__back
+    <a href="javascript:void(0)" class="sliding-subview__header__back
       sliding-subview__header__back--is-icon
       js-sliding-subview-close">
       <span class="icon icon__arrow icon__arrow--left pull-left">
