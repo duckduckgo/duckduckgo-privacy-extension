785a5276278d83a1b12d45ad70ffd60d4f385a6a
Brian Stoner <brianstoner@Brians-MacBook-Pro-2.local>
2018-01-09 08:49:44 -0500

    store https list in local storage

--- shared/js/background.js
+++ shared/js/background.js
@@ -230,33 +230,15 @@ chrome.webRequest.onBeforeRequest.addListener(
         // Is this request from the tab's main frame?
         const isMainFrame = requestData.type === 'main_frame' ? true : false
 
-        // Fetch upgrade rule from db (synchronous -- Chrome only)
-        if (utils.isChromeBrowser()) {
-            const url = https.pipeRequestUrl(requestData.url, thisTab, isMainFrame)
-            if (url.toLowerCase() !== requestData.url.toLowerCase()) {
-                console.log('HTTPS: upgrade request url to ' + url)
-                if (isMainFrame) thisTab.upgradedHttps = true
-                thisTab.addHttpsUpgradeRequest(url)
-                return {redirectUrl: url}
-            } else {
-              return
-            }
-
-        // Fetch upgrade rule from db (asynchronous -- Firefox/Mozilla)
+        // Fetch upgrade rule from https module:
+        const url = https.getUpgradedUrl(requestData.url, thisTab, isMainFrame)
+        if (url.toLowerCase() !== requestData.url.toLowerCase()) {
+            console.log('HTTPS: upgrade request url to ' + url)
+            if (isMainFrame) thisTab.upgradedHttps = true
+            thisTab.addHttpsUpgradeRequest(url)
+            return {redirectUrl: url}
         } else {
-            return new Promise ((resolve) => {
-                https.pipeRequestUrl(requestData.url, thisTab, isMainFrame).then(
-                    (url) => {
-                        if (url.toLowerCase() !== requestData.url.toLowerCase()) {
-                            console.log('HTTPS: upgrade request url to ' + url)
-                            if (isMainFrame) thisTab.upgradedHttps = true
-                            thisTab.addHttpsUpgradeRequest(url)
-                            resolve({redirectUrl: url})
-                        }
-                        resolve()
-                    }
-                )
-            })
+          return
         }
     },
     {
--- shared/js/https.js
+++ shared/js/https.js
@@ -1,228 +1,149 @@
 (function () {
-const db = require('db')
+const load = require('load')
 const settings = require('settings')
 const utils = require('utils')
-let knownMixedContentList
 
-settings.ready().then(() => {
-    load.JSONfromLocalFile(constants.httpsWhitelist, (wl) => knownMixedContentList = wl)
-})
+let httpsUpgradeList = []
 
 class HTTPS {
 
     constructor () {
-        this.db = null
-        this.dbObjectStore = 'https'
-
-        // if this.isSync: load Chrome-only synchronous rules object into memory
-        this.isSync = utils.isChromeBrowser() ? true : false
-        this.syncRuleCache = {}
-
         this.isReady = false
-        this._ready = this.init().then(() => this.isReady = true)
+        this.init().then(() => this.isReady = true)
+
+        // in the background, try to update the list:
+        this.updateList()
 
         return this
     }
 
     init () {
-        return new Promise((resolve) => {
-            db.ready().then(() => {
-                this.db = db
+        console.log("HTTPS: init()")
 
-                if (this.isSync) {
-                    this.initSync().then(() => resolve())
-                } else {
-                    resolve()
+        return new Promise((resolve) => {
+            // Try to load the list from local storage
+            chrome.storage.local.get('https-upgrade-list', (results) => {
+                // if items were found in local storage,
+                // use those:
+                if (results && results['https-upgrade-list']) {
+                    try {
+                        httpsUpgradeList = JSON.parse(results['https-upgrade-list'])
+
+                        console.log("HTTPS: init() using upgrade list from local storage")
+                        console.log("HTTPS: init() " + httpsUpgradeList.simpleUpgrade.top500.length + " rules loaded")
+
+                        return resolve()
+                    } catch(e) {
+                        console.log("HTTPS: init() error parsing JSON from local storage")
+                    }
                 }
+
+                console.log("HTTPS: init() no upgrade list in local storage, loading from local file")
+
+                // fallback to loading smaller list packaged
+                // with the extension so we at least have something:
+                settings.ready().then(() => {
+                    load.JSONfromLocalFile(constants.httpsUpgradeList, (l) => {
+                        console.log("HTTPS: init() loaded upgrade list from local file")
+                        console.log("HTTPS: init() " + l.simpleUpgrade.top500.length + " rules loaded")
+
+                        httpsUpgradeList = l
+                        return resolve()
+                    })
+                })
             })
         })
     }
 
-    initSync () {
-        return new Promise((resolve) => {
-            this.db.logAllRecords(this.dbObjectStore, this.syncRuleCache)
-            resolve()
+    updateList() {
+        console.log("HTTPS: updateList()")
+
+        let etag = settings.getSetting('https-etag') || ''
+        
+        load.loadExtensionFile({
+            url: constants.httpsUpgradeListUrl,
+            source: 'external',
+            etag: etag
+        }, (data, res) => {
+            console.log("HTTPS: updateList() got updated list from server")
+
+            let newEtag = res.getResponseHeader('etag') || ''
+
+            try {
+                // update blacklist in memory
+                httpsUpgradeList = JSON.parse(data)
+
+                console.log("HTTPS: updateList() valid server response, swapping it in and saving to local storage")
+                console.log("HTTPS: updateList() " + httpsUpgradeList.simpleUpgrade.top500.length + " rules loaded")
+                
+                // save blacklist locally
+                chrome.storage.local.set({ 'https-upgrade-list': data })
+
+                // save new etag for next time
+                settings.updateSetting('https-etag', newEtag)
+            } catch(e) {
+                console.log("HTTPS: updateList() error parsing server response")
+            }
         })
     }
 
-    ready () {
-        return this._ready
+    getUpgradeList () {
+        return httpsUpgradeList
     }
 
-    pipeRequestUrl (reqUrl, tab, isMainFrame) {
+    getUpgradedUrl (reqUrl, tab, isMainFrame) {
 
-        function checkDb (resolve) {
-            if (!this.isReady) {
-                console.warn('HTTPS: .pipeRequestUrl() this.db is not ready')
-                if (this.isSync) { return reqUrl } else { return resolve(reqUrl) }
-            }
-
-            // Only deal with http calls
-            const protocol = utils.getProtocol(reqUrl).toLowerCase()
-            if (!protocol.indexOf('http:') === 0) {
-                if (this.isSync) { return reqUrl } else { return resolve(reqUrl) }
-            }
-
-            // Obey global settings (options page)
-            if (!settings.getSetting('httpsEverywhereEnabled')) {
-                if (this.isSync) { return reqUrl } else { resolve(reqUrl) }
-            }
-
-            // Skip upgrading sites that have been whitelisted by user
-            // via on/off toggle in popup
-            if (tab.site.whitelisted) {
-                console.log(`HTTPS: ${tab.site.domain} was whitelisted by user. skip upgrade check.`)
-                if (this.isSync) { return reqUrl } else { return resolve(reqUrl) }
-            }
-
-            // Skip upgrading sites that have been 'HTTPSwhitelisted'
-            // bc they contain mixed https content when forced to upgrade
-            if (tab.site.HTTPSwhitelisted) {
-                console.log(`HTTPS: ${tab.site.domain} has known mixed content. skip upgrade check.`)
-                if (this.isSync) { return reqUrl } else { return resolve(reqUrl) }
-            }
-
-            // If `isMainFrame` request and host has known mixed content,
-            // skip db check (don't force upgrade)
-            if (isMainFrame) {
-                if (knownMixedContentList && knownMixedContentList[tab.site.domain]) {
-                    console.log(`HTTPS: ${tab.site.domain} has known mixed content. skip upgrade check.`)
-                    if (this.isSync) { return reqUrl } else { return resolve(reqUrl) }
-                }
-            }
-
-            // Determine host
-            const host = utils.extractHostFromURL(reqUrl)
-            const loop = [host]
-
-            // Check if host has an entry as a wildcarded subdomain in db
-            const subdomain = utils.extractTopSubdomainFromHost(host)
-            if (subdomain && subdomain !== 'www') {
-                const wildcard = host.replace(subdomain, '*')
-                loop.push(wildcard)
-            }
-
-            // Check db for simple upgrades
-            let isResolved = false
-            let syncUrlResolution = reqUrl // Only used for sync version!
-            loop.forEach((r, i) => {
-                if (isResolved) return
-
-                if (this.isSync) {
-                    if (this.syncRuleCache[r] &&
-                        this.syncRuleCache[r].simpleUpgrade &&
-                        this.syncRuleCache[r].simpleUpgrade === true) {
-                        const upgrade = reqUrl.replace(/^(http|https):\/\//i, 'https://')
-                        isResolved = true
-                        return syncUrlResolution = upgrade
-                    }
-                } else {
-                    this.db
-                        .get(this.dbObjectStore, r.toLowerCase())
-                        .then(
-                            (record) => {
-                                if (record && record.simpleUpgrade) {
-                                    const upgrade = reqUrl.replace(/^(http|https):\/\//i, 'https://')
-                                    isResolved = true
-                                    return resolve(upgrade)
-                                }
-                                if (i === (loop.length - 1)) return resolve(reqUrl)
-                            },
-                            () => {
-                                console.warn('HTTPS: pipeRequestUrl() encountered a db error')
-                                if (i === (loop.length -1)) return resolve(reqUrl)
-                            }
-                        )
-                }
-            })
-
-            if (this.isSync) return syncUrlResolution
+        if (!this.isReady) {
+            console.warn('HTTPS: .pipeRequestUrl() this.db is not ready')
+            return reqUrl
         }
 
-        if (this.isSync) {
-            return checkDb.call(this)
-        } else {
-            return new Promise((resolve) => checkDb.call(this, resolve))
+        // Only deal with http calls
+        const protocol = utils.getProtocol(reqUrl).toLowerCase()
+        if (!protocol.indexOf('http:') === 0) {
+            return reqUrl
         }
-    }
 
-    /**
-     * For debugging/development/test purposes only
-     * Logs all records in indexed db to console
-     */
-    logAllRecords () {
-        this.db.logAllRecords(this.dbObjectStore)
-    }
+        // Obey global settings (options page)
+        if (!settings.getSetting('httpsEverywhereEnabled')) {
+            return reqUrl
+        }
 
-    /**
-     * For debugging/development/test purposes only
-     * This only tests ability to retrieve record from indexed db
-     * Usage: .getHostRecord('goodreads.com').then((r) => console.log(r))
-     */
-    getHostRecord (host) {
-        return new Promise ((resolve, reject) => {
-            if (!this.isReady) {
-                console.warn('HTTPS: this.db is not ready')
-                return reject()
+        // Skip upgrading sites that have been whitelisted by user
+        // via on/off toggle in popup
+        if (tab.site.whitelisted) {
+            console.log(`HTTPS: ${tab.site.domain} was whitelisted by user. skip upgrade check.`)
+            return reqUrl
+        }
+
+        // Determine host
+        const host = utils.extractHostFromURL(reqUrl)
+        const hosts = [host]
+
+        // Check if host has an entry as a wildcarded subdomain
+        const subdomain = utils.extractTopSubdomainFromHost(host)
+        if (subdomain && subdomain !== 'www') {
+            const wildcard = host.replace(subdomain, '*')
+            hosts.push(wildcard)
+        }
+
+        // Check for upgrades
+        for (let i=0; i<hosts.length; i++) {
+            if (httpsUpgradeList.simpleUpgrade.top500.indexOf(hosts[i]) > -1) {
+                return reqUrl.replace(/^(http|https):\/\//i, 'https://')
             }
+        }
 
-            this.db.get(this.dbObjectStore, host).then(
-                (record) => {
-                    if (record) return resolve(record)
-                    return resolve()
-                },
-                () => {
-                    console.warn('HTTPS: getHostRecord() encountered a db error.')
-                    return reject()
-                }
-            )
-
-        })
-    }
-
-    /* For debugging/development/test purposes only
-     * Tests the .getHostRecord() method against an array of test hosts
-     * defined below
-     */
-    testGetHostRecord (cb) {
-        // These hosts should always have records that were xhr'd
-        // into the client-side db from server
-        const testHosts = [
-            '1337x.to',
-            'submit.pandora.com',
-            '*.api.roblox.com',
-            'thump.vice.com',
-            'yts.ag'
-        ]
-
-        // Test that host records exist after db install from server
-        testHosts.forEach((host, i) => {
-            this.getHostRecord(host, { site: {} } ).then(
-                (record) => {
-                    if (record) {
-                        console.log('HTTPS: retrieved record for host: ' + host)
-                        console.log(record)
-                        if (cb && i === (testHosts.length - 1)) return cb()
-                        return
-                    }
-                    console.error('HTTPS: could not find record for host: ' + host)
-                    if (cb) cb(new Error('HTTPS: could not find record for host: ' + host))
-                },
-                () => {
-                    console.error('HTTPS: testDBKeys() encountered a db error for host: ' + host)
-                    if (cb) cb(new Error('HTTPS: testDBKeys() encountered a db error for host: ' + host))
-                }
-            )
-        })
+        // If it falls to here, default to reqUrl
+        return reqUrl
     }
 
     /**
      * For debugging/development/test purposes only
-     * Tests the .pipeRequestUrl() method in both synchronous (Chrome) and
-     * asynchronous modes (depending on browser it is run in) for
-     * array of test urls defined below
+     * Tests the .pipeRequestUrl() method 
+     * for array of test urls defined below
      */
-    testPipeRequestUrl () {
+    testGetUpgradedUrl () {
         // These hosts should always have records that were xhr'd
         // into the client-side db from server
         const testUrls = [
@@ -232,27 +153,15 @@ class HTTPS {
             'http://THUMP.vice.com',
             'http://yts.ag'
         ]
-        console.log('HTTPS: testPipeRequestUrl() for ' + testUrls.length + ' urls')
 
-        function _handleDone (r, i) {
+        console.log('HTTPS: testGetUgradedUrl() for ' + testUrls.length + ' urls')
+        console.time('testGetUpgradedUrlTimer')
+
+        testUrls.forEach((url, i) => {
+            const r = this.getUpgradedUrl(url, { site: {}} )
             console.log(r)
-            if (i === (testUrls.length - 1)) console.timeEnd('testPipeRequestUrlTimer')
-        }
-
-        console.time('testPipeRequestUrlTimer')
-        if (this.isSync) {
-            testUrls.forEach((url, i) => {
-                const r = this.pipeRequestUrl(url, { site: {}} )
-                _handleDone(r, i)
-            })
-
-        } else {
-            testUrls.forEach((url, i) => {
-                this.pipeRequestUrl(url, { site: {}} )
-                    .then((r) => _handleDone(r, i),
-                          (r) => _handleDone(r, i))
-            })
-        }
+            if (i === (testUrls.length - 1)) console.timeEnd('testGetUpgradedUrlTimer')
+        })
     }
 }
 
