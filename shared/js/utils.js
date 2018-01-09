c51b3e31ba6c95471a57aed041be8325f47d3719
Lauren Garcia <lauren.garcia@gmail.com>
2018-01-08 20:36:40 -1000

    js/utils parseUserAgentString() include Safari

--- shared/js/utils.js
+++ shared/js/utils.js
@@ -29,7 +29,7 @@ require.scopes.utils = ( () => {
 
     function parseUserAgentString (uaString) {
         if (!uaString) uaString = window.navigator.userAgent
-        const rgx = uaString.match(/(Firefox|Chrome)\/([0-9]+)/)
+        const rgx = uaString.match(/(Firefox|Chrome|Safari)\/([0-9]+)/)
         return {
             browser: rgx[1],
             majorVersion: rgx[2]
