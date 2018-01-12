07ec43332e72620727e6c81e44e0ea28224ef4b5
Andrey Pissantchev <pissantchev@gmail.com>
2018-01-12 13:48:00 +0000

    tweak height, remove card shadows

--- shared/js/ui/templates/site.es6.js
+++ shared/js/ui/templates/site.es6.js
@@ -7,7 +7,7 @@ module.exports = function () {
   const tosdrMsg = (this.model.tosdr && this.model.tosdr.message) ||
      window.constants.tosdrMessages.unknown
 
-  return bel`<section class="site-info site-info--main card">
+  return bel`<section class="site-info site-info--main">
     <ul class="default-list">
     <li class="site-info__rating-li silver-bg">
       ${ratingHero(this.model, {
