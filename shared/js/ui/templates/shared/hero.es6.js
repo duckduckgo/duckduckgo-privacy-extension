f20fac98fac07ac07bc69c5dcf7661c4212af282
MariagraziaAlastra <maria@duckduckgo.com>
2018-01-17 13:18:50 -0500

    clickable divs should have cursor:pointer

--- shared/js/ui/templates/shared/hero.es6.js
+++ shared/js/ui/templates/shared/hero.es6.js
@@ -2,7 +2,7 @@ const bel = require('bel')
 
 module.exports = function (ops) {
   const slidingSubviewClass = ops.showClose ? 'js-sliding-subview-close' : ''
-  return bel`<div class="hero border--bottom text--center js-hero ${slidingSubviewClass}">
+  return bel`<div class="hero border--bottom text--center ${slidingSubviewClass}">
     ${renderOpenOrCloseButton(ops.showClose)}
     <div class="hero__icon hero__icon--${ops.status}">
     </div>
