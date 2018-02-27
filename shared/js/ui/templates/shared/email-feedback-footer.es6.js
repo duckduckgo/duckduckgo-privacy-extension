4a4f1591c00b6937954e5d459c93ded4b4c394c3
Andrey Pissantchev <pissantchev@gmail.com>
2018-02-27 13:04:15 +0200

    include extension version

--- shared/js/ui/templates/shared/email-feedback-footer.es6.js
+++ shared/js/ui/templates/shared/email-feedback-footer.es6.js
@@ -1,11 +1,15 @@
 module.exports = function (browserInfo, url) {
-  let footer = '---'
+  let footer = '---\n'
 
   if (url) {
-    footer += `\nURL is ${url}`
+    footer += `URL is ${url}\n`
   }
 
-  footer += `\nBrowser is ${browserInfo.browser} ${browserInfo.version}`
+  // TODO fix this so it's not hardcoded
+  let extensionVersion = `2018.18.22`
+
+  footer += `Browser is ${browserInfo.browser} ${browserInfo.version}
+Extension version is ${extensionVersion}`
 
   return footer
 }
