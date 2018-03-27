cffb195d91f165c1492b37c6e730e6b17075c028
MariagraziaAlastra <maria@duckduckgo.com>
2018-03-27 13:53:09 -0400

    push unblocked domains to the bottom + specify they're unblocked

--- shared/js/ui/models/site-company-list.es6.js
+++ shared/js/ui/models/site-company-list.es6.js
@@ -75,7 +75,7 @@ SiteCompanyList.prototype = window.$.extend({},
       if (!company || !company.urls) return false
 
       const urls = Object.keys(company.urls)
-      return urls.some((url) => url.isBlocked === false)
+      return urls.some((url) => company.urls[url].isBlocked === false)
     },
 
     // Determines sorting order of the company list
