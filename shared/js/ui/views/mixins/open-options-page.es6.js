b4f69167b6a80c42bf8cc51ba3bb2095dfbb4315
jdorweiler <jason@duckduckgo.com>
2018-04-06 12:37:37 -0600

    open options page

--- shared/js/ui/views/mixins/open-options-page.es6.js
+++ shared/js/ui/views/mixins/open-options-page.es6.js
@@ -1,11 +1,18 @@
+const wrapper = require('./../../../background/$BROWSER-polyfill.es6')
+
 module.exports = {
   openOptionsPage: function () {
     this.model.fetch({getBrowser: true}).then(browser => {
       if (browser === 'moz') {
         window.chrome.tabs.create({url: window.chrome.extension.getURL('/html/options.html')})
         window.close()
-      } else {
+      } 
+      else if (browser === 'chrome') {
         window.chrome.runtime.openOptionsPage()
+      }
+      else if (browser === 'safari') {
+          let tab = safari.application.activeBrowserWindow.openTab()
+          tab.url = wrapper.getExtensionURL('html/options.html')
       }
     })
   }
