ba3864d2d0fd08e9a6bed4f3708122ee9f005d3b
Zaahir Moolla <moollaza@duckduckgo.com>
2018-04-17 14:37:49 -0400

    Add unit test for new _canShowPostInstall fn

---
+++ unit-test/background/atb.es6.js
@@ -0,0 +1,31 @@
+const atb = require('../../shared/js/background/atb.es6')
+const settings = require('../../shared/js/background/settings.es6')
+
+const canShowAtbCases = [
+    {
+        "domain": "duckduckgo.com/software",
+        "result": false
+    }, {
+        "domain": "duckduckgo.com/app",
+        "result": false
+    }, {
+        "domain": "duckduckgo.com/about",
+        "result": true
+    }, {
+        "domain": "duckduckgo.com",
+        "result": true
+    }
+]
+
+describe('atb._canShowPostInstall()', () => {
+    canShowAtbCases.forEach((test) => {
+        it(`should return ${test.result} when the domain is: '${test.domain}'`, () => {
+
+            // ensure settings.getSettings('hasSeenPostInstall') == false
+            spyOn(settings, 'getSetting').and.returnValue(false);
+
+            const result = atb._canShowPostInstall(test.domain);
+            expect(result).toBe(test.result)
+        })
+    })
+});
