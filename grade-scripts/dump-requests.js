061f26a238b7e5c3abb108d790a484bc2b2af5e0
Andrey Pissantchev <andrey@duckduckgo.com>
2018-04-19 08:40:14 -0400

    get grade dumping script to work as per spec

--- grade-scripts/dump-requests.js
+++ grade-scripts/dump-requests.js
@@ -1,17 +1,21 @@
 const puppeteer = require('puppeteer')
 const listManager = require('./shared/list-manager')
+const program = require('commander')
+const fs = require('fs')
+const execSync = require('child_process').execSync
+const chalk = require('chalk')
 
-const Grade = require('../src/classes/grade')
 const https = require('../src/https')
 const trackers = require('../src/trackers')
 const surrogates = require('../src/surrogates')
 
-let grade
+let browser
 
-let siteToCheck = 'reddit.com'
-
-const handleRequest = (request) => {
+const handleRequest = (requests, siteToCheck, request) => {
     let url = request.url()
+    let type = request.resourceType()
+
+    requests.push([url, type])
 
     let upgradedUrl = https.getUpgradedUrl(url)
 
@@ -19,12 +23,9 @@ const handleRequest = (request) => {
         url = upgradedUrl
     }
 
-    // fake some tracker blocking for now
-    let tracker = trackers.isTracker(url, siteToCheck, url.resourceType)
+    let tracker = trackers.isTracker(url, siteToCheck, type)
 
     if (tracker && tracker.block) {
-        grade.update({ trackerBlocked: tracker })
-
         if (tracker.redirectUrl) {
             url = tracker.redirectUrl
         } else {
@@ -38,7 +39,7 @@ const handleRequest = (request) => {
     })
 }
 
-(async () => {
+const setup = async () => {
     // load any lists and plug them into any classes that wait for them
     await listManager.loadLists()
     https.init(listManager.getList('https'))
@@ -49,27 +50,75 @@ const handleRequest = (request) => {
     surrogates.init(listManager.getList('surrogates'))
 
     // set up headless browser
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
         args: ['--no-sandbox']
     })
+
+    execSync(`mkdir -p sites`)
+}
+
+const teardown = async () => {
+    await browser.close()
+}
+
+const getSiteData = async (siteToCheck) => {
     const page = await browser.newPage()
+    const url = `http://${siteToCheck}`
+    let requests = []
 
     await page.setRequestInterception(true)
-    page.on('request', handleRequest)
+    page.on('request', handleRequest.bind(null, requests, siteToCheck))
 
-    // visit page!
-    grade = new Grade('', siteToCheck)
     // wait for the page to load and then an extra 3s, to be sure
-    await page.goto(`http://${siteToCheck}`, { timeout: 10000, waitUntil: 'load' })
+    await page.goto(url, { timeout: 10000, waitUntil: 'load' })
     await page.waitFor(3000)
 
-    // check if https
-    if (page.url().indexOf('https:') === 0) {
-        grade.update({ hasHttps: true })
+    return { url, requests }
+}
+
+const run = async (filename) => {
+    let sites
+
+    try {
+        sites = fs.readFileSync(filename, { encoding: 'utf8' }).trim().split('\n')
+    } catch (e) {
+        console.log(chalk.red(`Error getting sites from file ${filename}: ${e.message}`))
+        return
     }
 
-    console.log(`grade is: ${JSON.stringify(grade.get())}`)
-    console.log(`number of trackers blocked: ${grade.totalBlocked}`)
+    await setup()
 
-    await browser.close()
-})()
+    for (let siteToCheck of sites) {
+        let path = `sites/${siteToCheck}.json`
+        let fileExists
+
+        try {
+            fileExists = fs.existsSync(path)
+        } catch (e) {
+            // ¯\_(ツ)_/¯
+        }
+
+        if (fileExists) {
+            console.log(`dump file exists for ${siteToCheck}, skipping`)
+            continue
+        }
+
+        let data = await getSiteData(siteToCheck)
+
+        if (!data.requests.length) {
+            console.log(chalk.red(`couldn't get requests for ${siteToCheck}`))
+        }
+
+        console.log(chalk.green(`got ${data.requests.length} requests for ${siteToCheck}`))
+
+        fs.writeFileSync(`sites/${siteToCheck}.json`, JSON.stringify(data))
+    }
+
+    await teardown()
+}
+
+program
+    .arguments('<filename>')
+    .action(run)
+    .parse(process.argv)
+
