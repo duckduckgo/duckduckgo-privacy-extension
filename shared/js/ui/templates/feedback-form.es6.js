00e351f5a9d7b088251cd38ff73096b3de2a5d88
Andrey Pissantchev <pissantchev@gmail.com>
2018-05-22 18:39:00 +0100

    styling

--- shared/js/ui/templates/feedback-form.es6.js
+++ shared/js/ui/templates/feedback-form.es6.js
@@ -5,21 +5,25 @@ module.exports = function () {
 
     if (this.model.isBrokenSite) {
         fields = bel`<div>
-            <p>Which website is broken?</p>
+            <label class='frm__label'>Which website is broken?</label>
             <input class='frm__input' type='text' placeholder='Please copy and paste your URL'/>
-            <p>Describe the issue</p>
+            <label class='frm__label'>Describe the issue</label>
             <textarea class='frm__text'></textarea>
         </div>`
     } else {
         fields = bel`<div>
-            <p>Tell us which features or functionality your feedback refers to. What do you love? What isn't working? How could it be improved?</p>
+            <label class='frm__label'>Tell us which features or functionality your feedback refers to. What do you love? What isn't working? How could it be improved?</label>
             <textarea class='frm__text'></textarea>
         </div>`
     }
 
     return bel`<form class='frm'>
-        <p>Help us improve by sharing a little info about the issue you've encountered.</p>
+        <p class='frm__label'>Help us improve by sharing a little info about the issue you've encountered.</p>
+        <label class='frm__label'>
+            <input type='checkbox' class='frm__label__chk' checked='checked'/>
+            I want to report a broken site
+        </label>
         ${fields}
-        <input class='btn' type='submit' value='submit'/>
+        <input class='btn' type='submit' value='Submit'/>
     </form>`
 }
