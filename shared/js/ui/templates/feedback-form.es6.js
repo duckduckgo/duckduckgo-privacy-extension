526fda3e1afff9ffa78a16fdca44b816e222f15c
Andrey Pissantchev <pissantchev@gmail.com>
2018-05-22 15:42:12 +0100

    port over some styling from the SERP

--- shared/js/ui/templates/feedback-form.es6.js
+++ shared/js/ui/templates/feedback-form.es6.js
@@ -6,20 +6,20 @@ module.exports = function () {
     if (this.model.isBrokenSite) {
         fields = bel`<div>
             <p>Which website is broken?</p>
-            <input type='test' placeholder='Please copy and paste your URL'/>
+            <input class='frm__input' type='text' placeholder='Please copy and paste your URL'/>
             <p>Describe the issue</p>
-            <textarea></textarea>
+            <textarea class='frm__text'></textarea>
         </div>`
     } else {
         fields = bel`<div>
             <p>Tell us which features or functionality your feedback refers to. What do you love? What isn't working? How could it be improved?</p>
-            <textarea></textarea>
+            <textarea class='frm__text'></textarea>
         </div>`
     }
 
-    return bel`<form>
+    return bel`<form class='frm'>
         <p>Help us improve by sharing a little info about the issue you've encountered.</p>
         ${fields}
-        <input type='submit' value='submit'/>
+        <input class='btn' type='submit' value='submit'/>
     </form>`
 }
