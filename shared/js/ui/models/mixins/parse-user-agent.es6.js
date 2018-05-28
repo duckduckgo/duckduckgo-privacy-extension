c1d2e1e36b16058f1eafdfed248d48d26f9878b0
Andrey Pissantchev <pissantchev@gmail.com>
2018-05-28 19:15:02 +0100

    only return first version number

--- shared/js/ui/models/mixins/parse-user-agent.es6.js
+++ shared/js/ui/models/mixins/parse-user-agent.es6.js
@@ -3,7 +3,7 @@ const browserUIWrapper = require('./../../base/$BROWSER-ui-wrapper.es6.js')
 module.exports = {
     parseUserAgentString (uaString) {
         if (!uaString) uaString = window.navigator.userAgent
-        const rgx = uaString.match(/(Firefox|Chrome|Safari)\/([0-9.]+)/)
+        const rgx = uaString.match(/(Firefox|Chrome|Safari)\/([0-9]+)/)
         const extensionVersion = browserUIWrapper.getExtensionVersion()
         return {
             browser: rgx[1],
