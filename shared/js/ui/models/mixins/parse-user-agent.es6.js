478277672a51910b76952a358e7cdf201411871e
Andrey Pissantchev <pissantchev@gmail.com>
2018-05-28 19:21:00 +0100

    parse Safari versions correctly, harden in case of errors

--- shared/js/ui/models/mixins/parse-user-agent.es6.js
+++ shared/js/ui/models/mixins/parse-user-agent.es6.js
@@ -3,11 +3,27 @@ const browserUIWrapper = require('./../../base/$BROWSER-ui-wrapper.es6.js')
 module.exports = {
     parseUserAgentString (uaString) {
         if (!uaString) uaString = window.navigator.userAgent
-        const rgx = uaString.match(/(Firefox|Chrome|Safari)\/([0-9]+)/)
+
+        let browser
+        let version
+
+        try {
+            const rgx = uaString.match(/(Firefox|Chrome|Safari)\/([0-9]+)/)
+            browser = rgx[1]
+            version = rgx[2]
+
+            if (browser === 'Safari') {
+                version = uaString.match(/Version\/(\d+)/)[1]
+            }
+        } catch (e) {
+            // unlikely, prevent extension from exploding if we don't recognize the UA
+            browser = version = ''
+        }
+
         const extensionVersion = browserUIWrapper.getExtensionVersion()
         return {
-            browser: rgx[1],
-            version: rgx[2],
+            browser: browser,
+            version: version,
             extension: extensionVersion
         }
     }
