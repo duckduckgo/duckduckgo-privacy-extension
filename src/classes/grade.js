3028a31bbc6a945bdffec06a98dd3b73409047ba
Andrey Pissantchev <pissantchev@gmail.com>
2018-06-12 13:02:06 +0100

    add new grade algo with tests

--- src/classes/grade.js
+++ src/classes/grade.js
@@ -1,250 +1,158 @@
-const utils = require('../utils')
-
-const tosdr = require('../../data/generated/tosdr')
-const tosdrMessages = require('../../data/tosdr-messages')
-const majorTrackingNetworks = require('../../data/major-tracking-networks')
-
-const tosdrRegexList = Object.keys(tosdr).map(x => new RegExp(`(^)${utils.getDomain(x)}`)) // only match domains, and from the start of the URL
-const tosdrClassMap = {'A': -1, 'B': 0, 'C': 0, 'D': 1, 'E': 2} // map tosdr class rankings to increase/decrease in grade
-const siteScores = ['A', 'B', 'C', 'D']
+const UNKNOWN_PRIVACY_SCORE = 2
 
 class Grade {
-    constructor (domain, specialPage) {
-        this.specialPage = specialPage // see specialDomain() in class Site below
-        this.hasHTTPS = false
-        this.inMajorTrackingNetwork = false
-        this.totalBlocked = 0
-        this.decisions = []
-        this.hasObscureTracker = false
-        this.domain = utils.getDomain(domain) // strip the subdomain. Fixes matching tosdr for eg encrypted.google.com
-        this.isaMajorTrackingNetwork = this.isaMajorTrackingNetwork()
-        this.tosdr = this.getTosdr()
-        this.trackersByUrl = {}
-    }
+    constructor (attrs) {
+        // defaults
+        this.hasHttps = false
+        this.isAutoUpgradeable = false
+        this.privacyScore = UNKNOWN_PRIVACY_SCORE // unknown
 
-    getTosdr () {
-        let result = {}
+        this.companiesBlocked = {}
+        this.companiesNotBlocked = {}
 
-        tosdrRegexList.some(tosdrSite => {
-            let match = tosdrSite.exec(this.domain)
-            if (match) {
-                // remove period at end for lookup in majorTrackingNetworks
-                let tosdrData = tosdr[match[0]]
-
-                if (!tosdrData) return
-
-                const matchGood = (tosdrData.match && tosdrData.match.good) || []
-                const matchBad = (tosdrData.match && tosdrData.match.bad) || []
-
-                // tosdr message
-                // 1. If we have a defined tosdr class look up the tosdr message
-                //    for the corresponding letter class
-                // 2. If there are both good and bad points -> 'mixed'
-                // 3. Else use the calculated tosdr score to determine the message
-                let message = tosdrMessages.unknown
-                if (tosdrData.class) {
-                    message = tosdrMessages[tosdrData.class]
-                } else if (matchGood.length && matchBad.length) {
-                    message = tosdrMessages.mixed
-                } else {
-                    if (tosdrData.score < 0) {
-                        message = tosdrMessages.good
-                    } else if (tosdrData.score === 0 && (matchGood.length || matchBad.length)) {
-                        message = tosdrMessages.mixed
-                    } else if (tosdrData.score > 0) {
-                        message = tosdrMessages.bad
-                    }
-                }
-
-                result = {
-                    score: tosdrData.score,
-                    class: tosdrData.class,
-                    reasons: {
-                        good: matchGood,
-                        bad: matchBad
-                    },
-                    message: message
-                }
-
-                return result
-            }
-        })
-        return result
-    }
-
-    /* is the parent site itself a major tracking network?
-     * minus one grade for each 10% of the top pages this
-     * network is found on.
-     */
-    isaMajorTrackingNetwork () {
-        let result = 0
-        if (this.specialPage || !this.domain) return result
-        const parentCompany = utils.findParent(this.domain.split('.'))
-        if (!parentCompany) return result
-        const isMajorNetwork = majorTrackingNetworks[parentCompany.toLowerCase()]
-        if (isMajorNetwork) {
-            result = Math.ceil(isMajorNetwork / 10)
+        this.scores = {
+            site: {},
+            enhanced: {}
         }
-        return result
     }
 
-    /*
-     * Calculates and returns a site score
-     */
+    setHttps (hasHttps, isAutoUpgradeable) {
+        // isAutoUpgradeable implies the site has https
+        // make sure hasHttps is correct in case of
+        // e.g. a difference between the two lists
+        this.hasHttps = hasHttps || isAutoUpgradeable
+        this.isAutoUpgradeable = isAutoUpgradeable
+    }
+
+    setPrivacyScore (score) {
+        this.privacyScore = typeof score === 'number' ? score : UNKNOWN_PRIVACY_SCORE
+    }
+
+    addTracker (tracker) {
+        let parentCompany = tracker.parentCompany
+
+        if (!parentCompany) return
+
+        if (tracker.blocked) {
+            this.companiesBlocked[parentCompany] = tracker.prevalence || 1
+        } else {
+            this.companiesNotBlocked[parentCompany] = tracker.prevalence || 1
+        }
+    }
+
+    setParentCompany (name, prevalence) {
+        if (!name || !prevalence) return
+
+        this.companiesNotBlocked[name] = prevalence
+    }
+
+    calculate () {
+        // HTTPS
+        let siteHttpsScore, enhancedHttpsScore
+
+        if (this.hasHttps && this.isAutoUpgradeable) {
+            siteHttpsScore = 0
+            enhancedHttpsScore = 0
+        } else if (this.hasHttps) {
+            siteHttpsScore = 3
+            enhancedHttpsScore = 0
+        } else {
+            siteHttpsScore = 10
+            enhancedHttpsScore = 10
+        }
+
+        // PRIVACY
+        // clamp to 10
+        let privacyScore = Math.min(this.privacyScore, 10)
+
+        // TRACKERS
+        let siteTrackerScore = 0
+        let enhancedTrackerScore = 0
+
+        for (let company in this.companiesBlocked) {
+            siteTrackerScore += this.normalizeTrackerScore(this.companiesBlocked[company])
+        }
+
+        for (let company in this.companiesNotBlocked) {
+            siteTrackerScore += this.normalizeTrackerScore(this.companiesNotBlocked[company])
+            enhancedTrackerScore += this.normalizeTrackerScore(this.companiesNotBlocked[company])
+        }
+
+        let siteTotalScore = siteHttpsScore + siteTrackerScore + privacyScore
+        let enhancedTotalScore = enhancedHttpsScore + enhancedTrackerScore + privacyScore
+
+        this.scores = {
+            site: {
+                grade: this.scoreToGrade(siteTotalScore),
+                score: siteTotalScore,
+                trackerScore: siteTrackerScore,
+                httpsScore: siteHttpsScore,
+                privacyScore: privacyScore
+            },
+            enhanced: {
+                grade: this.scoreToGrade(enhancedTotalScore),
+                score: enhancedTotalScore,
+                trackerScore: enhancedTrackerScore,
+                httpsScore: enhancedHttpsScore,
+                privacyScore: privacyScore
+            }
+        }
+    }
+
+    normalizeTrackerScore (pct) {
+        let score
+
+        if (!pct) {
+            score = 0
+        } else if (pct < 0.1) {
+            score = 1
+        } else if (pct < 1) {
+            score = 2
+        } else if (pct < 5) {
+            score = 3
+        } else if (pct < 10) {
+            score = 4
+        } else if (pct < 15) {
+            score = 5
+        } else if (pct < 20) {
+            score = 6
+        } else if (pct < 30) {
+            score = 7
+        } else if (pct < 45) {
+            score = 8
+        } else if (pct < 66) {
+            score = 9
+        } else {
+            score = 10
+        }
+
+        return score
+    }
+
+    scoreToGrade (score) {
+        let grade
+
+        if (score < 2) {
+            grade = 'A'
+        } else if (score < 4) {
+            grade = 'B+'
+        } else if (score < 10) {
+            grade = 'B'
+        } else if (score < 14) {
+            grade = 'C+'
+        } else if (score < 20) {
+            grade = 'C'
+        } else if (score < 30) {
+            grade = 'D'
+        } else {
+            grade = 'D-'
+        }
+
+        return grade
+    }
+
     get () {
-        if (this.specialPage) return {}
-
-        this.decisions = []
-
-        let beforeIndex = 1
-        let afterIndex = 1
-
-        this.addDecision({change: 1, index: beforeIndex, why: 'Default grade'})
-
-        if (this.isaMajorTrackingNetwork) {
-            beforeIndex += this.isaMajorTrackingNetwork
-            afterIndex += this.isaMajorTrackingNetwork
-            this.addDecision({
-                change: this.isaMajorTrackingNetwork,
-                index: beforeIndex,
-                why: 'Is a major tracking network'
-            })
-        } else {
-            this.addDecision({ change: 0, index: beforeIndex, why: 'Not a major tracking network' })
-        }
-
-        // If tosdr already determined a class ranking then we map that to increase or
-        // decrease the grade accordingly. Otherwise we apply a +/- to the grade based
-        // on the cumulative total of all the points we care about. see: scripts/tosdr-topics.json
-        if (this.tosdr) {
-            if (this.tosdr.class) {
-                beforeIndex += tosdrClassMap[this.tosdr.class]
-                afterIndex += tosdrClassMap[this.tosdr.class]
-
-                this.addDecision({
-                    change: tosdrClassMap[this.tosdr.class],
-                    index: beforeIndex,
-                    why: `Has tosdr class ${this.tosdr.class}`,
-                    tosdr: this.tosdr
-                })
-            } else if (this.tosdr.score) {
-                let tosdrScore = Math.sign(this.tosdr.score)
-                beforeIndex += tosdrScore
-                afterIndex += tosdrScore
-
-                this.addDecision({
-                    change: tosdrScore,
-                    index: beforeIndex,
-                    why: `Has tosdr score ${tosdrScore}`,
-                    tosdr: this.tosdr
-                })
-            } else {
-                this.addDecision({ change: 0, index: beforeIndex, why: `No tosdr` })
-            }
-        }
-
-        if (this.inMajorTrackingNetwork) {
-            beforeIndex++
-            this.addDecision({ change: 1, index: beforeIndex, why: `In major tracking network` })
-        } else {
-            this.addDecision({ change: 0, index: beforeIndex, why: `Not in major tracking network` })
-        }
-
-        if (!this.hasHTTPS) {
-            beforeIndex++
-            afterIndex++
-            this.addDecision({ change: 1, index: beforeIndex, why: `No HTTPS` })
-        } else {
-            this.addDecision({ change: 0, index: beforeIndex, why: `Has HTTPS` })
-        }
-
-        if (this.hasObscureTracker) {
-            beforeIndex++
-            this.addDecision({ change: 1, index: beforeIndex, why: `Has obscure tracker` })
-        } else {
-            this.addDecision({ change: 0, index: beforeIndex, why: `No obscure tracker` })
-        }
-
-        // decrease score for every 10, round up
-        let trackerPenalty = Math.ceil(this.totalBlocked / 10)
-        beforeIndex += trackerPenalty
-
-        this.addDecision({ change: trackerPenalty, index: beforeIndex, why: `${this.totalBlocked} trackers blocked` })
-
-        // negative scoreIndex should return the highest score
-        if (beforeIndex < 0) {
-            let diff = 0 - beforeIndex
-            beforeIndex = 0
-            this.addDecision({
-                change: diff,
-                index: beforeIndex,
-                why: `Don't allow negative score`
-            })
-        }
-        if (afterIndex < 0) afterIndex = 0
-
-        // only sites with a tosdr.class "A" can get a final grade of "A"
-        if (afterIndex === 0 && this.tosdr.class !== 'A') afterIndex = 1
-        if (beforeIndex === 0 && this.tosdr.class !== 'A') {
-            beforeIndex = 1
-
-            this.addDecision({
-                change: 1,
-                index: beforeIndex,
-                why: `Grade 'A' requires TOSDR class 'A'. this.tosdr.class: ${this.tosdr.class}`
-            })
-        }
-
-        // return corresponding score or lowest score if outside the array
-        let beforeGrade = siteScores[beforeIndex] || siteScores[siteScores.length - 1]
-        let afterGrade = siteScores[afterIndex] || siteScores[siteScores.length - 1]
-
-        this.addDecision({
-            change: 0,
-            index: beforeIndex,
-            why: `final grade ${beforeGrade}`
-        })
-
-        return {
-            before: beforeGrade,
-            beforeIndex: beforeIndex,
-            after: afterGrade,
-            afterIndex: afterIndex
-        }
-    }
-
-    /*
-     * Update the score attruibues as new events come in. The actual
-     * site score is calculated later when you call .get()
-     */
-    update (event) {
-        let IPRegex = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/
-
-        if (event.hasHTTPS) {
-            this.hasHTTPS = true
-        } else if (event.trackerBlocked) {
-            let tracker = event.trackerBlocked
-            if (this.trackersByUrl[tracker.url]) { return }
-
-            this.trackersByUrl[tracker.url] = true
-
-            // tracker is from one of the top blocked companies
-            if (majorTrackingNetworks[tracker.parentCompany.toLowerCase()]) {
-                this.inMajorTrackingNetwork = true
-            }
-
-            // trackers with IP address
-            if (tracker.url.match(IPRegex)) {
-                this.hasObscureTracker = true
-            }
-
-            this.totalBlocked++
-        }
-    }
-
-    addDecision (decision) {
-        decision.grade = siteScores[decision.index] || 'D'
-        this.decisions.push(decision)
+        return this.scores
     }
 }
 
--- test/classes/grade.js
+++ test/classes/grade.js
@@ -1,177 +1,29 @@
-const utils = require('../../src/utils')
 const Grade = require('../../src/classes/grade')
-const gradeTestCases = require('../data/grade-cases')
-const isaMajorNetworkTestCases = [
-    {
-        'specialPage': 0,
-        'domain': 'facebook.com',
-        'slicedDomain': ['facebook', 'com'],
-        'parent': 'Facebook',
-        'isZeroResult': 0,
-        'descr': 'a number greater than zero for Facebook (major network)'
-    },
-    {
-        'specialPage': 0,
-        'domain': 'encrypted.google.com',
-        'slicedDomain': ['google', 'com'],
-        'parent': 'Google',
-        'isZeroResult': 0,
-        'descr': 'a number greater than zero for Google (major network)'
-    },
-    {
-        'specialPage': 0,
-        'domain': '',
-        'slicedDomain': [],
-        'parent': '',
-        'isZeroResult': 1,
-        'descr': 'zero, because we have no domain'
-    },
-    {
-        'specialPage': 1,
-        'domain': '',
-        'slicedDomain': [],
-        'parent': '',
-        'isZeroResult': 1,
-        'descr': "zero, because it's a special page"
-    },
-    {
-        'specialPage': 0,
-        'domain': 'duckduckgo.com',
-        'slicedDomain': ['duckduckgo', 'com'],
-        'parent': '',
-        'isZeroResult': 1,
-        'descr': 'zero, because of no major network parent'
-    },
-    {
-        'specialPage': 0,
-        'domain': 'bttf.duckduckgo.com',
-        'slicedDomain': ['duckduckgo', 'com'],
-        'parent': '',
-        'isZeroResult': 1,
-        'descr': 'zero, because of no major network parent'
-    }
-]
-
-const tosdrTestCases = [
-    {
-        'domain': 'google.com',
-        'isMessageBad': 1,
-        'descr': 'bad tosdr rating for google.com'
-    },
-    {
-        'domain': 'encrypted.google.com',
-        'isMessageBad': 1,
-        'descr': 'bad tosdr rating for encrypted.google.com (match domain)'
-    },
-    {
-        'domain': 'youtube.com',
-        'isMessageBad': 1,
-        'descr': 'bad tosdr rating for youtube.com'
-    },
-    {
-        'domain': 'duckduckgo.com',
-        'isMessageBad': 0,
-        'descr': 'good tosdr rating for duckduckgo.com'
-    },
-    {
-        'domain': 'bttf.duckduckgo.com',
-        'isMessageBad': 0,
-        'descr': 'good tosdr rating for bttf.duckduckgo.com'
-    },
-    {
-        'domain': 'deletefacebook.com',
-        'isMessageBad': 0,
-        'descr': 'not bad tosdr rating for deletefacebook.com'
-    }
-]
+const tests = require('../data/grade-cases')
 
 let grade
 
-describe('grade', () => {
+describe('example grades', () => {
     beforeEach(() => {
         grade = new Grade()
     })
 
-    gradeTestCases.forEach((test) => {
-        it(`should return the correct grades for: ${test.descr}`, () => {
-            Object.keys(test.values).forEach((prop) => {
-                grade[prop] = test.values[prop]
+    tests.forEach((test) => {
+        it(`should calculate the correct grade for ${test.url}`, () => {
+            grade.setHttps(test.input.https, test.input.httpsAutoUpgrade)
+            grade.setPrivacyScore(test.input.privacyScore)
+            grade.setParentCompany(test.input.parentCompany, test.input.parentTrackerPrevalence)
+
+            test.input.trackers.forEach((tracker) => {
+                grade.addTracker(tracker)
             })
 
-            let result = grade.get()
+            grade.calculate()
 
-            expect(result.before).toEqual(test.result.before)
-            expect(result.after).toEqual(test.result.after)
-            expect(result.beforeIndex).toEqual(test.result.beforeScore)
-            expect(result.afterIndex).toEqual(test.result.afterScore)
-        })
-    })
-})
+            let gradeData = grade.get()
 
-describe('isaMajorNetwork', () => {
-    isaMajorNetworkTestCases.forEach((test) => {
-        it(`should return ${test.descr}`, () => {
-            spyOn(utils, 'findParent').and.returnValue(test.parent)
-            grade = new Grade(test.domain, test.specialPage)
-
-            if (test.parent) {
-                expect(utils.findParent).toHaveBeenCalledWith(test.slicedDomain)
-            }
-
-            let result = grade.isaMajorTrackingNetwork
-
-            if (test.isZeroResult) {
-                expect(result).toEqual(0)
-            } else {
-                expect(result).toBeGreaterThan(0)
-            }
-        })
-    })
-})
-
-describe('getTosdr', () => {
-    tosdrTestCases.forEach((test) => {
-        it(`should return ${test.descr}`, () => {
-            grade = new Grade(test.domain)
-
-            let result = grade.tosdr
-            let message = result.message
-
-            if (test.isMessageBad) {
-                expect(message).toEqual('Bad')
-            } else {
-                expect(message).not.toEqual('Bad')
-            }
-        })
-    })
-})
-
-// piggy back on the existing grade test cases, and make sure decisions are kept track of properly
-describe('decisions', () => {
-    // re-use instance of the grade class so we can validate grades are
-    // getting cleared between invocations of get()
-    grade = new Grade()
-    gradeTestCases.forEach((test) => {
-        it(`should get decisions correctly for case: ${test.grade}`, () => {
-            Object.keys(test.values).forEach((prop) => {
-                grade[prop] = test.values[prop]
-            })
-
-            let result = grade.get()
-
-            let decisions = grade.decisions
-            let currentIndex = 0
-
-            expect(decisions instanceof Array).toBeTruthy()
-            decisions.forEach((decision) => {
-                expect(typeof decision.why).toBe('string', 'every decision should have a reason')
-                expect(decision.grade).toMatch(/^[A-F]$/, 'every decision should include the grade')
-
-                currentIndex += decision.change
-                expect(currentIndex).toEqual(decision.index, 'the decision index should match what the change says')
-            })
-
-            expect(currentIndex).toEqual(result.beforeIndex, 'all the changes in the decisions should match the final grade reported')
+            expect(gradeData.site).toEqual(test.expected.site, 'site grade should be correct')
+            expect(gradeData.enhanced).toEqual(test.expected.enhanced, 'enhanced grade should be correct')
         })
     })
 })
