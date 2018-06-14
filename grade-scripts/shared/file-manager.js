1db3c2bee1b35d741ae128c2525838ed9f54b409
Andrey Pissantchev <pissantchev@gmail.com>
2018-06-14 16:47:41 +0100

    calculate tracker prevalence after tracking step finishes

--- grade-scripts/shared/file-manager.js
+++ grade-scripts/shared/file-manager.js
@@ -4,7 +4,7 @@
 
 const fs = require('fs')
 
-const getSiteData = (inputPath, outputPath, fileForSubset) => {
+const getSiteData = (inputPath, outputPath, fileForSubset, skipExistingFiles) => {
     // get initial file data
     let siteDataFiles = fs.readdirSync(inputPath)
 
@@ -22,24 +22,26 @@ const getSiteData = (inputPath, outputPath, fileForSubset) => {
     }
 
     // skip files that exist
-    siteDataFiles = siteDataFiles.filter((fileName) => {
-        let destPath = `${outputPath}/${fileName}`
-        let hostname = fileName.replace(/\.json$/, '')
-        let fileExists
+    if (skipExistingFiles && outputPath) {
+        siteDataFiles = siteDataFiles.filter((fileName) => {
+            let destPath = `${outputPath}/${fileName}`
+            let hostname = fileName.replace(/\.json$/, '')
+            let fileExists
 
-        try {
-            fileExists = fs.existsSync(destPath)
-        } catch (e) {
-            // ¯\_(ツ)_/¯
-        }
+            try {
+                fileExists = fs.existsSync(destPath)
+            } catch (e) {
+                // ¯\_(ツ)_/¯
+            }
 
-        if (fileExists) {
-            console.log(`grade file exists for ${hostname}, skipping`)
-            return false
-        }
+            if (fileExists) {
+                console.log(`grade file exists for ${hostname}, skipping`)
+                return false
+            }
 
-        return true
-    })
+            return true
+        })
+    }
 
     let siteData = siteDataFiles.map(fileName => require(`${process.cwd()}/${inputPath}/${fileName}`))
 
--- grade-scripts/trackers-from-requests.js
+++ grade-scripts/trackers-from-requests.js
@@ -3,7 +3,7 @@ const program = require('commander')
 const fs = require('fs')
 const execSync = require('child_process').execSync
 const chalk = require('chalk')
-const fileManager = require('shared/file-manager')
+const fileManager = require('./shared/file-manager')
 
 const trackers = require('../src/trackers')
 const surrogates = require('../src/surrogates')
@@ -15,7 +15,7 @@ program
     .parse(process.argv)
 
 const input = program.input
-const inputPath = `${input}-requests`
+const inputPath = `${input}-sites`
 const output = program.output
 const outputPath = `${output}-trackers`
 const fileForSubset = program.file
@@ -32,7 +32,7 @@ const run = async () => {
     execSync(`mkdir -p ${outputPath}`)
 
     // get initial file data
-    let siteDataArray = fileManager.getSiteData(inputPath, outputPath, fileForSubset)
+    let siteDataArray = fileManager.getSiteData(inputPath, outputPath, fileForSubset, true)
 
     for (let siteData of siteDataArray) {
         let url = siteData.url
@@ -79,4 +79,75 @@ const run = async () => {
     }
 }
 
+const calculateTrackerPrevalence = () => {
+    console.log('calculating tracker prevalence data...')
+
+    // get all the files that we've just processed
+    let siteDataArray = fileManager.getSiteData(outputPath)
+    let entityList = listManager.getList('entityList')
+
+    let networksSeen = {}
+
+    // count up all the tracker networks seen on each site
+    siteDataArray.forEach((siteData) => {
+        let networksOnThisSite = {}
+
+        // get all the tracker networks seen on a specific site
+        Object.keys(siteData.trackersBlocked).forEach((network) => {
+            Object.keys(siteData.trackersBlocked[network]).forEach((trackerUrl) => {
+                let parent = entityList[trackerUrl] || network
+                networksOnThisSite[parent] = true
+            })
+        })
+        Object.keys(siteData.trackersNotBlocked).forEach((network) => {
+            Object.keys(siteData.trackersNotBlocked[network]).forEach((trackerUrl) => {
+                let parent = entityList[trackerUrl] || network
+                networksOnThisSite[parent] = true
+            })
+        })
+
+        // add them to the global counts
+        Object.keys(networksOnThisSite).forEach((network) => {
+            if (!networksSeen[network]) {
+                networksSeen[network] = 0
+            }
+
+            networksSeen[network] += 1
+        })
+    })
+
+    // calculate overall prevalence
+    let networkPrevalence = {}
+    let totalSites = siteDataArray.length
+
+    Object.keys(networksSeen).forEach((network) => {
+        let percent = networksSeen[network] / totalSites * 100
+        // round to 2 significant digits
+        percent = Math.round(percent * 100) / 100
+
+        networkPrevalence[network] = percent
+    })
+
+    // write the prevalence to file, then add it to all the file data
+    fs.writeFileSync(`prevalence.json`, JSON.stringify(networkPrevalence))
+
+    siteDataArray.forEach((siteData) => {
+        siteData.parentEntity = entityList[siteData.url] || ''
+        siteData.parentTrackerPrevalence = networkPrevalence[siteData.parentEntity] || 0
+
+        Object.keys(siteData.trackersBlocked).forEach((network) => {
+            siteData.trackersBlocked[network].prevalence = networkPrevalence[network]
+        })
+        Object.keys(siteData.trackersNotBlocked).forEach((network) => {
+            siteData.trackersNotBlocked[network].prevalence = networkPrevalence[network]
+        })
+
+        let hostname = siteData.url.replace(/https?:\/\//, '')
+        fs.writeFileSync(`${outputPath}/${hostname}.json`, JSON.stringify(siteData))
+    })
+
+    console.log('done!')
+}
+
 run()
+    .then(calculateTrackerPrevalence)
