5d6f186f5ebd57f3421b101184466954aaa044b8
jdorweiler <jason@duckduckgo.com>
2018-07-12 11:29:11 -0600

    working test

--- unit-test/background/https.es6.js
+++ unit-test/background/https.es6.js
@@ -3,18 +3,28 @@ const https = require('../../shared/js/background/https.es6')
 const httpsStorage = require('../../shared/js/background/storage/https.es6')
 let originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
+let dataFromStorage = []
 
 describe('Https ready', () => {
     beforeAll(() => {
         return new Promise((resolve) => {
             httpsStorage.getLists().then(lists => {
-                https.setLists(lists)
+                dataFromStorage = lists
+                https.setLists(dataFromStorage)
             })
             setTimeout(() => resolve(), 9000)
         })
     })
 
+    it('httpsStorage gets list data', () => {
+        let haveData = !!dataFromStorage.length
+        expect(haveData).toEqual(true)
+    })
+
     describe('https on', () => {
+        beforeEach(() => {
+            https.isReady = true
+        })
 
         it('should be ready to upgrade', () => {
             expect(https.isReady).toEqual(true)
