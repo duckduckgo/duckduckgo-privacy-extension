7953d4e0de9a8cfb718f9d94b529a77bb7ce4d0a
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2018-07-20 12:06:36 +0100

    add comment

--- integration-test/helpers/wait.js
+++ integration-test/helpers/wait.js
@@ -3,6 +3,11 @@ const ms = async (ms) => {
     await new Promise((resolve) => { setTimeout(resolve, ms) })
 }
 
+/**
+ * waitForFunction() uses requestAnimationFrame by default,
+ * but that seems to be broken on background pages
+ * so we poll every 100ms instead
+ */
 const forSetting = async (bgPage, key) => {
     await bgPage.waitForFunction((key) => {
         if (!window.dbg) return
