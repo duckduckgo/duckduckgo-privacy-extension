f8cffdac4b957284d7e0741202b8834de900a43f
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2018-08-02 17:05:16 +0100

    add labels to hero navigation (fixes #252)

--- shared/js/ui/templates/shared/hero.es6.js
+++ shared/js/ui/templates/shared/hero.es6.js
@@ -18,7 +18,11 @@ module.exports = function (ops) {
 function renderOpenOrCloseButton (isCloseButton) {
     const openOrClose = isCloseButton ? 'close' : 'open'
     const arrowIconClass = isCloseButton ? 'icon__arrow--left' : ''
-    return bel`<a href="javascript:void(0)" class="hero__${openOrClose}">
+    return bel`<a href="javascript:void(0)"
+        class="hero__${openOrClose}"
+        role="button"
+        aria-label="${isCloseButton ? 'Go back' : 'More details'}"
+        >
     <span class="icon icon__arrow icon__arrow--large ${arrowIconClass}">
     </span>
 </a>`
