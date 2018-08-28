c199802ba176859dd856307792df5c45f7833be4
jdorweiler <jason@duckduckgo.com>
2018-08-28 09:55:08 -0600

    missing access to localstorage

--- shared/js/ui/pages/popup.es6.js
+++ shared/js/ui/pages/popup.es6.js
@@ -48,16 +48,20 @@ Trackers.prototype = window.$.extend({},
             // 'seenIcon' tells us that the popup is being created from a user click
             // on the icon and not from a rerender. We set a flag to avoid seeing this 
             // message again.
-            if (localStorage['seenIcon']) {
+            if (!localStorage || localStorage['seenIcon']) {
+                // for some reason searches from the popup lose access to localStorage
+                // since the search comes from the popup we can assum the user
+                // saw the alert and just set seenAlert to true in this case
+                let seenAlert = localStorage ? localStorage['seenUpdateAlert'] : true
                 this.views.updateMessage = new UpdateMessageView({
                     pageView: this, 
-                    model: new UpdateMessageModel({seenAlert: localStorage['seenUpdateAlert']}),
+                    model: new UpdateMessageModel({seenAlert: seenAlert}),
                     appendTo: this.$parent,
                     template: updateMessageTemplate
                 })
 
                 // set flag so we don't show alert again
-                localStorage['seenUpdateAlert'] = 1
+                if (localStorage) localStorage['seenUpdateAlert'] = 1
             }
 
             /*
