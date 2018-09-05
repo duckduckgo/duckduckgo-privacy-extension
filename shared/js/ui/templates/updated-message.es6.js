462ac18b00978544c394417196c4bb624781e9da
jdorweiler <jason@duckduckgo.com>
2018-09-05 08:12:43 -0600

    half bold first line

--- shared/js/ui/templates/updated-message.es6.js
+++ shared/js/ui/templates/updated-message.es6.js
@@ -3,8 +3,8 @@ const bel = require('bel')
 module.exports = function (ops) {
     return bel`<section>
         <div class="update-container">
-            <div class="update-message__text--bold">
-                Our Safari extension now only includes DuckDuckGo Private Search.
+            <div class="update-message__text--top">
+                Our Safari extension now <span class="update-message__text--bold">only includes DuckDuckGo Private Search</span>.
             </div>
             <div class="update-message__text">
                 Due to changes in Safari 12, this extension no longer includes a tracker blocker.
