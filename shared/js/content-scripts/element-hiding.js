ed0ab2fa28167ba00e3e00b1135ec2a54e3aa1ba
dharb <harbagio@gmail.com>
2018-09-05 07:18:59 -0700

    use querySelectorAll instead of getElementsByTagName

--- shared/js/content-scripts/element-hiding.js
+++ shared/js/content-scripts/element-hiding.js
@@ -11,7 +11,6 @@
         constructor () {
             // Determine if content script is running in iframe or main frame
             this.frameType = window === window.top ? 'main' : window.parent === window.top ? 'topLevelFrame' : 'nestedFrame'
-            this.foundFrames = []
             this.containsBlockedRequest = false
             this.disabled = false
             this.frameListener = this.frameListener.bind(this)
@@ -46,8 +45,8 @@
                     window.top.postMessage({frameUrl: document.location.href, type: 'frameIdRequest'}, req.mainFrameUrl)
                 }
             } else if (req.type === 'blockedFrame') {
-                this.foundFrames = document.getElementsByTagName('iframe')
-                Array.prototype.forEach.call(this.foundFrames, (frame) => {
+                const foundFrames = document.querySelectorAll('iframe')
+                foundFrames.forEach((frame) => {
                     if (frame.src === req.request.url) {
                         this.collapseDomNode(frame)
                     }
@@ -69,8 +68,8 @@
         frameListener (e) {
             if (this.disabled) return
             if (e.data.type === 'frameIdRequest') {
-                this.foundFrames = document.getElementsByTagName('iframe')
-                Array.prototype.forEach.call(this.foundFrames, (frame) => {
+                const foundFrames = document.querySelectorAll('iframe')
+                foundFrames.forEach((frame) => {
                     if (frame.id && !frame.className.includes('ddg-hidden') && frame.src) {
                         frame.contentWindow.postMessage({frameId: frame.id, mainFrameUrl: document.location.href, type: 'setFrameId'}, '*')
                     }
