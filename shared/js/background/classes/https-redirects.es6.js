3426f0d6c6e1e88030e61e71a2cc22890edfcd1f
MariagraziaAlastra <maria@duckduckgo.com>
2018-09-13 11:54:21 -0400

    anonymous alert when https upgrading fails

--- shared/js/background/classes/https-redirects.es6.js
+++ shared/js/background/classes/https-redirects.es6.js
@@ -2,6 +2,7 @@ const utils = require('../utils.es6')
 
 const MAINFRAME_RESET_MS = 3000
 const REQUEST_REDIRECT_LIMIT = 7
+const pixel = require('./../pixel.es6')
 
 class HttpsRedirects {
     constructor () {
@@ -64,6 +65,7 @@ class HttpsRedirects {
 
         // remember this hostname as previously failed, don't try to upgrade it
         if (!canRedirect) {
+            if (request.type === 'main_frame') pixel.fire('ehd')
             this.failedUpgradeHosts[hostname] = true
             console.log(`HTTPS: not upgrading, redirect loop protection kicked in for url: ${request.url}`)
         }
