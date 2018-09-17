a75c6470316a22ad3b027c9fa28e6197e9fc306b
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2018-09-17 18:45:54 +0100

    harden integration tests

--- integration-test/background/grades.js
+++ integration-test/background/grades.js
@@ -10,6 +10,9 @@ const tests = [
 let browser
 let bgPage
 
+// getting HTTPS can take a while
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
+
 const getGradeByUrl = (url) => {
     const tabsById = dbg.tabManager.tabContainer
     let tab
@@ -23,32 +26,41 @@ const getGradeByUrl = (url) => {
         }
     })
 
-    const gradeData = tab.site.grade.get()
-
-    return {
-        siteGrade: gradeData.site.grade,
-        enhancedGrade: gradeData.enhanced.grade
-    }
+    return tab.site.grade.get()
 }
 
 describe('grade sanity checks', () => {
     beforeAll(async () => {
         ({ browser, bgPage } = await harness.setup())
+
+        // wait for HTTPs to successfully load
+        await bgPage.waitForFunction(
+            () => window.dbg && dbg.https.isReady,
+            { polling: 100, timeout: 60000 }
+        )
     })
     afterAll(async () => {
         await harness.teardown(browser)
     })
 
-    tests.forEach(async (test) => {
+    tests.forEach(test => {
         it(`${test.url} should be graded as a ${test.siteGrade} -> ${test.enhancedGrade}`, async () => {
             const page = await browser.newPage()
+            const ua = await browser.userAgent()
+            await page.setUserAgent(ua.replace(/Headless /, ''))
 
-            await page.goto(`http://${test.url}`, { waitUntil: 'networkidle2' })
+            try {
+                await page.goto(`http://${test.url}`, { waitUntil: 'networkidle2' })
+            } catch (e) {
+                // timed out waiting for page to load, let's try running the test anyway
+            }
+            // give it another second just to be sure
+            await page.waitFor(1000)
 
             const grades = await bgPage.evaluate(getGradeByUrl, test.url)
 
-            expect(grades.siteGrade).toEqual(test.siteGrade)
-            expect(grades.enhancedGrade).toEqual(test.enhancedGrade)
+            expect(grades.site.grade).toEqual(test.siteGrade)
+            expect(grades.enhanced.grade).toEqual(test.enhancedGrade)
 
             await page.close()
         })
