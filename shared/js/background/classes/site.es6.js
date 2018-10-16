693e839eff4ea57e07b31e72642e257dae64a38f
MariagraziaAlastra <maria@duckduckgo.com>
2018-10-16 16:50:03 -0400

    pull out subdomain + comments

--- shared/js/background/classes/site.es6.js
+++ shared/js/background/classes/site.es6.js
@@ -13,6 +13,7 @@ const privacyPractices = require('../privacy-practices.es6')
 const Grade = require('@duckduckgo/privacy-grade').Grade
 const trackerPrevalence = require('../../../data/tracker_lists/prevalence')
 const browserWrapper = require('../$BROWSER-wrapper.es6')
+const tldjs = require('tldjs')
 
 class Site {
     constructor (url) {
@@ -56,6 +57,11 @@ class Site {
 
         if (!trackersWhitelistTemporary) return
 
+        // Match independently of subdomain
+        domain = tldjs.getDomain(domain) || domain
+
+        // Make sure we match at the end of the URL
+        // so we're extra sure it's the legit main domain
         return trackersWhitelistTemporary.some(brokenSiteDomain => brokenSiteDomain.match(new RegExp(domain + '$')))
     }
 
