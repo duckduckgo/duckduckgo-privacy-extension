2ae3eb424b2eca6ccae1aa9500454402f4f0e91b
Jason <jason@Jasons-MacBook-Pro.local>
2019-08-21 10:29:04 -0600

    move tracker algo to npm

--- shared/js/background/trackers.es6.js
+++ shared/js/background/trackers.es6.js
@@ -1,4 +1,4 @@
 const utils = require('./utils.es6')
 const tldjs = require('tldjs')
-const Trackers = require('./trackers-algo.js')
+const Trackers = require('@duckduckgo/privacy-grade').Trackers
 module.exports = new Trackers({tldjs, utils})
