4f6c3557c4df3c95436c325d32f3ca40783abcb1
Konrad Dzwinel <kdzwinel@gmail.com>
2020-01-20 18:51:49 +0100

    Append catch-all https upgrade rule.

--- scripts/mv3/tdsToMV3Rules.js
+++ scripts/mv3/tdsToMV3Rules.js
@@ -6,6 +6,22 @@ const tds = JSON.parse(buff.toString())
 
 const {rules, stats} = transform(tds)
 
+const lastId = rules[rules.length - 1].id
+
+// append catch-all https upgrade rule
+rules.push({
+    id: lastId + 1,
+    priority: 1,
+    condition: {
+        urlFilter: '|http://*',
+        isUrlFilterCaseSensitive: false,
+        resourceTypes: ['main_frame']
+    },
+    action: {
+        type: 'upgradeScheme'
+    }
+})
+
 console.log(stats)
 
 fs.writeFileSync('./data/rules.json', JSON.stringify(rules, null, 2))
