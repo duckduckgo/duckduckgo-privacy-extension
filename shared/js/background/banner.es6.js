2d813c71d5e88f69db5183490cf5ec56dd176081
Zaahir Moolla <moollaza@fastmail.fm>
2020-03-12 14:14:19 -0400

    switch to webNavigation API

--- shared/js/background/banner.es6.js
+++ shared/js/background/banner.es6.js
@@ -1,137 +1,131 @@
+const pixel = require("./pixel.es6");
+
 const filterUrls = {
-    valid: [
-        'https://www.google.com/',
-        'https://www.google.com/search',
-        'https://www.google.com/webhp',
-        'https://www.google.com/videohp',
-        'https://www.google.com/shopping',
-        'https://images.google.com/'
-    ],
-    invalid: [
-        'https://www.google.com/maps'
-    ]
+  valid: [
+    "https://www.google.com/",
+    "https://www.google.com/search",
+    "https://www.google.com/webhp",
+    "https://www.google.com/videohp",
+    "https://www.google.com/shopping",
+    "https://images.google.com/"
+  ],
+  invalid: ["https://www.google.com/maps"]
+};
+
+let updatedTabs = {};
+
+function isValidURL(url) {
+  const urlObj = new URL(url);
+  const href = urlObj.href;
+  // ensure match is at beginning of string
+  return (
+    filterUrls.valid.some(pattern => href.indexOf(pattern) === 0) &&
+    !filterUrls.invalid.some(pattern => href.indexOf(pattern) === 0)
+  );
 }
 
-let updatedTabs = {}
-let tabState = {}
+function isDdgURL(url) {
+  const urlObj = new URL(url);
+  const href = urlObj.href;
+  const hostname = urlObj.hostname;
+  const params = urlObj.searchParams;
+  const hasIaParam = params.has("ia");
 
-function isValidURL (url) {
-    const urlObj = new URL(url)
-    const href = urlObj.href
-    console.log(`Validating URL: ${href}`)
-    // ensure match is at beginning of string
-    return filterUrls.valid.some(pattern => href.indexOf(pattern) === 0) &&
-        !filterUrls.invalid.some(pattern => href.indexOf(pattern) === 0)
-}
-
-function isDdgURL (url) {
-    const urlObj = new URL(url)
-    const href = urlObj.href
-    const hostname = urlObj.hostname
-    console.log(`Validating DDG URL: ${href}`)
-
-    if (hostname !== 'duckduckgo.com') return false
-
-    const params = urlObj.searchParams
-    const hasIaParam = params.has('ia')
-
-    // Ignore static pages, but not DDG cached pages e.g. duckduckgo.com/apple?ia=web
+  if (hostname !== "duckduckgo.com") return false;
+  // Ignore static pages, but not DDG cached pages e.g. duckduckgo.com/apple?ia=web
   if (urlObj.pathname !== "/" && !hasIaParam) return false;
 
-    return true
+  return true;
 }
 
-function resetTab (tabId) {
-    updatedTabs[tabId] = false
+function resetTab(tabId) {
+  updatedTabs[tabId] = false;
 }
 
-function injectAssets (tabId) {
-    // Inject CSS
-    chrome.tabs.insertCSS({
-        file: '/public/css/banner.css',
-        runAt: 'document_start'
+function injectAssets(tabId) {
+  // Inject CSS
+  chrome.tabs.insertCSS(
+    {
+      file: "/public/css/banner.css",
+      runAt: "document_start"
     },
-    function () {
-        console.group('DDG BANNER')
-        console.log(`Tab ${tabId}: CSS injected!`)
-        console.groupEnd()
-    })
+    function() {
+      console.group("DDG BANNER");
+      console.warn(`Tab ${tabId}: CSS injected!`);
+      console.groupEnd();
+    }
+  );
 
-    //  Inject JS
-    chrome.tabs.executeScript({
-        file: '/public/js/content-scripts/banner.js',
-        runAt: 'document_start'
+  //  Inject JS
+  chrome.tabs.executeScript(
+    {
+      file: "/public/js/content-scripts/banner.js",
+      runAt: "document_start"
     },
-    function (array) {
-        console.group('DDG BANNER')
-        console.log(`Tab ${tabId}: Content Script injected!`)
-        console.groupEnd()
-    })
+    function(array) {
+      console.group("DDG BANNER");
+      console.warn(`Tab ${tabId}: Content Script injected!`);
+      console.groupEnd();
+    }
+  );
 }
 
 // Check if we can show the banner
-function canShowBanner (url) {
-    if (isValidURL(url)) {
-        console.log('✅ URL IS VALID')
-        return true
-    } else if (isDdgURL(url)) {
-        console.log('🦆 URL IS DDG URL')
-        return false
-    } else {
-        console.log('❌ URL IS NOT VALID')
-        return false
-    }
+function canShowBanner(url) {
+  console.warn("ℹ️ Checking URL: ", url);
+  let isValid = false;
+  if (isValidURL(url)) {
+    console.warn("✅ URL IS VALID");
+    isValid = true;
+  } else if (isDdgURL(url)) {
+    console.warn("🦆 URL IS DDG URL");
+  } else {
+    console.warn("❌ URL IS NOT VALID");
+  }
+  return isValid;
 }
 
-function createBanner (tabId, changeInfo, tabInfo) {
-    const url = changeInfo.url || tabInfo.url || false
-    // if (!updatedTabs[tabId]) resetTab(tabId)
-
-    // Ignore page if url is invalid
-    if (!canShowBanner(url)) {
-        console.groupEnd()
-        return
-    }
-
-    // Check if tab is loading a URL
-    if (changeInfo.status && changeInfo.status === 'loading') {
-        // reset updated status as this is a fresh page load
-        resetTab(tabId)
-    }
-
-    if (changeInfo.status && changeInfo.status === 'complete') {
-        injectAssets(tabId)
-        // prevent injecting more than once
-        updatedTabs[tabId] = true
-    }
-    console.groupEnd()
+function createBanner(tabId) {
+  injectAssets(tabId);
+  // prevent injecting more than once
+  updatedTabs[tabId] = true;
 }
 
-function handleUpdated (tabId, changeInfo, tabInfo) {
-    console.group('DDG BANNER')
+function handleUpdated(details) {
+  console.group("DDG BANNER");
+  const { url, tabId } = details;
+
+  console.warn(`🔔 Updated tab: ${tabId}`);
+  console.warn("ℹ️ Details: ", details);
+
+  // TODO: RESET TAB?
+  //   resetTab(tabId);
+
+  if (isDdgURL(url) && details.transitionType === "form_submit") {
+    chrome.storage.local.get(["bannerDismissed"], result => {
+      // cast boolean to int
+      pixel.fire("evg", { b: +result.bannerDismissed });
+    });
+  }
+
+  if (canShowBanner(url)) {
     // Check if banner dismissed before loading
-    chrome.storage.local.get(['bannerDismissed'], function (result) {
-        if (result.bannerDismissed) {
-            console.log('IGNORING. BANNER DISMISSED')
-            console.groupEnd()
-            return
-        }
-
-        if (changeInfo.status === 'complete') {
-            console.log(`🔔 Updated tab: ${tabId}`)
-            console.log('ℹ️ Changed attributes: ', changeInfo)
-            console.log('ℹ️ New tab Info: ', tabInfo)
-
-            createBanner(tabId, changeInfo, tabInfo)
-        }
-    })
-    console.groupEnd()
+    chrome.storage.local.get(["bannerDismissed"], result => {
+      if (!result.bannerDismissed) {
+        createBanner(tabId);
+      } else {
+        console.warn("IGNORING. BANNER DISMISSED");
+      }
+      console.groupEnd();
+    });
+  }
+  console.groupEnd();
 }
 
 var Banner = (() => {
-    return {
-        handleUpdated
-    }
-})()
+  return {
+    handleUpdated
+  };
+})();
 
-module.exports = Banner
+module.exports = Banner;
--- shared/js/background/chrome-events.es6.js
+++ shared/js/background/chrome-events.es6.js
@@ -4,53 +4,52 @@
  * on FF, we might actually miss the onInstalled event
  * if we do too much before adding it
  */
-const ATB = require('./atb.es6')
-const utils = require('./utils.es6')
+const ATB = require("./atb.es6");
+const utils = require("./utils.es6");
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    if (details.reason.match(/install/)) {
-        ATB.updateATBValues()
-            .then(ATB.openPostInstallPage)
-    }
-})
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason.match(/install/)) {
+    ATB.updateATBValues().then(ATB.openPostInstallPage);
+  }
+});
 
 /**
  * REQUESTS
  */
 
-const redirect = require('./redirect.es6')
-const tabManager = require('./tab-manager.es6')
-const pixel = require('./pixel.es6')
-const https = require('./https.es6')
-const constants = require('../../data/constants')
-let requestListenerTypes = utils.getUpdatedRequestListenerTypes()
+const redirect = require("./redirect.es6");
+const tabManager = require("./tab-manager.es6");
+const pixel = require("./pixel.es6");
+const https = require("./https.es6");
+const constants = require("../../data/constants");
+let requestListenerTypes = utils.getUpdatedRequestListenerTypes();
 
 // Shallow copy of request types
 // And add beacon type based on browser, so we can block it
 chrome.webRequest.onBeforeRequest.addListener(
-    redirect.handleRequest,
-    {
-        urls: ['<all_urls>'],
-        types: requestListenerTypes
-    },
-    ['blocking']
-)
+  redirect.handleRequest,
+  {
+    urls: ["<all_urls>"],
+    types: requestListenerTypes
+  },
+  ["blocking"]
+);
 
 chrome.webRequest.onHeadersReceived.addListener(
-    (request) => {
-        if (request.type === 'main_frame') {
-            tabManager.updateTabUrl(request)
-        }
-
-        if (/^https?:\/\/(.*?\.)?duckduckgo.com\/\?/.test(request.url)) {
-            // returns a promise
-            return ATB.updateSetAtb(request)
-        }
-    },
-    {
-        urls: ['<all_urls>']
+  request => {
+    if (request.type === "main_frame") {
+      tabManager.updateTabUrl(request);
     }
-)
+
+    if (/^https?:\/\/(.*?\.)?duckduckgo.com\/\?/.test(request.url)) {
+      // returns a promise
+      return ATB.updateSetAtb(request);
+    }
+  },
+  {
+    urls: ["<all_urls>"]
+  }
+);
 
 // keep track of URLs that the browser navigates to.
 //
@@ -58,241 +57,266 @@ chrome.webRequest.onHeadersReceived.addListener(
 // tabManager.updateTabUrl only fires when a tab has finished loading with a 200,
 // which misses a couple of edge cases like browser special pages
 // and Gmail's weird redirect which returns a 200 via a service worker
-chrome.webNavigation.onCommitted.addListener(
-    (details) => {
-        // ignore navigation on iframes
-        if (details.frameId !== 0) return
+chrome.webNavigation.onCommitted.addListener(details => {
+  // ignore navigation on iframes
+  if (details.frameId !== 0) return;
 
-        const tab = tabManager.get({tabId: details.tabId})
+  const tab = tabManager.get({ tabId: details.tabId });
 
-        if (!tab) return
+  if (!tab) return;
 
-        tab.updateSite(details.url)
-    }
-)
+  tab.updateSite(details.url);
+});
 
 /**
  * TABS
  */
 
-const Companies = require('./companies.es6')
+const Companies = require("./companies.es6");
 
 chrome.tabs.onUpdated.addListener((id, info) => {
-    // sync company data to storage when a tab finishes loading
-    if (info.status === 'complete') {
-        Companies.syncToStorage()
-    }
+  // sync company data to storage when a tab finishes loading
+  if (info.status === "complete") {
+    Companies.syncToStorage();
+  }
 
-    tabManager.createOrUpdateTab(id, info)
-})
+  tabManager.createOrUpdateTab(id, info);
+});
 
 // PRIVACY NUDGE BANNER
-const Banner = require('./banner.es6')
-chrome.tabs.onUpdated.addListener(Banner.handleUpdated)
+const Banner = require("./banner.es6");
+chrome.webNavigation.onCommitted.addListener(Banner.handleUpdated);
+// chrome.webNavigation.onDOMContentLoaded.addListener(Banner.handleUpdated);
 // END PRIVACY NUDGE BANNER
 
 chrome.tabs.onRemoved.addListener((id, info) => {
-    // remove the tab object
-    tabManager.delete(id)
-})
+  // remove the tab object
+  tabManager.delete(id);
+});
 
 // message popup to close when the active tab changes. this can send an error message when the popup is not open. check lastError to hide it
-chrome.tabs.onActivated.addListener(() => chrome.runtime.sendMessage({closePopup: true}, () => chrome.runtime.lastError))
+chrome.tabs.onActivated.addListener(() =>
+  chrome.runtime.sendMessage(
+    { closePopup: true },
+    () => chrome.runtime.lastError
+  )
+);
 
 // search via omnibox
 
-chrome.omnibox.onInputEntered.addListener(function (text) {
-    chrome.tabs.query({
-        'currentWindow': true,
-        'active': true
-    }, function (tabs) {
-        chrome.tabs.update(tabs[0].id, {
-            url: 'https://duckduckgo.com/?q=' + encodeURIComponent(text) + '&bext=' + localStorage['os'] + 'cl'
-        })
-    })
-})
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true
+    },
+    function(tabs) {
+      chrome.tabs.update(tabs[0].id, {
+        url:
+          "https://duckduckgo.com/?q=" +
+          encodeURIComponent(text) +
+          "&bext=" +
+          localStorage["os"] +
+          "cl"
+      });
+    }
+  );
+});
 
 /**
  * MESSAGES
  */
 
-const settings = require('./settings.es6')
-const browserWrapper = require('./chrome-wrapper.es6')
+const settings = require("./settings.es6");
+const browserWrapper = require("./chrome-wrapper.es6");
 
 // handle any messages that come from content/UI scripts
 // returning `true` makes it possible to send back an async response
 chrome.runtime.onMessage.addListener((req, sender, res) => {
-    if (sender.id !== chrome.runtime.id) return
+  if (sender.id !== chrome.runtime.id) return;
 
-    if (req.getCurrentTab) {
-        utils.getCurrentTab().then((tab) => {
-            res(tab)
-        })
+  if (req.getCurrentTab) {
+    utils.getCurrentTab().then(tab => {
+      res(tab);
+    });
 
-        return true
+    return true;
+  }
+
+  if (req.updateSetting) {
+    let name = req.updateSetting["name"];
+    let value = req.updateSetting["value"];
+    settings.ready().then(() => {
+      settings.updateSetting(name, value);
+    });
+  } else if (req.getSetting) {
+    let name = req.getSetting["name"];
+    settings.ready().then(() => {
+      res(settings.getSetting(name));
+    });
+
+    return true;
+  }
+
+  // popup will ask for the browser type then it is created
+  if (req.getBrowser) {
+    res(utils.getBrowserName());
+    return true;
+  }
+
+  if (req.getExtensionVersion) {
+    res(browserWrapper.getExtensionVersion());
+    return true;
+  }
+
+  if (req.getTopBlocked) {
+    res(Companies.getTopBlocked(req.getTopBlocked));
+    return true;
+  } else if (req.getTopBlockedByPages) {
+    res(Companies.getTopBlockedByPages(req.getTopBlockedByPages));
+    return true;
+  } else if (req.resetTrackersData) {
+    Companies.resetData();
+  }
+
+  if (req.whitelisted) {
+    tabManager.whitelistDomain(req.whitelisted);
+  } else if (req.whitelistOptIn) {
+    tabManager.setGlobalWhitelist(
+      "whitelistOptIn",
+      req.whitelistOptIn.domain,
+      req.whitelistOptIn.value
+    );
+  } else if (req.getTab) {
+    res(tabManager.get({ tabId: req.getTab }));
+    return true;
+  } else if (req.getSiteGrade) {
+    const tab = tabManager.get({ tabId: req.getSiteGrade });
+    let grade = {};
+
+    if (!tab.site.specialDomainName) {
+      grade = tab.site.grade.get();
     }
 
-    if (req.updateSetting) {
-        let name = req.updateSetting['name']
-        let value = req.updateSetting['value']
-        settings.ready().then(() => {
-            settings.updateSetting(name, value)
-        })
-    } else if (req.getSetting) {
-        let name = req.getSetting['name']
-        settings.ready().then(() => {
-            res(settings.getSetting(name))
-        })
+    res(grade);
+    return true;
+  }
 
-        return true
+  if (req.firePixel) {
+    let fireArgs = req.firePixel;
+    if (fireArgs.constructor !== Array) {
+      fireArgs = [req.firePixel];
     }
-
-    // popup will ask for the browser type then it is created
-    if (req.getBrowser) {
-        res(utils.getBrowserName())
-        return true
-    }
-
-    if (req.getExtensionVersion) {
-        res(browserWrapper.getExtensionVersion())
-        return true
-    }
-
-    if (req.getTopBlocked) {
-        res(Companies.getTopBlocked(req.getTopBlocked))
-        return true
-    } else if (req.getTopBlockedByPages) {
-        res(Companies.getTopBlockedByPages(req.getTopBlockedByPages))
-        return true
-    } else if (req.resetTrackersData) {
-        Companies.resetData()
-    }
-
-    if (req.whitelisted) {
-        tabManager.whitelistDomain(req.whitelisted)
-    } else if (req.whitelistOptIn) {
-        tabManager.setGlobalWhitelist('whitelistOptIn', req.whitelistOptIn.domain, req.whitelistOptIn.value)
-    } else if (req.getTab) {
-        res(tabManager.get({'tabId': req.getTab}))
-        return true
-    } else if (req.getSiteGrade) {
-        const tab = tabManager.get({tabId: req.getSiteGrade})
-        let grade = {}
-
-        if (!tab.site.specialDomainName) {
-            grade = tab.site.grade.get()
-        }
-
-        res(grade)
-        return true
-    }
-
-    if (req.firePixel) {
-        let fireArgs = req.firePixel
-        if (fireArgs.constructor !== Array) {
-            fireArgs = [req.firePixel]
-        }
-        res(pixel.fire.apply(null, fireArgs))
-        return true
-    }
-})
+    res(pixel.fire.apply(null, fireArgs));
+    return true;
+  }
+});
 
 /**
  * ALARMS
  */
 
-const httpsStorage = require('./storage/https.es6')
-const httpsService = require('./https-service.es6')
-const tdsStorage = require('./storage/tds.es6')
-const trackers = require('./trackers.es6')
+const httpsStorage = require("./storage/https.es6");
+const httpsService = require("./https-service.es6");
+const tdsStorage = require("./storage/tds.es6");
+const trackers = require("./trackers.es6");
 
 // recheck tracker and https lists every 12 hrs
-chrome.alarms.create('updateHTTPSLists', {periodInMinutes: 12 * 60})
+chrome.alarms.create("updateHTTPSLists", { periodInMinutes: 12 * 60 });
 // tracker lists / whitelists are 30 minutes
-chrome.alarms.create('updateLists', {periodInMinutes: 30})
+chrome.alarms.create("updateLists", { periodInMinutes: 30 });
 // update uninstall URL every 10 minutes
-chrome.alarms.create('updateUninstallURL', {periodInMinutes: 10})
+chrome.alarms.create("updateUninstallURL", { periodInMinutes: 10 });
 // remove expired HTTPS service entries
-chrome.alarms.create('clearExpiredHTTPSServiceCache', {periodInMinutes: 60})
+chrome.alarms.create("clearExpiredHTTPSServiceCache", { periodInMinutes: 60 });
 
 chrome.alarms.onAlarm.addListener(alarmEvent => {
-    if (alarmEvent.name === 'updateHTTPSLists') {
-        settings.ready().then(() => {
-            httpsStorage.getLists(constants.httpsLists)
-                .then(lists => https.setLists(lists))
-                .catch(e => console.log(e))
-        })
-    } else if (alarmEvent.name === 'updateUninstallURL') {
-        chrome.runtime.setUninstallURL(ATB.getSurveyURL())
-    } else if (alarmEvent.name === 'updateLists') {
-        settings.ready().then(() => {
-            https.sendHttpsUpgradeTotals()
-        })
+  if (alarmEvent.name === "updateHTTPSLists") {
+    settings.ready().then(() => {
+      httpsStorage
+        .getLists(constants.httpsLists)
+        .then(lists => https.setLists(lists))
+        .catch(e => console.log(e));
+    });
+  } else if (alarmEvent.name === "updateUninstallURL") {
+    chrome.runtime.setUninstallURL(ATB.getSurveyURL());
+  } else if (alarmEvent.name === "updateLists") {
+    settings.ready().then(() => {
+      https.sendHttpsUpgradeTotals();
+    });
 
-        tdsStorage.getLists()
-            .then(lists => trackers.setLists(lists))
-            .catch(e => console.log(e))
-    } else if (alarmEvent.name === 'clearExpiredHTTPSServiceCache') {
-        httpsService.clearExpiredCache()
-    }
-})
+    tdsStorage
+      .getLists()
+      .then(lists => trackers.setLists(lists))
+      .catch(e => console.log(e));
+  } else if (alarmEvent.name === "clearExpiredHTTPSServiceCache") {
+    httpsService.clearExpiredCache();
+  }
+});
 
 /**
  * on start up
  */
 let onStartup = () => {
-    chrome.tabs.query({currentWindow: true, status: 'complete'}, function (savedTabs) {
-        for (var i = 0; i < savedTabs.length; i++) {
-            var tab = savedTabs[i]
+  chrome.tabs.query({ currentWindow: true, status: "complete" }, function(
+    savedTabs
+  ) {
+    for (var i = 0; i < savedTabs.length; i++) {
+      var tab = savedTabs[i];
 
-            if (tab.url) {
-                tabManager.create(tab)
-            }
-        }
-    })
+      if (tab.url) {
+        tabManager.create(tab);
+      }
+    }
+  });
 
-    settings.ready().then(() => {
-        httpsStorage.getLists(constants.httpsLists)
-            .then(lists => https.setLists(lists))
-            .catch(e => console.log(e))
+  settings.ready().then(() => {
+    httpsStorage
+      .getLists(constants.httpsLists)
+      .then(lists => https.setLists(lists))
+      .catch(e => console.log(e));
 
-        tdsStorage.getLists()
-            .then(lists => trackers.setLists(lists))
-            .catch(e => console.log(e))
+    tdsStorage
+      .getLists()
+      .then(lists => trackers.setLists(lists))
+      .catch(e => console.log(e));
 
-        https.sendHttpsUpgradeTotals()
+    https.sendHttpsUpgradeTotals();
 
-        Companies.buildFromStorage()
-    })
-}
+    Companies.buildFromStorage();
+  });
+};
 
 // Fire pixel on https upgrade failures to allow bad data to be removed from lists
-chrome.webRequest.onErrorOccurred.addListener((e) => {
-    if (!(e.type === 'main_frame')) return
+chrome.webRequest.onErrorOccurred.addListener(
+  e => {
+    if (!(e.type === "main_frame")) return;
 
-    let tab = tabManager.get({tabId: e.tabId})
+    let tab = tabManager.get({ tabId: e.tabId });
 
     // We're only looking at failed main_frame upgrades. A tab can send multiple
     // main_frame request errors so we will only look at the first one then set tab.hasHttpsError.
     if (!tab || !tab.mainFrameUpgraded || tab.hasHttpsError) {
-        return
+      return;
     }
 
     if (e.error && e.url.match(/^https/)) {
-        const errCode = constants.httpsErrorCodes[e.error]
-        tab.hasHttpsError = true
+      const errCode = constants.httpsErrorCodes[e.error];
+      tab.hasHttpsError = true;
 
-        if (errCode) {
-            https.incrementUpgradeCount('failedUpgrades')
-            const url = new URL(e.url)
-            pixel.fire('ehd', {
-                'url': `${encodeURIComponent(url.hostname)}`,
-                'error': errCode
-            })
-        }
+      if (errCode) {
+        https.incrementUpgradeCount("failedUpgrades");
+        const url = new URL(e.url);
+        pixel.fire("ehd", {
+          url: `${encodeURIComponent(url.hostname)}`,
+          error: errCode
+        });
+      }
     }
-}, {urls: ['<all_urls>']})
+  },
+  { urls: ["<all_urls>"] }
+);
 
 module.exports = {
-    onStartup: onStartup
-}
+  onStartup: onStartup
+};
