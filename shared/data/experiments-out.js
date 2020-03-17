872ed4bebd73290972e06f42f7854e83d430b84a
Zaahir Moolla <moollaza@fastmail.fm>
2020-03-17 18:39:41 -0400

    update experiment definitions

--- shared/data/experiments-out.js
+++ shared/data/experiments-out.js
@@ -1,16 +1,17 @@
 module.exports = {
-    '_': {
-        'name': 'privacy_nudge',
-        'description': 'Privacy nudge experiment group',
-        'settings': {
-            'bannerEnabled': true
-        }
-    },
-    'z': {
+    '_': {},
+    'y': {
         'name': 'privacy_nudge',
         'description': 'Privacy nudge control group',
         'settings': {
             'bannerEnabled': false
+        }
+    },
+    'z': {
+        'name': 'privacy_nudge',
+        'description': 'Privacy nudge experiment group',
+        'settings': {
+            'bannerEnabled': true
         }
     }
 }
