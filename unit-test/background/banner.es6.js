3881ce75dae6c4bd3d69323c12737f7bad607da0
Zaahir Moolla <moollaza@fastmail.fm>
2020-03-23 22:30:51 -0400

    Create banner unit test file

---
+++ unit-test/background/banner.es6.js
@@ -0,0 +1,15 @@
+const pixel = require('../../shared/js/background/pixel.es6')
+const settings = require('../../shared/js/background/settings.es6')
+const experiment = require('../../shared/js/background/experiments.es6')
+
+describe('banner', () => {
+    beforeEach(() => { })
+
+    it('shows on accepted URLs', () => {
+        expect(true).toBe(true)
+    })
+
+    it('does not show on unaccepted URLs', () => {
+        expect(true).toBe(true)
+    })
+})
