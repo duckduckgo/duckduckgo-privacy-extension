020f5c051e5e860055e4b83f1b21b6af9253c6ba
Zaahir Moolla <moollaza@fastmail.fm>
2020-03-24 20:27:00 -0400

    First pass at unit tests

--- unit-test/background/banner.es6.js
+++ unit-test/background/banner.es6.js
@@ -1,15 +1,59 @@
+const banner = require('../../shared/js/background/banner.es6')
 const pixel = require('../../shared/js/background/pixel.es6')
-const settings = require('../../shared/js/background/settings.es6')
-const experiment = require('../../shared/js/background/experiments.es6')
+// const settings = require('../../shared/js/background/settings.es6')
+const settingHelper = require('../helpers/settings.es6')
 
-describe('banner', () => {
-    beforeEach(() => { })
+fdescribe('banner.handleOnCommitted ', () => {
+    const passingTests = [
+        {
+            pixelID: 'evg',
+            pixelOps: {be: 1},
+            details: {
+                tabId: 0,
+                frameId: 0,
+                transitionType: 'form_submit',
+                url: 'https://www.google.com/search?q=test'
+            }
+        },
+        {
+            pixelID: 'evd',
+            pixelOps: {be: 1},
+            details: {
+                tabId: 0,
+                frameId: 0,
+                transitionType: 'form_submit',
+                url: 'https://duckduckgo.com/?q=test'
+            }
+        }
+    ]
 
-    it('shows on accepted URLs', () => {
-        expect(true).toBe(true)
+    beforeAll(() => {
+        spyOn(pixel, 'fire')
     })
 
-    it('does not show on unaccepted URLs', () => {
-        expect(true).toBe(true)
+    passingTests.forEach((test) => {
+        it(`fires a pixel for ${test.details.url}`, () => {
+            settingHelper.stub({
+                activeExperiment: {name: 'privacy_nudge'},
+                bannerEnabled: true
+            })
+            banner.handleOnCommitted(test.details)
+            expect(pixel.fire).toHaveBeenCalledWith(test.pixelID, test.pixelOps)
+        })
+    })
+})
+
+describe('banner.handleOnDOMContentLoaded', () => {
+    const tests = [
+        { tabId: 0, frameId: 0, transitionType: 'form_submit', url: 'https://www.google.com/search?q=test' },
+        { tabId: 0, frameId: 0, transitionType: 'generated', url: 'https://www.duckduckgo.com/?q=test' }
+    ]
+
+    tests.forEach((test) => {
+        it(`shows a banner for ${test.url}`, () => {
+            spyOn(settings, 'getSetting').and.returnValue(false)
+            banner.handleOnDOMContentLoaded(test)
+            expect(chrome.tabs.insertCSS).toHaveBeenCalled()
+        })
     })
 })
