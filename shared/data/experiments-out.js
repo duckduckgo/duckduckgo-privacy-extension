b017bb46326c125ce76235d184dbe456d3b8363d
Zaahir Moolla <moollaza@duckduckgo.com>
2020-06-23 13:30:14 -0400

    Remove nudge (#460)
    * Remove nudge banner
    
    * Remove banner build steps
    
    * Remove banner code from chrome-events

--- shared/data/experiments-out.js
+++ shared/data/experiments-out.js
@@ -1,32 +1,13 @@
 module.exports = {
     _: {},
-    r: {
-        name: 'privacy_nudge',
-        description: 'Privacy nudge experiment',
-        active: true,
-        atbExperiments: {
-            'y': {
-                description: 'Privacy nudge control group',
-                settings: {
-                    bannerEnabled: false
-                }
-            },
-            'z': {
-                description: 'Privacy nudge experiment group',
-                settings: {
-                    bannerEnabled: true
-                }
-            }
-        }
-    },
     l: {
         name: 'full tracker list',
         description: 'Testing full Tracker Radar list',
         active: false,
         atbExperiments: {
             'm': {
-                descripiton: 'Full list experiment group',
-                settings: { 
+                description: 'Full list experiment group',
+                settings: {
                     experimentData: {
                         listName: 'tds',
                         url: 'https://staticcdn.duckduckgo.com/trackerblocking/lm/tds.json'
