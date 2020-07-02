c36401f6b5ae5a0eedfc3d190de630a01f64a6c1
MariagraziaAlastra <maria@duckduckgo.com>
2020-07-02 17:46:32 -0400

    Further copy updates

--- shared/js/ui/templates/whitelist.es6.js
+++ shared/js/ui/templates/whitelist.es6.js
@@ -14,10 +14,10 @@ module.exports = function () {
     function addToWhitelist () {
         return bel`<div>
     <p class="whitelist-show-add js-whitelist-show-add">
-        <a href="javascript:void(0)">Enter URL to disable Protection</a>
+        <a href="javascript:void(0)">Add Unprotected Site</a>
     </p>
     <input class="is-hidden whitelist-url float-left js-whitelist-url" type="text" placeholder="Enter URL">
-    <div class="is-hidden whitelist-add is-disabled float-right js-whitelist-add">Disable</div>
+    <div class="is-hidden whitelist-add is-disabled float-right js-whitelist-add">Add</div>
 
     <div class="is-hidden modal-box js-whitelist-error float-right">
         <div class="modal-box__popout">
