a778ae9ae9428c3ccc47885e4032fab2ca27fc8f
MariagraziaAlastra <maria@duckduckgo.com>
2020-07-09 21:10:55 -0400

    don't include the 'no tracker data' view in the main popup + vertical spacing in the grade details subview + other styling tweaks

--- shared/js/ui/templates/top-blocked-truncated.es6.js
+++ shared/js/ui/templates/top-blocked-truncated.es6.js
@@ -1,6 +1,5 @@
 const bel = require('bel')
 const listItems = require('./top-blocked-truncated-list-items.es6.js')
-const noData = require('./shared/top-blocked-no-data.es6.js')
 
 module.exports = function () {
     if (this.model.companyListMap && this.model.companyListMap.length > 0) {
@@ -17,14 +16,6 @@ module.exports = function () {
             All Tracker Networks
         </a>
     </div>
-</section>`
-    } else {
-        return bel`<section class="top-blocked card card--transparent">
-    <ol class="default-list top-blocked__list top-blocked__list--truncated">
-        <li class="top-blocked__li top-blocked__li--no-data">
-            ${noData()}
-        </li>
-    </ol>
 </section>`
     }
 }
