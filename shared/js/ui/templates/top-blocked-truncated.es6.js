bc8ce581099e7ba8b1b60b1c1d14ffe6fa176ade
MariagraziaAlastra <maria@duckduckgo.com>
2020-07-23 20:50:01 -0400

    styles for top blocked icons in the main popup

--- shared/js/ui/templates/top-blocked-truncated.es6.js
+++ shared/js/ui/templates/top-blocked-truncated.es6.js
@@ -2,13 +2,13 @@ const bel = require('bel')
 const listItems = require('./top-blocked-truncated-list-items.es6.js')
 
 module.exports = function () {
-    if (this.model.companyListMap && this.model.companyListMap.length > 0) {
+    if (true) { // (this.model.companyListMap && this.model.companyListMap.length > 0) {
         return bel`<section class="top-blocked top-blocked--truncated">
     <div class="top-blocked__see-all js-top-blocked-see-all">
         <a href="javascript:void(0)" class="link-secondary">
             <span class="icon icon__arrow pull-right"></span>
             Top Tracking Offenders
-            <span class="is-hidden top-blocked__list top-blocked__list--truncated top-blocked__list--icons">
+            <span class="top-blocked__list top-blocked__list--truncated top-blocked__list--icons">
                 ${listItems(this.model.companyListMap)}
             </span>
         </a>
