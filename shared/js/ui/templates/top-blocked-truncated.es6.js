ea3669a6195dd123656e8a7fcc1555eaed1069d7
MariagraziaAlastra <maria@duckduckgo.com>
2020-07-24 16:04:25 -0400

    remove debug code

--- shared/js/ui/templates/top-blocked-truncated.es6.js
+++ shared/js/ui/templates/top-blocked-truncated.es6.js
@@ -2,7 +2,7 @@ const bel = require('bel')
 const listItems = require('./top-blocked-truncated-list-items.es6.js')
 
 module.exports = function () {
-    if (true) { // (this.model.companyListMap && this.model.companyListMap.length > 0) {
+    if (this.model.companyListMap && this.model.companyListMap.length > 0) {
         return bel`<section class="top-blocked top-blocked--truncated">
     <div class="top-blocked__see-all js-top-blocked-see-all">
         <a href="javascript:void(0)" class="link-secondary">
