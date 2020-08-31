c588285f8aa371b31b69c3c56543e48c68f12318
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2020-08-31 16:59:13 +0200

    Limit fetch attempts to 5 max
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- shared/js/background/email-utils.es6.js
+++ shared/js/background/email-utils.es6.js
@@ -1,6 +1,9 @@
 const {updateSetting} = require('./settings.es6')
 const REFETCH_ALIAS_ALARM = 'refetchAlias'
 
+// Keep track of the number of attempted fetches. Stop trying after 5.
+let attempts = 1
+
 const fetchAlias = () => {
     // if another fetch was previously scheduled, clear that and execute now
     chrome.alarms.get(REFETCH_ALIAS_ALARM, () => chrome.alarms.clear(REFETCH_ALIAS_ALARM))
@@ -8,15 +11,23 @@ const fetchAlias = () => {
     fetch('')
         .then(response => {
             if (response.ok) {
-                response.text().then(alias => updateSetting('nextAlias', alias))
+                response.text().then(alias => {
+                    updateSetting('nextAlias', alias)
+                    // Reset attempts
+                    attempts = 1
+                })
             } else {
-                // ERROR
+                throw new Error('An error occurred while fetching the alias')
             }
         })
         .catch(e => {
             // TODO: Do we want to logout if the error is a 401 unauthorized?
-            console.log(e)
-            chrome.alarms.create(REFETCH_ALIAS_ALARM, {delayInMinutes: 2})
+            console.log('Error fetching new alias', e)
+            // Don't try fetching more than 5 times in a row
+            if (attempts < 5) {
+                chrome.alarms.create(REFETCH_ALIAS_ALARM, {delayInMinutes: 2})
+                attempts++
+            }
         })
 }
 
