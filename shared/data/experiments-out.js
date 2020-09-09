af379ed9d37d03d1d706093e091012995b2e2d45
Charlie Belmer <cbelmer@duckduckgo.com>
2020-09-09 08:40:52 -0400

    Enable fingerprint protection for all users

--- shared/data/experiments-out.js
+++ shared/data/experiments-out.js
@@ -15,20 +15,5 @@ module.exports = {
                 }
             }
         }
-    },
-    f: {
-        name: 'Fingerprint protection',
-        description: 'Testing basic fingerprint protection',
-        active: true,
-        atbExperiments: {
-            'k': {
-                description: 'Basic fingerprint protection experiment group',
-                settings: {
-                    experimentData: {
-                        fingerprint_protection: true
-                    }
-                }
-            }
-        }
     }
 }
