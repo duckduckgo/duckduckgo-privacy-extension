18f7e7812edca5423980a5be47856620743876ca
Charlie Belmer <cbelmer@duckduckgo.com>
2020-10-20 10:28:55 -0400

    Minor update to cache handling

--- shared/js/background/storage/agents.es6.js
+++ shared/js/background/storage/agents.es6.js
@@ -49,6 +49,16 @@ class AgentStorage {
                         settings.updateSetting(`${listName}-etag`, newEtag)
                     } else if (response && response.status === 304) {
                         console.log(`${list.url} returned 304, resource not changed`)
+                        if (this.agents.length == 0) {
+                            this.loadAgentList(listName)
+                                .then(queryData => {
+                                    this.processList(listName, queryData.listData)
+                                })
+                                .catch(e => {
+                                    console.log(`Error loading UserAgent settings from storage: ${e}`)
+                                    settings.updateSetting(`${listName}-etag`, '')
+                                })
+                        }
                     }
                 })
                 .catch(e => {
