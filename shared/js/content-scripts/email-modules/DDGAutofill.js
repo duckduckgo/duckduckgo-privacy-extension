ce5eceaa6d6b481b2e0cc46dd5b29e44009fc0af
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2020-11-19 15:34:09 +0100

    Check clicks with IntersectionObserver v2
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- shared/js/content-scripts/email-modules/DDGAutofill.js
+++ shared/js/content-scripts/email-modules/DDGAutofill.js
@@ -39,6 +39,27 @@ class DDGAutofill extends HTMLElement {
         this.dismissButton = shadow.querySelector('.js-dismiss')
         this.confirmButton = shadow.querySelector('.js-confirm')
         this.aliasEl = shadow.querySelector('.alias')
+
+        /**
+         * Use IntersectionObserver v2 to make sure the element is visible when clicked
+         * https://developers.google.com/web/updates/2019/02/intersectionobserver-v2
+         */
+        this.safeExecute = (el, fn) => {
+            const intObs = new IntersectionObserver((changes) => {
+                for (const change of changes) {
+                    // Feature detection
+                    if (typeof change.isVisible === 'undefined') {
+                        // The browser doesn't support Intersection Observer v2, falling back to v1 behavior.
+                        change.isVisible = true;
+                    }
+                    if (change.isIntersecting && change.isVisible) {
+                        fn()
+                    }
+                }
+                intObs.disconnect()
+            }, {trackVisibility: true, delay: 100})
+            intObs.observe(el)
+        }
     }
 
     static updateButtonPosition (el) {
@@ -138,7 +159,7 @@ class DDGAutofill extends HTMLElement {
         this.trigger.addEventListener('click', (e) => {
             if (!e.isTrusted) return
 
-            this.showTooltip()
+            this.safeExecute(this.trigger, () => this.showTooltip())
         })
         this.dismissButton.addEventListener('click', (e) => {
             if (!e.isTrusted) return
@@ -149,10 +170,12 @@ class DDGAutofill extends HTMLElement {
         })
         this.confirmButton.addEventListener('click', (e) => {
             if (!e.isTrusted) return
-
             e.stopImmediatePropagation()
-            this.autofillInputs()
-            this.hideTooltip()
+
+            this.safeExecute(this.confirmButton, () => {
+                this.autofillInputs()
+                this.hideTooltip()
+            })
         })
     }
 }
