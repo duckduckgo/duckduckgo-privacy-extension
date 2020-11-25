c90c0a2b3222ff2b291ebd890a27413246468e99
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2020-11-25 16:39:20 +0100

    Add url to test description for faster debugging
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- integration-test/content-scripts/email-autofill.js
+++ integration-test/content-scripts/email-autofill.js
@@ -34,7 +34,7 @@ describe('Email autofill input detection Tests', () => {
     })
 
     sites.forEach(({name, url, autofillExpected, actions}) => {
-        it(`Test input field detection on ${name}`, async () => {
+        it(`Test input field detection on ${name} at ${url}`, async () => {
             const page = await browser.newPage()
             await page.setViewport({width: 1300, height: 800})
             const ua = await browser.userAgent()
