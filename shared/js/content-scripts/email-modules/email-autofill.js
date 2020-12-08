4cf3ad4fd70a022a56a6f491942acdf3e383e5ca
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2020-12-08 11:14:55 +0100

    Skip aria-hidden elements ins elector
    This solves a bug in Walmart.com
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- shared/js/content-scripts/email-modules/email-autofill.js
+++ shared/js/content-scripts/email-modules/email-autofill.js
@@ -123,15 +123,15 @@
         })
 
         const EMAIL_SELECTOR = `
-            input:not([type])[name*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=""][name*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=text][name*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input:not([type])[id*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=""][id*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=text][placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=""][placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input:not([type])[placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]),
-            input[type=email]:not([readonly]):not([disabled]):not([hidden]),
+            input:not([type])[name*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=""][name*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=text][name*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input:not([type])[id*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=""][id*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=text][placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=""][placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input:not([type])[placeholder*=mail i]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
+            input[type=email]:not([readonly]):not([disabled]):not([hidden]):not([aria-hidden=true]),
             input[type=text][aria-label*=mail i],
             input:not([type])[aria-label*=mail i],
             input[type=text][placeholder*=mail i]:not([readonly])
