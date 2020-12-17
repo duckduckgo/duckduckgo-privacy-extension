86968ff6afefa5c89b4f4e7f215719731d670307
Dave Harbage <harbagio@gmail.com>
2020-12-17 18:45:59 -0500

    GPC default on. Properly deal with extension conflicts.

--- shared/js/content-scripts/GPC.js
+++ shared/js/content-scripts/GPC.js
@@ -17,15 +17,27 @@
         // if we can't find content type, go ahead with injection
     }
     const scriptString = `
-        // Catch errors if signal is already set by user agent or other extension
-        try {
-            Object.defineProperty(navigator, 'globalPrivacyControl', {
-                value: ${globalPrivacyControlValue},
-                enumerable: true
-            })
+        (function() {
+            // If GPC on, set DOM property to true if not already true
+            if (${globalPrivacyControlValue}) {
+                if (navigator.globalPrivacyControl) return
+                Object.defineProperty(navigator, 'globalPrivacyControl', {
+                    value: true,
+                    enumerable: true
+                })
+            } else {
+                // If GPC off, set DOM property prototype to false so it may be overwritten
+                // with a true value by user agent or other extensions
+                if (typeof navigator.globalPrivacyControl !== "undefined") return
+                Object.defineProperty(Object.getPrototypeOf(navigator), 'globalPrivacyControl', {
+                    value: false,
+                    enumerable: true
+                })
+            }
             // Remove script tag after execution
             document.currentScript.parentElement.removeChild(document.currentScript)
-        } catch (e) {}`
+        })()
+        `
     const scriptElement = document.createElement('script')
     scriptElement.innerHTML = scriptString
     document.documentElement.prepend(scriptElement)
