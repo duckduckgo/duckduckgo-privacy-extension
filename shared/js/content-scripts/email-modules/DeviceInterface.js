dcf453693a6aeffca371751b9bab06ae5844f416
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-01-13 09:27:41 +0100

    Move DeviceInterface to separate file
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

---
+++ shared/js/content-scripts/email-modules/DeviceInterface.js
@@ -0,0 +1,41 @@
+const {DDG_DOMAIN_REGEX, sendAndWaitForAnswer} = require('./autofill-utils')
+const Form = require('./Form.js')
+const scanForInputs = require('./scanForInputs.js')
+
+const DeviceInterface = {
+    isDeviceSignedIn: () => new Promise(resolve => chrome.runtime.sendMessage(
+        {getSetting: {name: 'userData'}},
+        userData => resolve(!!(userData && userData.nextAlias))
+    )),
+    trySigningIn: () => {
+        if (window.origin.match(DDG_DOMAIN_REGEX)) {
+            sendAndWaitForAnswer({signMeIn: true}, 'addUserData')
+                .then(data => DeviceInterface.storeUserData(data))
+        }
+    },
+    storeUserData: (data) => chrome.runtime.sendMessage(data),
+    addDeviceListeners: () => {
+        // Add contextual menu listeners
+        let activeEl = null
+        document.addEventListener('contextmenu', e => {
+            activeEl = e.target
+        })
+
+        chrome.runtime.onMessage.addListener((message, sender) => {
+            if (sender.id !== chrome.runtime.id) return
+
+            switch (message.type) {
+                case 'ddgUserReady':
+                    scanForInputs()
+                    break
+                case 'contextualAutofill':
+                    Form.autofillInput(activeEl, message.alias)
+                    break
+                default:
+                    break
+            }
+        })
+    }
+}
+
+module.exports = DeviceInterface
--- shared/js/content-scripts/email-modules/email-autofill.js
+++ shared/js/content-scripts/email-modules/email-autofill.js
@@ -3,45 +3,8 @@
     require('intersection-observer')
     require('./requestIdleCallback')
     require('@webcomponents/webcomponentsjs')
-    const {DDG_DOMAIN_REGEX, sendAndWaitForAnswer} = require('./autofill-utils')
-    const Form = require('./Form.js')
+    const DeviceInterface = require('./DeviceInterface')
     const scanForInputs = require('./scanForInputs.js')
-
-    const DeviceInterface = {
-        isDeviceSignedIn: () => new Promise(resolve => chrome.runtime.sendMessage(
-            {getSetting: {name: 'userData'}},
-            userData => resolve(!!(userData && userData.nextAlias))
-        )),
-        trySigningIn: () => {
-            if (window.origin.match(DDG_DOMAIN_REGEX)) {
-                sendAndWaitForAnswer({signMeIn: true}, 'addUserData')
-                    .then(data => DeviceInterface.storeUserData(data))
-            }
-        },
-        storeUserData: (data) => chrome.runtime.sendMessage(data),
-        addDeviceListeners: () => {
-            // Add contextual menu listeners
-            let activeEl = null
-            document.addEventListener('contextmenu', e => {
-                activeEl = e.target
-            })
-
-            chrome.runtime.onMessage.addListener((message, sender) => {
-                if (sender.id !== chrome.runtime.id) return
-
-                switch (message.type) {
-                    case 'ddgUserReady':
-                        scanForInputs()
-                        break
-                    case 'contextualAutofill':
-                        Form.autofillInput(activeEl, message.alias)
-                        break
-                    default:
-                        break
-                }
-            })
-        }
-    }
 
     DeviceInterface.addDeviceListeners()
     DeviceInterface.isDeviceSignedIn().then(deviceSignedIn => {
