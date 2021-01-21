31c2848a46de8de39bda9ad4de7f6ae10ec99373
Brad Slayter <slayterdevelopment@gmail.com>
2021-01-21 11:12:28 -0600

    Clear previously set cookies when injecting script

--- shared/js/content-scripts/block-cookie.js
+++ shared/js/content-scripts/block-cookie.js
@@ -26,6 +26,23 @@
         if (isThridParty) {
             const scriptString = `
                 (function() {
+                    // Clear previously set cookies
+                    var cookies = document.cookie.split("; ");
+                    for (var c = 0; c < cookies.length; c++) {
+                        var d = window.location.hostname.split(".");
+                        while (d.length > 0) {
+                            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+                            var p = location.pathname.split('/');
+                            document.cookie = cookieBase + '/';
+                            while (p.length > 0) {
+                                document.cookie = cookieBase + p.join('/');
+                                p.pop();
+                            };
+                            d.shift();
+                        }
+                    }
+
+                    // disable setting cookies
                     document.__defineSetter__ ('cookie', function(value) { })
                 })()
             `
