99379ac6743084abd2a25430455c008c32c1dd51
Jonathan Kingston <jonathan@jooped.co.uk>
2021-01-29 17:15:51 +0000

    Change allow-lists for broken canvas scripts to be loaded remotely

--- shared/data/constants.js
+++ shared/data/constants.js
@@ -98,15 +98,15 @@ module.exports = {
         },
         {
             "name": "brokenCanvasSiteList",
-            "url": "/data/broken-canvas-sites.txt",
+            "url": "https://duckduckgo.com/contentblocking/broken-canvas-sites.txt",
             "format": "text",
-            "source": "local"
+            "source": "external"
         },
         {
             "name": "brokenCanvasScriptList",
-            "url": "/data/broken-canvas-scripts.txt",
+            "url": "https://duckduckgo.com/contentblocking/broken-canvas-scripts.txt",
             "format": "text",
-            "source": "local"
+            "source": "external"
         },
         {
             "name": "ReferrerExcludeList",
