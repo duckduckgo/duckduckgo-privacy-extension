39947a374acddd53a26a5471a5a5a4b48326b45c
Jonathan Kingston <jonathan@jooped.co.uk>
2021-02-09 12:47:12 +0000

    Add visibility check for sjcl lib

--- integration-test/background/test-fingerprint.js
+++ integration-test/background/test-fingerprint.js
@@ -149,3 +149,46 @@ describe('First Party Fingerprint Randomization', () => {
         expect(plugin.size).toEqual(tests.length)
     })
 })
+
+describe('Verify injected script is not visible to the page', () => {
+    beforeAll(async () => {
+        ({ browser, bgPage } = await harness.setup())
+
+        // wait for HTTPs to successfully load
+        await bgPage.waitForFunction(
+            () => window.dbg && dbg.https.isReady,
+            { polling: 100, timeout: 6000 }
+        )
+    })
+    afterAll(async () => {
+        await harness.teardown(browser)
+    })
+
+    tests.forEach(test => {
+        it('Fingerprints should not match across first parties', async () => {
+            const page = await browser.newPage()
+
+            try {
+                await page.goto(`http://${test.url}`, { waitUntil: 'networkidle0' })
+            } catch (e) {
+                // timed out waiting for page to load, let's try running the test anyway
+            }
+
+            // give it another second just to be sure
+            await page.waitFor(1000)
+
+            const sjclVal = await page.evaluate(() => {
+                if ('sjcl' in window) {
+                    return 'visible';
+                } else {
+                    return 'invisible';
+                }
+            })
+
+            await page.close()
+
+
+            expect(sjclVal).toEqual('invisible')
+        })
+    })
+})
