074955eee550f255a6519918de9018e74528b5d5
Brad Slayter <slayterdevelopment@gmail.com>
2021-02-09 09:25:27 -0600

    Don't inject of non-html docs

--- shared/js/content-scripts/block-cookie.js
+++ shared/js/content-scripts/block-cookie.js
@@ -1,5 +1,16 @@
 // Set up 3rd party cookie blocker
 (function cookieBlocker () {
+
+    // don't inject into non-HTML documents (such as XML documents)
+    // but do inject into XHTML documents
+    if (document instanceof HTMLDocument === false && (
+        document instanceof XMLDocument === false ||
+        document.createElement('div') instanceof HTMLDivElement === false
+    )) {
+        return;
+    }
+  
+
     function clearInjectedCookiesAndBlock () {
         // Clear previously set cookies
         var cookies = document.cookie.split('; ')
