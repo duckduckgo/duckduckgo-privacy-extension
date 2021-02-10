f965a4699bde471ba27a61b2dea6ab46a1cb8ebe
emanuele@duckduckgo.com <emanuele@duckduckgo.com>
2021-02-10 16:52:59 +0100

    Update shared/js/background/atb.es6.js
    Co-authored-by: abrown@duckduckgo.com <abrown@duckduckgo.com>

--- shared/js/background/atb.es6.js
+++ shared/js/background/atb.es6.js
@@ -208,7 +208,7 @@ const ATB = (() => {
                         const domain = (tabs && tabs[0]) ? tabs[0].url : ''
                         if (ATB.canShowPostInstall(domain)) {
                             settings.updateSetting('hasSeenPostInstall', true)
-                            let postInstallURL = 'https://quack.duckduckgo.com/email/signup/'
+                            let postInstallURL = 'https://quack.duckduckgo.com/email/signup/?'
                             const atb = settings.getSetting('atb')
                             postInstallURL += atb ? `&atb=${atb}` : ''
                             chrome.tabs.create({
