daa20c1021c4dc9642d7ebe3b70bee74e572c372
Charlie Belmer <cbelmer@duckduckgo.com>
2021-02-10 18:33:02 -0500

    FB login seems to be working now, for sites tested

--- shared/js/content-scripts/click-to-load.js
+++ shared/js/content-scripts/click-to-load.js
@@ -1,20 +1,34 @@
 (function clickToLoad () {
+    let appID
+
     class DuckWidget {
         constructor (widgetData, originalElement) {
             this.imgURI = this.createImgURI(widgetData.imgFile)
             this.clickAction = {...widgetData.clickAction} // shallow copy
             this.originalElement = originalElement
-            this.copySocialDataElements()
         }
 
         createImgURI (imgFile) {
             return chrome.runtime.getURL(imgFile)
         }
 
-        copySocialDataElements () {
+        getTargetURL () {
+            // This has to be done lazy, since some required data may not be
+            // captured until after page scripts run.
+            this.copySocialDataFields()
+            return this.clickAction.targetURL
+        }
+
+        copySocialDataFields () {
             if (!this.clickAction.urlDataAttributesToPreserve) {
                 return
             }
+
+            // App ID may be set by client scripts, and is required for some elements.
+            if (this.clickAction.urlDataAttributesToPreserve.includes('app_id_replace') && appID != null) {
+                this.clickAction.targetURL = this.clickAction.targetURL.replace('app_id_replace', appID)
+            }
+
             for (const dataString of this.clickAction.urlDataAttributesToPreserve) {
                 this.clickAction.targetURL = this.clickAction.targetURL.replace(dataString, this.originalElement.getAttribute(dataString))
             }
@@ -51,6 +65,11 @@
                 button.addEventListener('click', function handleClick (e) {
                     if (e.isTrusted) {
                         enableSocialTracker(entity)
+                        if (widget.clickAction.type === 'allowFull') {
+                            const parent = button.parentNode
+                            parent.replaceChild(originalElement, button)
+                            window.dispatchEvent(new CustomEvent('LoadFBSDK'))
+                        }
                         if (widget.clickAction.type === 'iFrame') {
                             replaceDDGWidgetWithIFrame(button, widget)
                         }
@@ -79,7 +98,7 @@
 
     function replaceDDGWidgetWithIFrame (widgetElement, widgetData) {
         const frame = document.createElement('iframe')
-        frame.setAttribute('src', widgetData.clickAction.targetURL)
+        frame.setAttribute('src', widgetData.getTargetURL())
         frame.setAttribute('style', widgetData.clickAction.style)
         const parent = widgetElement.parentNode
         parent.replaceChild(frame, widgetElement)
@@ -108,10 +127,61 @@
         }
     })
 
+    function createModal (entity, message, eventName) {
+        const elementCSS = `
+            height: 100%;
+            width: 100%;
+            z-index: 2147483647;
+            display: block;
+            position: fixed;
+        `
+        const overlayCSS = `
+            height: 100%;
+            width: 100%;
+            background-color: black;
+            opacity: .5;
+            display: block;
+            position: fixed;
+            top: 0;
+            right: 0;
+        `
+        const modalCSS = `
+            height: 20%;
+            width: 50%;
+            margin: 20% 25%;
+            background-color: orange;
+            position: relative;
+            display: block;
+        `
+        let element = document.createElement('div')
+        element.style.cssText = elementCSS
+        element.id = 'duckduckgoctlmodal'
+        let overlay = document.createElement('div')
+        overlay.style.cssText = overlayCSS
+        let modal = document.createElement('div')
+        modal.style.cssText = modalCSS
+        const allowButton = document.createElement('button')
+        allowButton.innerHTML = 'Allow'
+        const msg = document.createElement('p')
+        allowButton.addEventListener('click', function handleClick (e) {
+            if (e.isTrusted) {
+                enableSocialTracker(entity)
+                window.dispatchEvent(new CustomEvent(eventName))
+                const modalParent = element.parentNode
+                modalParent.removeChild(element)
+            }
+        })
+        msg.innerHTML = message
+        modal.appendChild(msg)
+        modal.appendChild(allowButton)
+        element.appendChild(overlay)
+        element.appendChild(modal)
+        return element
+    }
+
     window.addEventListener('message', (event) => {
         // Only accept messages from the same frame
         if (event.source !== window) {
-            console.log('message, but not from frame')
             return
         }
 
@@ -119,9 +189,26 @@
 
         // Only accept messages that we know are ours
         if (typeof message !== 'object' || message === null || (!!message.source && message.source !== 'fb-surrogate')) {
-            console.log(`message, but doesn't look like ours`)
             return
         }
-        console.log(event)
+
+        // Save appID
+        if (message.payload.appID) {
+            appID = JSON.stringify(message.payload.appID).replace(/"/g, '')
+        }
+
+        // Handle login call
+        if (message.payload.login || message.payload.fbui) {
+            const body = document.body
+            let e = createModal('Facebook', 'This site is trying to use login', 'RunFBLogin')
+            body.insertBefore(e, body.childNodes[0])
+            // body.appendChild(e)
+        }
+        
+        if (message.payload.fbui) {
+            const body = document.body
+            let e = createModal('Facebook', 'This page is trying to use facebook social buttons, would you like to allow it?', 'LoadFBSDK')
+            body.insertBefore(e, body.childNodes[0])
+        }
     })
 })()
