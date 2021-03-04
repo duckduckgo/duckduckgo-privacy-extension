53fdf630af00c2548c4b7746033ae165fce31b5e
Sam Macbeth <smacbeth@duckduckgo.com>
2021-03-04 12:34:36 +0100

    Enforce version format

--- scripts/nightly-manifest.js
+++ scripts/nightly-manifest.js
@@ -6,9 +6,13 @@ gecko.id = 'firefox-beta@duckduckgo.com'
 gecko.update_url = 'https://sammacbeth.github.io/duckduckgo-privacy-extension/updates.json'
 
 const day = new Date()
+const versionRegex = /^[0-9]{4}.[0-9]{1,2}.[0-9]{1,2}$/
 manifest.version = process.env.VERSION || `${day.getUTCFullYear()}.${day.getUTCMonth() + 1}.${day.getUTCDate()}`
 if (process.env.GITHUB_REF && process.env.GITHUB_REF.startsWith('refs/tags/')) {
     // in CI we pull the version from the tag ref
     manifest.version = process.env.GITHUB_REF.slice(10)
+}
+if (!versionRegex.test(manifest.version)) {
+    throw new Error(`Version does not match expected format ${manifest.version}`)
 }
 console.log(JSON.stringify(manifest, null, 4))
