33e6c2a8ad475cac3ae4ee456a8b158120fd3bea
Sam Macbeth <smacbeth@duckduckgo.com>
2021-03-04 11:10:31 +0100

    wip

--- scripts/updater.js
+++ scripts/updater.js
@@ -2,9 +2,10 @@ const fs = require('fs')
 
 const addonId = 'firefox-beta@duckduckgo.com'
 const [, , updateFile, version, xpiUrl] = process.argv
+console.log('args', updateFile, version, xpiUrl)
 const updateManifest = JSON.parse(fs.readFileSync(updateFile, 'utf-8'))
 updateManifest.addons[addonId].updates.push({
     version,
     update_link: xpiUrl
 })
-console.log(JSON.stringify(updateManifest, null, 2))
+fs.writeFileSync(updateFile, JSON.stringify(updateManifest, null, 2), 'utf-8')
