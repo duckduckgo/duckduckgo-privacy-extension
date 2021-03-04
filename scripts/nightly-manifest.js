9e1be8d40aba36748602c7187e273ac110a4795c
Sam Macbeth <smacbeth@duckduckgo.com>
2021-03-04 10:11:21 +0100

    Add AMO signing

--- scripts/nightly-manifest.js
+++ scripts/nightly-manifest.js
@@ -5,5 +5,5 @@ const manifest = JSON.parse(fs.readFileSync(process.stdin.fd, 'utf-8'))
 const gecko = manifest.applications.gecko
 gecko.id = 'firefox-beta@duckduckgo.com'
 gecko.update_url = 'https://sammacbeth.github.io/duckduckgo-privacy-extension/updates.json'
-// manifest.version = `${day.getUTCFullYear()}.${day.getUTCMonth() + 1}.${day.getUTCDate()}`
+manifest.version = `${day.getUTCFullYear()}.${day.getUTCMonth() + 1}.${day.getUTCDate()}`
 console.log(JSON.stringify(manifest, null, 4))
