c956de5edb60133bb658fa91d1bb8e962e2c6ac2
Sam Macbeth <smacbeth@duckduckgo.com>
2021-03-04 12:53:27 +0100

    Add beta to manifest name

--- scripts/nightly-manifest.js
+++ scripts/nightly-manifest.js
@@ -7,6 +7,7 @@ gecko.update_url = 'https://sammacbeth.github.io/duckduckgo-privacy-extension/up
 
 const day = new Date()
 const versionRegex = /^[0-9]{4}.[0-9]{1,2}.[0-9]{1,2}$/
+manifest.name += ' (Beta)'
 manifest.version = process.env.VERSION || `${day.getUTCFullYear()}.${day.getUTCMonth() + 1}.${day.getUTCDate()}`
 if (process.env.GITHUB_REF && process.env.GITHUB_REF.startsWith('refs/tags/')) {
     // in CI we pull the version from the tag ref
