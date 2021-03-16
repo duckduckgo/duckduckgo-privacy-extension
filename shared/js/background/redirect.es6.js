64b4d292aa4e05c8ba25199148ad2386eb5c41a3
Jason <jason@duckduckgo.com>
2021-03-16 14:32:24 -0600

    Move surrogates to web accessible resources (#550)
    * split up surrogates
    
    * split up surrogates, add web resource access to tab
    
    * comments
    
    * handle redirect in onBeforeSendHeaders for some surrogates
    
    * test locally
    
    * update makefile
    
    * emptylines
    
    * comment
    
    * comment
    
    * comment
    
    * remove old surrogates
    
    * update resources, and makefile to format them
    
    * lint
    
    * Update shared/js/background/redirect.es6.js
    
    Co-authored-by: Sam Macbeth <sammacbeth@users.noreply.github.com>
    
    * update makefile and surrogates
    
    * fix makefile
    
    * match on origin
    
    * linter fixes
    
    Co-authored-by: Jason <jason@Jasons-MacBook-Pro.local>
    Co-authored-by: Sam Macbeth <sammacbeth@users.noreply.github.com>

--- shared/js/background/redirect.es6.js
+++ shared/js/background/redirect.es6.js
@@ -6,6 +6,8 @@ const tabManager = require('./tab-manager.es6')
 const ATB = require('./atb.es6')
 const browserWrapper = require('./$BROWSER-wrapper.es6')
 const settings = require('./settings.es6')
+const webResourceURL = browserWrapper.getExtensionURL('/web_accessible_resources')
+const browser = utils.getBrowserName()
 
 const debugRequest = false
 
@@ -42,6 +44,13 @@ function handleRequest (requestData) {
     if (tabId === -1) { return }
 
     let thisTab = tabManager.get(requestData)
+
+    // control access to web accessible resources
+    if (requestData.url.startsWith(webResourceURL)) {
+        if (!thisTab || !thisTab.hasWebResourceAccess(requestData.url)) {
+            return { cancel: true }
+        }
+    }
 
     // For main_frame requests: create a new tab instance whenever we either
     // don't have a tab instance for this tabId or this is a new requestId.
@@ -145,9 +154,17 @@ function handleRequest (requestData) {
                 // return surrogate redirect if match, otherwise
                 // tell Chrome to cancel this webrequest
                 if (tracker.redirectUrl) {
-                    // safari gets return data in message
-                    requestData.message = { redirectUrl: tracker.redirectUrl }
-                    return { redirectUrl: tracker.redirectUrl }
+                    const webResource = browserWrapper.getExtensionURL(`web_accessible_resources/${tracker.matchedRule.surrogate}`)
+                    // Firefox: check these for Origin headers in onBeforeSendHeaders before redirecting or not. Workaround for
+                    // https://bugzilla.mozilla.org/show_bug.cgi?id=1694679
+                    // Surrogates that for sure need to load should have 'strictRedirect' set, and will have their headers checked
+                    // in onBeforeSendHeaders
+                    if (tracker.matchedRule.strictRedirect && browser === 'moz') {
+                        thisTab.surrogates[requestData.url] = webResource
+                    } else {
+                        const key = thisTab.addWebResourceAccess(webResource)
+                        return { redirectUrl: `${webResource}?key=${key}` }
+                    }
                 } else {
                     requestData.message = { cancel: true }
                     return { cancel: true }
