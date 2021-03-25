404cfe15444da33fec27c302119b7b48aacd71ce
Brad Slayter <slayterdevelopment@gmail.com>
2021-03-25 11:37:45 -0500

    Condition change

--- shared/js/content-scripts/block-cookie.js
+++ shared/js/content-scripts/block-cookie.js
@@ -129,11 +129,11 @@
         checkThirdParty: true,
         documentUrl: window.location.href
     }, function (action) {
-        if (!action || !action.shouldBlock || !action.isThirdParty) {
+        if (!action) {
             // action is undefined if the background has not yet registered the message listener
             return
         }
-        if (window.top !== window && action.isTrackerFrame) {
+        if (window.top !== window && action.isTrackerFrame && action.shouldBlock && action.isThirdParty) {
             // overrides expiry policy with blocking - only in subframes
             inject(blockCookies)
         }
