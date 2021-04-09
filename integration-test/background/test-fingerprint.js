0889ce1bb6e6ff763239f3ef08e07cbc253d3289
Sam Macbeth <smacbeth@duckduckgo.com>
2021-04-09 15:27:24 +0200

    Fix fingerprint comparison in tests
    FingerprintJS result is an object which is not hashable, so the set
    comparison was not working.

--- integration-test/background/test-fingerprint.js
+++ integration-test/background/test-fingerprint.js
@@ -140,8 +140,8 @@ describe('First Party Fingerprint Randomization', () => {
             const result = await runTest(testCase)
 
             // Add the fingerprints to a set, if the result doesn't match it won't be added
-            canvas.add(result.canvas)
-            plugin.add(result.plugin)
+            canvas.add(JSON.stringify(result.canvas))
+            plugin.add(JSON.stringify(result.plugin))
         }
 
         // Ensure that the number of test pages match the number in the set
