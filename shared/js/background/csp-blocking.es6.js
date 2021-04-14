c926bdf3f515cd19313e57fbfbb1340657cd46fd
Sam Macbeth <sammacbeth@users.noreply.github.com>
2021-04-14 16:00:21 +0200

    Block CSP reports that contain a moz-extension URL (#600)
    * Block CSP reports that contain a moz-extension URL
    
    * Update shared/js/background/csp-blocking.es6.js
    
    Co-authored-by: Konrad Dzwinel <kdzwinel@gmail.com>
    
    Co-authored-by: Konrad Dzwinel <kdzwinel@gmail.com>

---
+++ shared/js/background/csp-blocking.es6.js
@@ -0,0 +1,20 @@
+function init () {
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        try {
+            // parse requestBody as an ASCII string
+            const report = String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes))
+            if (report.indexOf('moz-extension://') !== -1) {
+                return { cancel: true }
+            }
+        } catch (e) {
+            console.warn('Unable to parse CSP report contents', details.url)
+        }
+    }, {
+        urls: ['<all_urls>'],
+        types: ['csp_report']
+    }, ['blocking', 'requestBody'])
+}
+
+module.exports = {
+    init
+}
