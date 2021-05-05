bf55ba1c3065c9d507980805a9b6f7d25cb94b44
Konrad Dzwinel <konrad@duckduckgo.com>
2021-05-05 13:21:54 +0200

    Fix bolding on "Added to unprotected sites" message.

--- shared/js/ui/templates/site.es6.js
+++ shared/js/ui/templates/site.es6.js
@@ -42,7 +42,7 @@ module.exports = function () {
         </li>
         <li class="site-info__li--toggle padded ${this.model.isWhitelisted ? '' : 'is-active'}">
             <p class="is-transparent site-info__whitelist-status js-site-whitelist-status">
-                <span class="text-line-after-icon privacy-on-off-message">
+                <span class="text-line-after-icon privacy-on-off-message bold">
                     ${setTransitionText(!this.model.isWhitelisted)}
                 </span>
             </p>
