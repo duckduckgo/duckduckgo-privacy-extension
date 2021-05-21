30e7d1645afa1b0526e24d1160176a04b349fb66
Jonathan Kingston <jonathan@jooped.co.uk>
2021-05-21 12:40:40 +0100

    Wrap floc code in try (#672)
    Fixes: #671
    
    Co-authored-by: Jonathan Kingston <jkingston@duckduckgo.com>

--- shared/js/content-scope/floc-protection.js
+++ shared/js/content-scope/floc-protection.js
@@ -1,5 +1,9 @@
 export function init () {
     if ('interestCohort' in Document.prototype) {
-        delete Document.prototype.interestCohort
+        try {
+            delete Document.prototype.interestCohort
+        } catch {
+            // Throw away this exception, it's likely a confict with another extension
+        }
     }
 }
