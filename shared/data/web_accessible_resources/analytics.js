bed17177d7bcada36b45581cfa08621f679ebe44
Jonathan Kingston <jonathan@jooped.co.uk>
2021-05-25 22:45:43 +0100

    Support getAll to get a Tracker object (#679)
    Co-authored-by: Jonathan Kingston <jkingston@duckduckgo.com>

--- shared/data/web_accessible_resources/analytics.js
+++ shared/data/web_accessible_resources/analytics.js
@@ -26,11 +26,28 @@
             }
         })
     }
+    const Tracker = new Proxy({}, {
+        get(target, prop) {
+            if (prop === 'get') {
+                return (fieldName) => {
+                    if (fieldName === 'linkerParam') {
+                        // This fixed string is an example value of this API.
+                        // As the extension exposes itself with many featues we shouldn't be concerned by exposing ourselves here also.
+                        // If we randomised this to some other fake value there wouldn't be much benefit and could risk being a tracking vector.
+                        // https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#linkerParam
+                        return '_ga=1.231587807.1974034684.1435105198';
+                    }
+                    return 'something'
+                };
+            }
+            return noop;
+        }
+    });
     ga.answer = 42
     ga.loaded = true
     ga.create = function() { return new Proxy({}, noopHandler) }
     ga.getByName = noopReturnNull
-    ga.getAll = noopReturnArray
+    ga.getAll = function() { return [Tracker] }
     ga.remove = noop
     window[gaPointer] = ga
     // prevent page delay, see https://developers.google.com/optimize
