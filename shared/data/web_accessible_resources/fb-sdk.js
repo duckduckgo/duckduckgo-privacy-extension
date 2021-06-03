1e32aac9f6303edb96b7e2e135108bed646d5f8f
Charlie <44004366+Charlie-belmer@users.noreply.github.com>
2021-06-03 14:19:20 -0400

    Remove remote styling & fix comment issue on some sites (#692)
    * Remove remote styling & fix comment issue on some sites
    
    * Add comment explaining loader

--- shared/data/web_accessible_resources/fb-sdk.js
+++ shared/data/web_accessible_resources/fb-sdk.js
@@ -5,6 +5,7 @@
     let siteInit = function () {}
     let fbIsEnabled = false
     let initData = {}
+    let runInit = false
     const parseCalls = []
     const popupName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 12)
 
@@ -25,12 +26,25 @@
         dispatchEvent(event)
     }
 
+    /**
+     * When setting up the Facebook SDK, the site may define a function called window.fbAsyncInit.
+     * Once the SDK loads, it searches for and calls window.fbAsyncInit. However, some sites may
+     * not use this, and just call FB.init directly at some point (after ensuring that the script has loaded).
+     *
+     * Our surrogate (defined below in window.FB) captures calls made to init by page scripts. If at a
+     * later point we load the real sdk here, we then re-call init with whatever arguments the page passed in
+     * originally. The runInit param should be true when a page has called init directly.
+     * Because we put it in asyncInit, the flow will be something like:
+     *
+     * FB SDK loads -> SDK calls window.fbAsyncInit -> Our function calls window.FB.init (maybe) ->
+     * our function calls original fbAsyncInit (if it existed)
+     */
     function enableFacebookSDK () {
         if (!fbIsEnabled) {
             window.FB = undefined
 
             window.fbAsyncInit = function () {
-                if (initData) {
+                if (runInit && initData) {
                     window.FB.init(initData)
                 }
                 siteInit()
@@ -40,6 +54,9 @@
             }
 
             const fbScript = document.createElement('script')
+            fbScript.setAttribute('crossorigin', 'anonymous')
+            fbScript.setAttribute('async', '')
+            fbScript.setAttribute('defer', '')
             fbScript.src = originalFBURL
             fbScript.onload = function () {
                 for (const node of parseCalls) {
@@ -48,6 +65,10 @@
             }
             document.head.appendChild(fbScript)
             fbIsEnabled = true
+        } else {
+            if (initData) {
+                window.FB.init(initData)
+            }
         }
     }
 
@@ -92,6 +113,7 @@
             init: function (obj) {
                 if (obj) {
                     initData = obj
+                    runInit = true
                     messageAddon({
                         'appID': obj.appId
                     })
