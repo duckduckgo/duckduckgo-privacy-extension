82c6ff9bd4f2c3c501f402b0b58cde22b5403d08
Sam Macbeth <smacbeth@duckduckgo.com>
2021-06-24 12:45:11 +0200

    Site breakage form updates
     * Make breakage report category optional
     * Update category list

--- shared/js/ui/templates/breakage-form.es6.js
+++ shared/js/ui/templates/breakage-form.es6.js
@@ -1,12 +1,10 @@
 const bel = require('bel')
 const categories = [
-    { category: 'Video didn\'t play', value: 'videos' },
-    { category: 'Images didn\'t load', value: 'images' },
-    { category: 'Comments didn\'t load', value: 'comments' },
+    { category: 'Video or images didn\'t load', value: 'images' },
     { category: 'Content is missing', value: 'content' },
     { category: 'Links or buttons don\'t work', value: 'links' },
-    { category: 'I can\'t sign in', value: 'login' },
-    { category: 'The site asked me to disable', value: 'paywall' }
+    { category: 'Can\'t sign in', value: 'login' },
+    { category: 'Site asked me to disable the extension', value: 'paywall' }
 ]
 
 function shuffle (arr) {
@@ -35,15 +33,14 @@ module.exports = function () {
         <div class="breakage-form__element js-breakage-form-element">
             <h2 class="breakage-form__title">Something broken?</h2>
             <div class="breakage-form__explanation">Submitting an anonymous broken site report helps us debug these issues and improve the extension.</div>
-            <div class="form__label__select">Describe what happened</div>
             <div class="form__select breakage-form__input--dropdown">
                 <select class="js-breakage-form-dropdown">
-                    <option value=''>Pick your issue from the list...</option>
+                    <option value='unspecified' disabled selected>Select a category (optional)</option>
                     ${shuffle(categories).map(function (item) { return bel`<option value=${item.value}>${item.category}</option>` })}
-                    <option value='Other'>Something else</option>
+                    <option value='other'>Something else</option>
                 </select>
             </div>
-            <btn class="form__submit js-breakage-form-submit btn-disabled" role="button" disabled="true">Send report</btn>
+            <btn class="form__submit js-breakage-form-submit" role="button">Send report</btn>
             <div class="breakage-form__footer">Reports sent to DuckDuckGo are 100% anonymous and only include your selection above, the URL, and a list of trackers we found on the site.</div>
         </div>
         <div class="breakage-form__message js-breakage-form-message is-transparent">
