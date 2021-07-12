ddc0438d07f01545387dcd5f74bf186bd1ac564a
Sam Macbeth <smacbeth@duckduckgo.com>
2021-07-12 14:43:51 +0200

    Make action handler pluggable.

--- shared/js/devtools/panel.es6.js
+++ shared/js/devtools/panel.es6.js
@@ -24,6 +24,89 @@ const actionIcons = {
     ignore: '⚠️'
 }
 
+const actionHandlers = {
+    tracker: (m) => {
+        const { tracker, url, requestData, siteUrl } = m.message
+        const row = document.getElementById('request-row').content.firstElementChild.cloneNode(true)
+        const cells = row.querySelectorAll('td')
+        const toggleLink = row.querySelector('.block-toggle')
+        toggleLink.href = ''
+        if (tracker.action === 'block') {
+            toggleLink.innerText = 'I'
+        } else {
+            toggleLink.innerText = 'B'
+        }
+        toggleLink.addEventListener('click', (ev) => {
+            ev.preventDefault()
+            port.postMessage({
+                action: toggleLink.innerText,
+                tabId,
+                tracker,
+                requestData,
+                siteUrl
+            })
+            row.classList.remove(tracker.action)
+            row.classList.add(toggleLink.innerText === 'I' ? 'ignore' : 'block')
+        });
+        [url, `${actionIcons[tracker.action]} ${tracker.action} (${tracker.reason})`, tracker.fullTrackerDomain, requestData.type].forEach((text, i) => {
+            cells[i + 1].innerText = text
+        })
+        row.classList.add(tracker.action)
+        table.appendChild(row)
+    },
+    tabChange: (m) => {
+        const tab = m.message
+        protectionButton.innerText = `Protection: ${tab.site?.whitelisted || tab.site?.isBroken ? 'OFF' : 'ON'}`
+        Object.keys(features).forEach((feature) => {
+            document.getElementById(feature).innerText = `${feature}: ${tab.site?.brokenFeatures.includes(features[feature]) ? 'OFF' : 'ON'}`
+        })
+    },
+    cookie: (m) => {
+        const { action, kind, url, requestId, type } = m.message
+        const rowId = `request-${requestId}`
+        if (document.getElementById(rowId) !== null) {
+            const row = document.getElementById(rowId)
+            const cells = row.querySelectorAll('td')
+            row.classList.add(kind)
+            cells[3].textContent = `${cells[3].textContent}, ${kind}`
+        } else {
+            const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
+            row.id = rowId
+            const cells = row.querySelectorAll('td')
+            const cleanUrl = new URL(url)
+            cleanUrl.search = ''
+            cleanUrl.hash = ''
+            cells[1].textContent = cleanUrl.href
+            cells[2].textContent = `🍪 ${action}`
+            cells[3].textContent = kind
+            cells[4].textContent = type
+            row.classList.add(kind)
+            table.appendChild(row)
+        }
+    },
+    jscookie: (m) => {
+        const { documentUrl, action, reason, value, scriptOrigins } = m.message
+        const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
+        const cells = row.querySelectorAll('td')
+        cells[1].textContent = documentUrl
+        cells[2].textContent = `JS🍪 ${action} (${reason})`
+        cells[3].textContent = scriptOrigins.join(',')
+        cells[4].textContent = value.split(';')[0]
+        row.classList.add('jscookie')
+        table.appendChild(row)
+    },
+    canvas: (m) => {
+        const { documentUrl, action, kind } = m.message
+        const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
+        const cells = row.querySelectorAll('td')
+        cells[1].textContent = documentUrl
+        cells[2].textContent = `Canvas ${action}`
+        cells[3].textContent = kind
+        row.classList.add('canvas')
+        table.appendChild(row)
+    }
+}
+
 function shouldShowRow (className) {
     const filter = document.getElementById(`display-${className}`)
     return !filter || filter.checked
@@ -36,83 +119,12 @@ function setRowVisible (row) {
 port.onMessage.addListener((message) => {
     const m = JSON.parse(message)
     if (m.tabId === tabId) {
-        if (m.action === 'tracker') {
-            const { tracker, url, requestData, siteUrl } = m.message
-            const row = document.getElementById('request-row').content.firstElementChild.cloneNode(true)
-            const cells = row.querySelectorAll('td')
-            const toggleLink = row.querySelector('.block-toggle')
-            toggleLink.href = ''
-            if (tracker.action === 'block') {
-                toggleLink.innerText = 'I'
-            } else {
-                toggleLink.innerText = 'B'
-            }
-            toggleLink.addEventListener('click', (ev) => {
-                ev.preventDefault()
-                port.postMessage({
-                    action: toggleLink.innerText,
-                    tabId,
-                    tracker,
-                    requestData,
-                    siteUrl
-                })
-                row.classList.remove(tracker.action)
-                row.classList.add(toggleLink.innerText === 'I' ? 'ignore' : 'block')
-            });
-            [url, `${actionIcons[tracker.action]} ${tracker.action} (${tracker.reason})`, tracker.fullTrackerDomain, requestData.type].forEach((text, i) => {
-                cells[i + 1].innerText = text
-            })
-            row.classList.add(tracker.action)
-            table.appendChild(row)
-        } else if (m.action === 'tabChange') {
-            const tab = m.message
-            protectionButton.innerText = `Protection: ${tab.site?.whitelisted || tab.site?.isBroken ? 'OFF' : 'ON'}`
-            Object.keys(features).forEach((feature) => {
-                document.getElementById(feature).innerText = `${feature}: ${tab.site?.brokenFeatures.includes(features[feature]) ? 'OFF' : 'ON'}`
-            })
-        } else if (m.action === 'cookie') {
-            const { action, kind, url, requestId, type } = m.message
-            const rowId = `request-${requestId}`
-            if (document.getElementById(rowId) !== null) {
-                const row = document.getElementById(rowId)
-                const cells = row.querySelectorAll('td')
-                row.classList.add(kind)
-                cells[3].textContent = `${cells[3].textContent}, ${kind}`
-            } else {
-                const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
-                row.id = rowId
-                const cells = row.querySelectorAll('td')
-                const cleanUrl = new URL(url)
-                cleanUrl.search = ''
-                cleanUrl.hash = ''
-                cells[1].textContent = cleanUrl.href
-                cells[2].textContent = `🍪 ${action}`
-                cells[3].textContent = kind
-                cells[4].textContent = type
-                row.classList.add(kind)
-                table.appendChild(row)
-            }
-        } else if (m.action === 'jscookie') {
-            const { documentUrl, action, reason, value, scriptOrigins } = m.message
-            const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
-            const cells = row.querySelectorAll('td')
-            cells[1].textContent = documentUrl
-            cells[2].textContent = `JS🍪 ${action} (${reason})`
-            cells[3].textContent = scriptOrigins.join(',')
-            cells[4].textContent = value.split(';')[0]
-            row.classList.add('jscookie')
-            table.appendChild(row)
-        } else if (m.action === 'canvas') {
-            const { documentUrl, action, kind } = m.message
-            const row = document.getElementById('cookie-row').content.firstElementChild.cloneNode(true)
-            const cells = row.querySelectorAll('td')
-            cells[1].textContent = documentUrl
-            cells[2].textContent = `Canvas ${action}`
-            cells[3].textContent = kind
-            row.classList.add('canvas')
-            table.appendChild(row)
+        if (actionHandlers[m.action]) {
+            actionHandlers[m.action](m)
         }
-        setRowVisible(document.querySelector('tbody').lastChild)
+        if (document.querySelector('tbody').lastChild) {
+            setRowVisible(document.querySelector('tbody').lastChild)
+        }
     }
 })
 
