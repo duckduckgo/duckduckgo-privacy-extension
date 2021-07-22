f05e781cf6777b359f9745ec67a7eb1a3ae843c0
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-07-22 11:42:27 +0200

    Update Email Protection section in settings
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- shared/js/ui/templates/user-data.es6.js
+++ shared/js/ui/templates/user-data.es6.js
@@ -2,26 +2,26 @@ const bel = require('bel')
 const { formatAddress } = require('../../background/email-utils.es6')
 
 module.exports = function () {
-    // We don't render anything if the user is not set
-    if (!this.model.userName) return bel`<div></div>`
-
     return bel`<section class="options-content__user-data divider-bottom">
-        <h2 class="menu-title">Email Autofill</h2>
+        <h2 class="menu-title">Email Protection</h2>
         ${renderUserDataContent(this.model)}
     </section>`
 }
 
 function renderUserDataContent (model) {
-    return model.loggingOut
+    return (!model.userName)
         ? bel`<div>
-                <p class="menu-paragraph">Disabled successfully.</p>
+                <p class="menu-paragraph">Off</p>
+                <p class="options-info">
+                    <a href="https://duckduckgo.com/email/enable-autofill">Add Duck Address</a>
+                </p>
             </div>`
         : bel`<div>
                 <p class="menu-paragraph">
-                    Enabled for <strong class="js-userdata-container">${formatAddress(model.userName)}</strong>
+                    <span class="js-userdata-container">${formatAddress(model.userName)}</span>
                 </p>
                 <p class="options-info js-userdata-logout">
-                    <a href="#">Disable</a>
+                    <a href="#">Remove from browser</a>
                 </p>
             </div>`
 }
