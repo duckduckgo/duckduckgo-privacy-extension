ecea21436d3ce53f9f673c1b0e240064e7b695ea
jdorweiler <jason@duckduckgo.com>
2021-07-27 09:21:40 -0600

    more tests

--- unit-test/background/tracker-allowlist-tests.js
+++ unit-test/background/tracker-allowlist-tests.js
@@ -11,6 +11,24 @@ const allowListTests = {
                 isAllowlisted: true
             },
             {
+                name: 'should match on all subdomains of an allowlisted site',
+                site: 'https://a.b.c.testsite.com',
+                request: 'https://allowlist-tracker-1.com/videos.js',
+                isAllowlisted: true
+            },
+            {
+                name: 'should match on all subdomains of an allowlisted tracker',
+                site: 'https://testsite.com',
+                request: 'https://a.b.c.allowlist-tracker-1.com/videos.js',
+                isAllowlisted: true
+            },
+            {
+                name: 'should not match on a substring of the domain',
+                site: 'https://anothertestsite.com',
+                request: 'https://allowlist-tracker-1.com/videos.js',
+                isAllowlisted: false
+            },
+            {
                 name: 'should not match on a site not listed in the allowlist entry domains list',
                 site: 'https://testsite2.com',
                 request: 'https://allowlist-tracker-1.com/videos.js',
@@ -20,6 +38,12 @@ const allowListTests = {
                 name: 'should match query strings',
                 site: 'https://testsite.com',
                 request: 'https://allowlist-tracker-1.com/videos.js?a=123&b=456',
+                isAllowlisted: true
+            },
+            {
+                name: 'should match random paths',
+                site: 'https://someothersite.com',
+                request: 'https://allowlist-tracker-2.com/comments/1234asdf/comment.js',
                 isAllowlisted: true
             }
         ]
