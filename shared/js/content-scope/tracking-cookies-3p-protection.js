904a78103f0f4612ea231bf83216cab88494caec
Sam Macbeth <smacbeth@duckduckgo.com>
2021-08-11 17:44:26 +0200

    Support JS cookie blocking in debugger panel

--- shared/js/content-scope/tracking-cookies-3p-protection.js
+++ shared/js/content-scope/tracking-cookies-3p-protection.js
@@ -1,18 +1,39 @@
-import { defineProperty } from './utils'
+import { defineProperty, postDebugMessage } from './utils'
 
-function blockCookies () {
+function blockCookies (debug) {
     // disable setting cookies
     defineProperty(document, 'cookie', {
         configurable: false,
-        set: function (value) { },
-        get: () => ''
+        set: function (value) {
+            if (debug) {
+                postDebugMessage('jscookie', {
+                    action: 'block',
+                    reason: 'tracker frame',
+                    documentUrl: document.location.href,
+                    scriptOrigins: [],
+                    value: value
+                })
+            }
+        },
+        get: () => {
+            if (debug) {
+                postDebugMessage('jscookie', {
+                    action: 'block',
+                    reason: 'tracker frame',
+                    documentUrl: document.location.href,
+                    scriptOrigins: [],
+                    value: 'getter'
+                })
+            }
+            return ''
+        }
     })
 }
 
 export function init (args) {
     args.cookie.debug = args.debug
     if (window.top !== window && args.cookie.isTrackerFrame && args.cookie.shouldBlock && args.cookie.isThirdParty) {
-        // overrides expiry policy with blocking - only in subframes
-        blockCookies()
+        // overrides expiry policy with blocking - only in subframesw
+        blockCookies(args.debug)
     }
 }
