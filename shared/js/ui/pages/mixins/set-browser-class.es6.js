8efffd94e501e3c81142b7b24ff9729ef0a0c4ce
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-08-11 09:31:04 +0200

    Temporary fix for Safari
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- shared/js/ui/pages/mixins/set-browser-class.es6.js
+++ shared/js/ui/pages/mixins/set-browser-class.es6.js
@@ -4,6 +4,9 @@ module.exports = {
             if (['edg', 'edge', 'brave'].includes(browser)) {
                 browser = 'chrome'
             }
+            // TEMPORARY FIX FOR SAFARI
+            if (browser === undefined) browser = 'chrome'
+
             const browserClass = 'is-browser--' + browser
             window.$('html').addClass(browserClass)
             window.$('body').addClass(browserClass)
