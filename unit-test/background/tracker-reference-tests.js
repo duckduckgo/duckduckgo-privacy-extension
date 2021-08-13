7ae937bb05c01486c8960b7b95d61a7a56eb99ad
Lucas Adamski <ladamski@users.noreply.github.com>
2021-08-13 09:53:24 -0500

    support surrogate ref tests (#800)
    * support surrogate ref tests
    
    * lint nits
    
    * Update reference-tests
    
    Co-authored-by: Lucas Adamski <ladamski@duckduckgo.com>

--- unit-test/background/tracker-reference-tests.js
+++ unit-test/background/tracker-reference-tests.js
@@ -1,45 +1,41 @@
 const tds = require('../../shared/js/background/trackers.es6')
 const tdsStorageStub = require('./../helpers/tds.es6')
-const settings = require('../../shared/js/background/settings.es6')
 
-const trackers = require('./reference-tests/tracker-radar-tests/TR-domain-matching/tracker_radar_reference.json')
+const refTrackers = require('./reference-tests/tracker-radar-tests/TR-domain-matching/tracker_radar_reference.json')
 const refTests = require('./reference-tests/tracker-radar-tests/TR-domain-matching/domain_matching_tests.json')
+const refSurrogates = require('./reference-tests/tracker-radar-tests/TR-domain-matching/surrogates.js')
 
 describe('Tracker reference tests:', () => {
-    let settingsObserver
-    let timer = Date.now()
-    const jump = 1000 * 60 * 31 // slightly more than cache timeout
-
     beforeAll(() => {
-        settingsObserver = spyOn(settings, 'getSetting')
         tdsStorageStub.stub()
+
         const testLists = [{
             name: 'tds',
-            data: trackers
+            data: refTrackers
+        },
+        {
+            name: 'surrogates',
+            data: refSurrogates.surrogates
         }]
-        tds.setLists(testLists)
-
-        // Make sure we don't use any list caches for these tests
-        spyOn(Date, 'now').and.callFake(function () {
-            // Cache may be updated on each run, so keep jumping the time forward for each call
-            timer += jump
-            return timer
-        })
-
-        settings.updateSetting('trackerBlockingEnabled', true)
-        settingsObserver.and.returnValue(undefined)
+        return tds.setLists(testLists)
     })
 
     const domainTests = refTests.domainTests.tests
+    const surrogateTests = refTests.surrogateTests.tests
+    const tests = domainTests.concat(surrogateTests)
 
-    for (const test of domainTests) {
+    for (const test of tests) {
         it(`${test.name}`, () => {
             const rootURL = test.siteURL
             const requestURL = test.requestURL
             const requestType = test.requestType
+            const expectRedirect = test.expectRedirect
             const result = tds.getTrackerData(requestURL, rootURL, { type: requestType })
             const action = (result && result.action)
             expect(test.expectAction).toEqual(action)
+            if (expectRedirect) {
+                expect(expectRedirect).toEqual(result.redirectUrl)
+            }
         })
     }
 })
