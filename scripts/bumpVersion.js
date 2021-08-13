abf1d62826dff90d337d09afd2e2dd363c9a7aa2
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-08-13 14:28:14 +0200

    Bump version (#806)
    * Update privacy config
    * Bump version
    * Use all dots in version (dashes not allowed)

--- scripts/bumpVersion.js
+++ scripts/bumpVersion.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const time = new Date();
 
 const seconds = (time.getUTCHours() * 60 * 60) + (time.getUTCMinutes() * 60) + time.getUTCSeconds();
-const newVersion = `${time.getUTCFullYear()}.${time.getUTCMonth()+1}.${time.getUTCDate()}-${seconds}`
+const newVersion = `${time.getUTCFullYear()}.${time.getUTCMonth()+1}.${time.getUTCDate()}.${seconds}`
 
 for (let browser of ['chrome', 'firefox']) {
     const manifest = fs.readFileSync(`browsers/${browser}/manifest.json`, 'utf8')
