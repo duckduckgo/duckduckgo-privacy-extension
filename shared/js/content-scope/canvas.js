b17ae26a65f5a29a2726b3303ca257a584f27182
Jonathan Kingston <jkingston@duckduckgo.com>
2021-08-13 09:49:11 +0100

    Windowing canvas

--- shared/js/content-scope/canvas.js
+++ shared/js/content-scope/canvas.js
@@ -1,10 +1,10 @@
 import { iterateDataKey, getDataKeySync } from './utils'
 
-export function computeOffScreenCanvas (canvas, domainKey, sessionKey, getImageDataProxy) {
+export function computeOffScreenCanvas (canvas, domainKey, sessionKey, getImageDataProxy, canvasKey) {
     const ctx = canvas.getContext('2d')
     // We *always* compute the random pixels on the complete pixel set, then pass back the subset later
     let imageData = getImageDataProxy._native.apply(ctx, [0, 0, canvas.width, canvas.height])
-    imageData = modifyPixelData(imageData, sessionKey, domainKey)
+    imageData = modifyPixelData(imageData, sessionKey, domainKey, canvasKey)
 
     // Make a off-screen canvas and put the data there
     const offScreenCanvas = document.createElement('canvas')
@@ -16,11 +16,54 @@ export function computeOffScreenCanvas (canvas, domainKey, sessionKey, getImageD
     return { offScreenCanvas, offScreenCtx }
 }
 
-export function modifyPixelData (imageData, domainKey, sessionKey) {
+export function modifyPixelData (imageData, domainKey, sessionKey, canvasKey) {
+    const length = imageData.data.length / 4
+    const windowSize = 1000
+    let windows = Math.ceil(length / windowSize)
+    let remainder = length % windowSize
+    for (let windowNumber = 0; windowNumber < windows; windowNumber++) {
+        let windowHash = getDataKeySync(sessionKey, domainKey, canvasKey + windowNumber)
+        let windowLength = windowSize
+        if (windowNumber === windows - 1) {
+            windowLength = remainder
+        }
+
+        let hits = 0
+        let ignored = 0
+        iterateDataKey(windowHash, (item, byte) => {
+            const channel = byte % 3
+            const lookupId = item % windowLength
+            const pixelCanvasIndex = (windowNumber * windowSize) + lookupId + channel
+            const pixelData = imageData.data[pixelCanvasIndex];
+            const d = imageData.data;
+    
+            if (d[lookupId + 1] === 0 && d[lookupId + 2] === 0) {
+                // Ignore non blank pixels there is high chance compression ignores them
+                if (d[lookupId] === 0 && d[lookupId + 3] === 0) {
+    ++ignored
+                    return
+                }
+                // Ignore phaser background
+                if (d[lookupId] === 255 && d[lookupId + 3] === 255) {
+    ++ignored
+                    return
+                }
+            }
+    
+            ++hits
+            imageData.data[pixelCanvasIndex] = imageData.data[pixelCanvasIndex] ^ (byte & 0x1)
+        })
+        if (hits < 10) {
+          // DO no hot path code.
+//console.log({hits, ignored, windows});
+        }
+    }
+
+    return imageData
+}
+
+function produceFilterMap() {
     const arr = []
-    // We calculate a checksum as passing imageData as a key is too slow.
-    // We might want to do something more pseudo random that is less observable through timing attacks and collisions (but this will come at a performance cost)
-    let checkSum = 0
     // Create an array of only pixels that have data in them
     const d = imageData.data
     for (let i = 0; i < d.length; i += 4) {
@@ -35,20 +78,6 @@ export function modifyPixelData (imageData, domainKey, sessionKey) {
                 continue
             }
         }
-        // Add to the checksum the R,G,B,A values
-        checkSum += d[i] + d[i + 1] + d[i + 2] + d[i + 3]
-        arr.push(i)
     }
-
-    const canvasKey = getDataKeySync(sessionKey, domainKey, checkSum)
     const length = arr.length
-    iterateDataKey(canvasKey, (item, byte) => {
-        const channel = byte % 3
-        const lookupId = item % length
-        const pixelCanvasIndex = arr[lookupId] + channel
-
-        imageData.data[pixelCanvasIndex] = imageData.data[pixelCanvasIndex] ^ (byte & 0x1)
-    })
-
-    return imageData
 }
