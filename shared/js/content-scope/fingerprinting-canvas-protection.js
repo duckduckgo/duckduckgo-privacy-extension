cd4ccf0a188314f9fb277f2367ddc6a6eab43199
Jonathan Kingston <jkingston@duckduckgo.com>
2021-08-13 09:49:11 +0100

    Windowing canvas

--- shared/js/content-scope/fingerprinting-canvas-protection.js
+++ shared/js/content-scope/fingerprinting-canvas-protection.js
@@ -1,4 +1,4 @@
-import { DDGProxy, DDGReflect } from './utils'
+import { DDGProxy, DDGReflect, getDataKeySync } from './utils'
 import { computeOffScreenCanvas } from './canvas'
 
 export function init (args) {
@@ -7,19 +7,30 @@ export function init (args) {
     const featureName = 'fingerprinting-canvas'
 
     const unsafeCanvases = new WeakSet()
+    const canvasMetadata = new WeakMap()
 
-    if (args.debug) {
+    function updateHash(canvas, args) {
+        // Add support for other data types like image and ImageData.
+        let stringified = JSON.stringify(args);
+        let existingHash = canvasMetadata.get(canvas) || ''
+        let newHash = getDataKeySync(sessionKey, domainKey, existingHash + stringified)
+        canvasMetadata.set(canvas, newHash)
+        console.log({stringified, args, newHash, existingHash}) 
+    }
+
+    //if (args.debug) {
         // Debugging of canvas methods
         const debuggingMethods = ['putImageData', 'drawImage']
         for (const methodName of debuggingMethods) {
             const debuggingProxy = new DDGProxy(featureName, CanvasRenderingContext2D.prototype, methodName, {
                 apply (target, thisArg, args) {
+                    updateHash(thisArg.canvas, args) 
                     return DDGReflect.apply(target, thisArg, args)
                 }
             })
             debuggingProxy.overload()
         }
-    }
+    //}
 
 
     // Include all these: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
@@ -44,6 +55,7 @@ export function init (args) {
         const unsafeProxy = new DDGProxy(featureName, CanvasRenderingContext2D.prototype, methodName, {
             apply (target, thisArg, args) {
                 unsafeCanvases.add(thisArg.canvas)
+                updateHash(thisArg.canvas, args) 
                 return DDGReflect.apply(target, thisArg, args)
             }
         })
@@ -58,7 +70,8 @@ export function init (args) {
             }
             // Anything we do here should be caught and ignored silently
             try {
-                const { offScreenCtx } = computeOffScreenCanvas(thisArg.canvas, domainKey, sessionKey, getImageDataProxy)
+                const canvasKey = canvasMetadata.get(thisArg.canvas)
+                const { offScreenCtx } = computeOffScreenCanvas(thisArg.canvas, domainKey, sessionKey, getImageDataProxy, canvasKey)
                 // Call the original method on the modified off-screen canvas
                 return DDGReflect.apply(target, offScreenCtx, args)
             } catch {
@@ -78,10 +91,12 @@ export function init (args) {
                     return DDGReflect.apply(target, thisArg, args)
                 }
                 try {
-                    const { offScreenCanvas } = computeOffScreenCanvas(thisArg, domainKey, sessionKey, getImageDataProxy)
+                    const canvasKey = canvasMetadata.get(thisArg) || ""
+                    const { offScreenCanvas } = computeOffScreenCanvas(thisArg, domainKey, sessionKey, getImageDataProxy, canvasKey)
                     // Call the original method on the modified off-screen canvas
                     return DDGReflect.apply(target, offScreenCanvas, args)
-                } catch {
+                } catch (e) {
+//console.log(e);
                     // Something we did caused an exception, fall back to the native
                     return DDGReflect.apply(target, thisArg, args)
                 }
