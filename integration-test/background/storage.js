d4b1d7e583f729235c7ea4f921d7b12ede9793d9
Sam Macbeth <sammacbeth@users.noreply.github.com>
2021-08-17 16:22:04 +0200

    Tests for 3rd party JS cookie blocking (#799)
    * Add tests for 3rd party JS cookie blocking
    
    * Support JS cookie blocking in debugger panel
    
    * Remove log
    
    * Use bring to front to focus test page
    
    * Use separate browser instances for tests
    
    * lint
    
    * Drop waiting
    
    * Add a little bit of waiting
    
    * Typo
    
    * Add a description for the test

--- integration-test/background/storage.js
+++ integration-test/background/storage.js
@@ -1,91 +1,117 @@
 const harness = require('../helpers/harness')
-const wait = require('../helpers/wait')
 
 const testPageDomain = 'privacy-test-pages.glitch.me'
+const thirdPartyDomain = 'good.third-party.site'
+const thirdPartyTracker = 'broken.third-party.site'
 
-describe(`On https://${testPageDomain}/privacy-protections/storage-blocking/`, () => {
-    const thirdPartyDomain = 'good.third-party.site'
-    const thirdPartyTracker = 'broken.third-party.site'
-    let cookies = []
+async function setup () {
+    const { browser, bgPage } = await harness.setup()
+    const page = await browser.newPage()
 
-    beforeAll(async () => {
-        const { browser, bgPage } = await harness.setup()
-        const page = await browser.newPage()
+    await bgPage.waitForFunction(
+        () => {
+            console.log('waiting for tds...')
+            return window.dbg && window.dbg.tds && window.dbg.tds.tds
+        },
+        { polling: 100, timeout: 10000 }
+    )
+    return { browser, page }
+}
 
-        await bgPage.waitForFunction(
-            () => {
-                console.log('waiting for tds...')
-                return window.dbg && window.dbg.tds && window.dbg.tds.tds
-            },
-            { polling: 100, timeout: 10000 }
-        )
+async function waitForAllResults (page) {
+    while ((await page.$$('#tests-details > li > span > ul')).length < 2) {
+        await new Promise(resolve => setTimeout(resolve, 100))
+    }
+}
 
-        let iframeFullyLoaded = false
-        try {
-            page.on('requestfinished', (req) => {
-                // once we see this url, we can consider the test completed
-                if (req.url().startsWith(`https://${thirdPartyTracker}/set-cookie`)) {
-                    iframeFullyLoaded = true
-                }
-            })
-            // Load the test pages home first to give some time for the extension background to start
-            // and register the content-script-message handler
-            await page.goto(`https://${testPageDomain}/`, { waitUntil: 'networkidle0' })
-            await page.goto(`https://${testPageDomain}/privacy-protections/storage-blocking/?store`, { waitUntil: 'networkidle0' })
-            // eslint-disable-next-line no-unmodified-loop-condition
-            while (!iframeFullyLoaded) {
-                await wait.ms(100)
-            }
-            // collect all browser cookies
-            do {
-                await wait.ms(1000) // allow cookies to be set
+describe('Storage blocking Tests', () => {
+    describe(`On https://${testPageDomain}/privacy-protections/storage-blocking/`, () => {
+        let cookies = []
+
+        beforeAll(async () => {
+            const { browser, page } = await setup()
+            try {
+                // Load the test pages home first to give some time for the extension background to start
+                // and register the content-script-message handler
+                await page.goto(`https://${testPageDomain}/`, { waitUntil: 'networkidle0' })
+                await page.bringToFront()
+                await page.goto(`https://${testPageDomain}/privacy-protections/storage-blocking/?store`, { waitUntil: 'networkidle2' })
+                await waitForAllResults(page)
+                // collect all browser cookies
                 cookies = (await page._client.send('Network.getAllCookies')).cookies
-            } while (cookies.length === 0)
-        } finally {
-            await page.close()
-        }
-        await harness.teardown(browser)
+            } finally {
+                await page.close()
+                await harness.teardown(browser)
+            }
+        })
+
+        it('does not block 1st party HTTP cookies', () => {
+            const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === testPageDomain)
+            expect(headerCookie).toBeTruthy()
+            expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
+        })
+
+        it('does not block 3rd party HTTP cookies not on block list', () => {
+            const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === thirdPartyDomain)
+            expect(headerCookie).toBeTruthy()
+            expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
+        })
+
+        it('blocks 3rd party HTTP cookies for trackers', () => {
+            const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === thirdPartyTracker)
+            expect(headerCookie).toBeUndefined()
+        })
+
+        it('does not block 1st party JS cookies', () => {
+            const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === testPageDomain)
+            expect(jsCookie).toBeTruthy()
+            expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
+        })
+
+        it('does not block 3rd party JS cookies not on block list', () => {
+            const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === thirdPartyDomain)
+            expect(jsCookie).toBeTruthy()
+            expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
+        })
+
+        it('blocks 3rd party JS cookies from trackers', () => {
+            const headerCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === thirdPartyTracker)
+            expect(headerCookie).toBeUndefined()
+        })
+
+        it('does not block 1st party JS cookies set by non-trackers', () => {
+            const jsCookie = cookies.find(({ name, domain }) => name === 'tpsdata' && domain === testPageDomain)
+            expect(jsCookie).toBeTruthy()
+            expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000 + 950400) // 11 days in the future
+        })
+
+        it('reduces the expiry of 1st party JS cookies set by trackers to 8 days', () => {
+            const jsCookie = cookies.find(({ name, domain }) => name === 'tptdata' && domain === testPageDomain)
+            expect(jsCookie).toBeTruthy()
+            expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
+            expect(jsCookie.expires).toBeLessThan(Date.now() / 1000 + 864000) // 10 days in the future
+        })
     })
 
-    it('does not block 1st party HTTP cookies', () => {
-        const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === testPageDomain)
-        expect(headerCookie).toBeTruthy()
-        expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
-    })
-
-    it('does not block 3rd party HTTP cookies not on block list', async () => {
-        const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === thirdPartyDomain)
-        expect(headerCookie).toBeTruthy()
-        expect(headerCookie.expires).toBeGreaterThan(Date.now() / 1000)
-    })
-
-    it('blocks 3rd party HTTP cookies for trackers', async () => {
-        const headerCookie = cookies.find(({ name, domain }) => name === 'headerdata' && domain === thirdPartyTracker)
-        expect(headerCookie).toBeUndefined()
-    })
-
-    it('does not block 1st party JS cookies', () => {
-        const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === testPageDomain)
-        expect(jsCookie).toBeTruthy()
-        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
-    })
-
-    it('does not block 3rd party JS cookies not on block list', async () => {
-        const jsCookie = cookies.find(({ name, domain }) => name === 'jsdata' && domain === thirdPartyDomain)
-        expect(jsCookie).toBeTruthy()
-        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
-    })
-
-    it('does not block 1st party JS cookies set by non-trackers', () => {
-        const jsCookie = cookies.find(({ name, domain }) => name === 'tpsdata' && domain === testPageDomain)
-        expect(jsCookie).toBeTruthy()
-        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000 + 950400) // 11 days in the future
-    })
-
-    it('reduces the expiry of 1st party JS cookies set by trackers to 8 days', () => {
-        const jsCookie = cookies.find(({ name, domain }) => name === 'tptdata' && domain === testPageDomain)
-        expect(jsCookie).toBeTruthy()
-        expect(jsCookie.expires).toBeGreaterThan(Date.now() / 1000)
-        expect(jsCookie.expires).toBeLessThan(Date.now() / 1000 + 864000) // 10 days in the future
+    describe(`On https://${thirdPartyTracker}/privacy-protections/storage-blocking/`, () => {
+        /*
+         * Loads the storage test page on the tracker origin (broken.third-party.site) to test if the same entity
+         * rule is observed for frames on that page.
+         */
+        it('does not block iFrame tracker cookies from same entity', async () => {
+            const { browser, page } = await setup()
+            await page.goto(`https://${thirdPartyTracker}/privacy-protections/storage-blocking/?store`, { waitUntil: 'networkidle0' })
+            await page.bringToFront()
+            await waitForAllResults(page)
+            await page.click('#retrive')
+            await waitForAllResults(page)
+            const results = JSON.parse(await page.evaluate('JSON.stringify(results);'))
+            const savedResult = results.results.find(({ id }) => id === 'memory').value
+            const sameEntityiFrameResult = results.results.find(({ id }) => id === 'tracking third party iframe')?.value.find(({ test }) => test === 'JS cookie')?.result
+            expect(sameEntityiFrameResult).toBeTruthy()
+            expect(sameEntityiFrameResult).toEqual(savedResult)
+            page.close()
+            await harness.teardown(browser)
+        })
     })
 })
