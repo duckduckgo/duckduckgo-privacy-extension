d62fb510d207f980c1c010bc2e3e52dfad58a314
Sam Macbeth <sammacbeth@users.noreply.github.com>
2021-09-01 18:10:39 +0200

    Release automation (#807)
    * CI: Create version bump PR and build releases
    
    * Drop args which are not needed when the workflow is triggered on release.
    
    * Trigger publish on tag push
    
    * Remove release-update script
    
    * Revert accidental regression
    
    * Un-revert removal of obsolete script
    
    Co-authored-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- scripts/bumpVersion.js
+++ scripts/bumpVersion.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
-const time = new Date();
 
-const seconds = (time.getUTCHours() * 60 * 60) + (time.getUTCMinutes() * 60) + time.getUTCSeconds();
-const newVersion = `${time.getUTCFullYear()}.${time.getUTCMonth()+1}.${time.getUTCDate()}.${seconds}`
+const newVersion = process.argv[2]
 
-for (let browser of ['chrome', 'firefox']) {
+for (const browser of ['chrome', 'firefox']) {
     const manifest = fs.readFileSync(`browsers/${browser}/manifest.json`, 'utf8')
-    const manifestData = JSON.parse(manifest);
-    manifestData.version = newVersion;
+    const manifestData = JSON.parse(manifest)
+    manifestData.version = newVersion
     fs.writeFileSync(
         `browsers/${browser}/manifest.json`,
-        JSON.stringify(manifestData, null, 4),
+        JSON.stringify(manifestData, null, 4)
     )
 }
