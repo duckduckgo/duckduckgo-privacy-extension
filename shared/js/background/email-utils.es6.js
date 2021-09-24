219012e2a0114d636e42da45941c216c1fd27e6d
Jonathan Kingston <jkingston@duckduckgo.com>
2021-09-24 08:56:08 +0100

    Move to contextMenu.onClicked as onclick isn't accessible in an event page

--- shared/js/background/email-utils.es6.js
+++ shared/js/background/email-utils.es6.js
@@ -50,15 +50,15 @@ const createAutofillContextMenuItem = () => {
         id: MENU_ITEM_ID,
         title: 'Use Duck Address',
         contexts: ['editable'],
-        visible: false,
-        onclick: (info, tab) => {
-            const userData = getSetting('userData')
-            if (userData.nextAlias) {
-                browser.tabs.sendMessage(tab.id, {
-                    type: 'contextualAutofill',
-                    alias: userData.nextAlias
-                })
-            }
+        visible: false
+    })
+    browser.contextMenus.onClicked.addListener((info, tab) => {
+        const userData = getSetting('userData')
+        if (userData.nextAlias) {
+            browser.tabs.sendMessage(tab.id, {
+                type: 'contextualAutofill',
+                alias: userData.nextAlias
+            })
         }
     })
 }
