f42784ab97cb08527c6da2004d044052c38fb787
Konrad Dzwinel <kdzwinel@gmail.com>
2021-11-15 23:58:36 +0100

    Fix allowlist tests

--- unit-test/helpers/tds.es6.js
+++ unit-test/helpers/tds.es6.js
@@ -3,25 +3,20 @@
  * stub out the get and fallback functions
  */
 const tdsStorage = require('../../shared/js/background/storage/tds.es6')
-const tdsData = {
-    tds: require('./../data/tds.json'),
-    surrogates: require('./../data/surrogates.js').surrogates,
-    ClickToLoadConfig: require('./../data/clickToLoadConfig.json'),
-    config: require('./../data/extension-config.json')
-}
-
-tdsData.config.features.trackerAllowlist = {
-    state: 'enabled',
-    settings: {
-        allowlistedTrackers: require('./../background/reference-tests/tracker-radar-tests/TR-domain-matching/tracker_allowlist_reference.json')
-    }
-}
-
-tdsStorage.config = tdsData.config
 
 const stub = (arg) => {
+    const tdsData = {
+        tds: require('./../data/tds.json'),
+        surrogates: require('./../data/surrogates.js').surrogates,
+        ClickToLoadConfig: require('./../data/clickToLoadConfig.json'),
+        config: require('./../data/extension-config.json')
+    }
+
     if (arg && arg.config) {
         tdsData.config = arg.config
+        console.log('STUB custom config - ' + arg.config.readme)
+    } else {
+        console.log('STUB default config - ' + tdsData.config.readme)
     }
 
     spyOn(tdsStorage, 'getVersionParam').and.returnValue('')
@@ -33,7 +28,9 @@ const stub = (arg) => {
         .and.callFake(key => Promise.resolve(tdsData[key]))
 
     spyOn(tdsStorage, 'getDataXHR')
-        .and.callFake((list, etag, source) => Promise.resolve({ response: 200, data: tdsData[list] }))
+        .and.callFake((list, etag, source) => {
+            return Promise.resolve({ response: 200, data: tdsData[list.name] })
+        })
 }
 module.exports = {
     stub
