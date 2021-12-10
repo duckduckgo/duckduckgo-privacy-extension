416f34540e142f625235544c2baa87d58ed40d54
Konrad Dzwinel <konrad@duckduckgo.com>
2021-12-10 22:43:35 +0100

    Referrer trimming reference tests (#951)
    * First set tests
    
    * Update reference-tests submodule
    
    * Revert weird order flip in .gitmodules
    
    * extract referrer trimming callback to a separate file
    
    * use referrer trimming callback for tests instad of the helper function
    
    * take CNAME cloaking into account when trimming referrers
    
    * referrer trimming JS tests integration
    
    * Adjust referrer tests to new paths
    
    * Bring back /reference-tests folder, move referrer trimming there
    
    * Clean up .gitmodules - unnecesary EOLs
    
    * Update reference-tests to the latest

---
+++ shared/js/background/events/referrer-trimming.js
@@ -0,0 +1,57 @@
+const tabManager = require('../tab-manager.es6')
+const trackerutils = require('../tracker-utils')
+const utils = require('../utils.es6')
+const browserName = utils.getBrowserName()
+
+/**
+ * @param {{tabId: number, url: string, requestHeaders: Array<{name: string, value:string}>}} e
+ *
+ * @returns {requestHeaders: Array<{name: string, value:string}>?}
+ */
+module.exports = function limitReferrerData (e) {
+    let referrer = e.requestHeaders.find(header => header.name.toLowerCase() === 'referer')
+    if (referrer) {
+        referrer = referrer.value
+    } else {
+        return
+    }
+
+    const tab = tabManager.get({ tabId: e.tabId })
+
+    // Firefox only - Check if this tab had a surrogate redirect request and if it will
+    // likely be blocked by CORS (Origin header). Chrome surrogate redirects happen in onBeforeRequest.
+    if (browserName === 'moz' && tab && tab.surrogates && tab.surrogates[e.url]) {
+        const hasOrigin = e.requestHeaders.filter(h => h.name.match(/^origin$/i))
+        if (!hasOrigin.length) {
+            const redirectUrl = tab.surrogates[e.url]
+            // remove redirect entry for the tab
+            delete tab.surrogates[e.url]
+
+            return { redirectUrl }
+        }
+    }
+
+    if (!tab || !tab.site.isFeatureEnabled('referrer')) {
+        return
+    }
+
+    // Additional safe list and broken site list checks are included in the referrer evaluation
+    const modifiedReferrer = trackerutils.truncateReferrer(referrer, e.url)
+    if (!modifiedReferrer) {
+        return
+    }
+
+    const requestHeaders = e.requestHeaders.filter(header => header.name.toLowerCase() !== 'referer')
+    if (!!tab && (!tab.referrer || tab.referrer.site !== tab.site.url)) {
+        tab.referrer = {
+            site: tab.site.url,
+            referrerHost: new URL(referrer).hostname,
+            referrer: modifiedReferrer
+        }
+    }
+    requestHeaders.push({
+        name: 'referer',
+        value: modifiedReferrer
+    })
+    return { requestHeaders: requestHeaders }
+}
---
+++ unit-test/background/reference-tests/referrer-trimming-tests.js
@@ -0,0 +1,115 @@
+require('../../helpers/mock-browser-api')
+
+const tds = require('../../../shared/js/background/trackers.es6')
+const tdsStorageStub = require('../../helpers/tds.es6')
+const tdsStorage = require('../../../shared/js/background/storage/tds.es6')
+
+const tabManager = require('../../../shared/js/background/tab-manager.es6')
+const browserWrapper = require('../../../shared/js/background/wrapper.es6')
+const jsReferrerProtection = require('../../../shared/js/content-scope/referrer-protection')
+
+const limitReferrerData = require('../../../shared/js/background/events/referrer-trimming')
+
+const configReference = require('../../data/reference-tests/referrer-trimming/config_reference.json')
+const blocklistReference = require('../../data/reference-tests/referrer-trimming/tracker_radar_reference.json')
+const testSets = require('../../data/reference-tests/referrer-trimming/tests.json')
+
+const EXT_ID = 'ogigmfedpbpnnbcpgjloacccaibkaoip'
+
+for (const setName of Object.keys(testSets)) {
+    const testSet = testSets[setName]
+
+    describe(`Referrer Trimming tests / ${testSet.name} /`, () => {
+        beforeAll(() => {
+            spyOn(browserWrapper, 'getExtensionId').and.returnValue(EXT_ID)
+            tdsStorageStub.stub({ config: configReference, tds: blocklistReference })
+
+            return tdsStorage.getLists().then(lists => tds.setLists(lists))
+        })
+
+        testSet.tests.forEach(test => {
+            if (test.exceptPlatforms && test.exceptPlatforms.includes('web-extension')) {
+                return
+            }
+
+            if ('expectReferrerHeaderValue' in test) {
+                it(`${test.name}`, () => {
+                    tabManager.delete(1)
+                    tabManager.create({
+                        tabId: 1,
+                        url: test.navigatingFromUrl || test.siteURL
+                    })
+
+                    const requestChanges = limitReferrerData({
+                        tabId: 1,
+                        url: test.navigatingToURL || test.requestURL,
+                        requestHeaders: [
+                            { name: 'something', value: 'else' },
+                            { name: 'referer', value: test.referrerValue }
+                        ],
+                        type: test.requestType || 'main_frame'
+                    })
+
+                    let refererHeaderValueAfter = test.referrerValue
+
+                    if (requestChanges && requestChanges.requestHeaders) {
+                        refererHeaderValueAfter = requestChanges.requestHeaders.find(h => h.name === 'referer')?.value
+                    }
+
+                    expect(refererHeaderValueAfter).toEqual(test.expectReferrerHeaderValue)
+                })
+            } else if ('expectReferrerAPIValue' in test) {
+                it(`${test.name}`, () => {
+                    tabManager.delete(1)
+                    tabManager.create({
+                        tabId: 1,
+                        url: test.siteURL
+                    })
+
+                    limitReferrerData({
+                        tabId: 1,
+                        url: test.siteURL,
+                        requestHeaders: [
+                            { name: 'something', value: 'else' },
+                            { name: 'referer', value: test.referrerValue }
+                        ],
+                        type: 'main_frame'
+                    })
+
+                    if ('frameURL' in test) {
+                        limitReferrerData({
+                            tabId: 1,
+                            url: test.frameURL,
+                            requestHeaders: [
+                                { name: 'referer', value: test.referrerValue }
+                            ],
+                            type: 'sub_frame'
+                        })
+                    }
+
+                    const tab = tabManager.get({ tabId: 1 })
+
+                    const FakeDocument = function () {}
+                    FakeDocument.prototype.referrer = test.referrerValue
+                    FakeDocument.prototype.location = {
+                        url: test.frameURL || test.siteURL
+                    }
+                    const orgDocument = globalThis.Document
+
+                    // replacing real document and Document with fake ones
+                    globalThis.Document = FakeDocument
+                    spyOnProperty(document, 'referrer', 'get').and.returnValue(test.referrerValue)
+
+                    jsReferrerProtection.init({ referrer: tab.referrer })
+
+                    // clean up
+                    globalThis.Document = orgDocument
+
+                    expect(FakeDocument.prototype.referrer).toEqual(test.expectReferrerAPIValue)
+                })
+            } else {
+                throw new Error(`Unknown type of test - ${test.name}`)
+            }
+        })
+    })
+}
