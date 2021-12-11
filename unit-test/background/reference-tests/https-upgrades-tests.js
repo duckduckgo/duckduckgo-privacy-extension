3522c6cf1b84e7d0f54703ded454801ba8d64df0
Konrad Dzwinel <kdzwinel@gmail.com>
2021-12-11 10:59:25 +0100

    https upgrades tests v1

---
+++ unit-test/background/reference-tests/https-upgrades-tests.js
@@ -0,0 +1,71 @@
+require('../../helpers/mock-browser-api')
+
+const tdsStorageStub = require('../../helpers/tds.es6')
+const tdsStorage = require('../../../shared/js/background/storage/tds.es6')
+
+const browserWrapper = require('../../../shared/js/background/wrapper.es6')
+
+const testSets = require('../../data/reference-tests/https-upgrades/tests.json')
+const config = require('../../data/reference-tests/https-upgrades/config_reference.json')
+
+const bloomFilter = require('../../data/reference-tests/https-upgrades/https_bloomfilter_reference.json')
+const bloomFilterAllowlist = require('../../data/reference-tests/https-upgrades/https_allowlist_reference.json')
+const negativeBloomFilter = require('../../data/reference-tests/https-upgrades/https_negative_bloomfilter_reference.json')
+const negativeBloomFilterAllowlist = require('../../data/reference-tests/https-upgrades/https_negative_allowlist_reference.json')
+
+const https = require('../../../shared/js/background/https.es6')
+const httpsStorage = require('../../../shared/js/background/storage/https.es6')
+const load = require('./../../helpers/https.es6')
+const Tab = require('../../../shared/js/background/classes/tab.es6')
+const httpsService = require('../../../shared/js/background/https-service.es6')
+
+const EXT_ID = 'ogigmfedpbpnnbcpgjloacccaibkaoip'
+
+for (const setName of Object.keys(testSets)) {
+    const testSet = testSets[setName]
+
+    describe(`HTTPS upgrades tests / ${testSet.name} /`, () => {
+        beforeAll(() => {
+            spyOn(browserWrapper, 'getExtensionId').and.returnValue(EXT_ID)
+
+            // disable https upgrade service
+            spyOn(httpsService, 'checkInCache').and.returnValue(false)
+
+            // load mock config
+            tdsStorageStub.stub({ config })
+
+            // load mock https upgrade data
+            load.loadStub({
+                httpsBloom: bloomFilter,
+                httpsAllowlist: bloomFilterAllowlist,
+                httpsNegativeBloom: negativeBloomFilter,
+                httpsNegativeAllowlist: negativeBloomFilterAllowlist
+            })
+
+            // initialize https upgreade data and config
+            return Promise.all([
+                httpsStorage.getLists().then(lists => https.setLists(lists)),
+                tdsStorage.getLists()
+            ])
+        })
+
+        testSet.tests.forEach(test => {
+            if (test.exceptPlatforms && test.exceptPlatforms.includes('web-extension')) {
+                return
+            }
+
+            it(test.name, () => {
+                // expect(https.canUpgradeUrl(test.requestURL)).toEqual(test.requestURL !== test.expectURL)
+                const url = test.requestURL
+                const tab = new Tab({
+                    id: 1,
+                    url: test.siteURL
+                })
+                const isMainFrame = (test.requestType === 'main_frame')
+                const isPost = false
+
+                expect(https.getUpgradedUrl(url, tab, isMainFrame, isPost)).toEqual(test.expectURL)
+            })
+        })
+    })
+}
