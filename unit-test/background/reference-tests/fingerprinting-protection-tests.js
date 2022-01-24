0c66dbe66e019dc427f560620eee0a311d5e6cc9
Konrad Dzwinel <kdzwinel@gmail.com>
2022-01-24 02:39:02 +0100

    Wrap up integration

--- unit-test/background/reference-tests/fingerprinting-protection-tests.js
+++ unit-test/background/reference-tests/fingerprinting-protection-tests.js
@@ -4,17 +4,18 @@ const tds = require('../../../shared/js/background/trackers.es6')
 const tdsStorageStub = require('../../helpers/tds.es6')
 const tdsStorage = require('../../../shared/js/background/storage/tds.es6')
 
-// const tabManager = require('../../../shared/js/background/tab-manager.es6')
+const tabManager = require('../../../shared/js/background/tab-manager.es6')
 const browserWrapper = require('../../../shared/js/background/wrapper.es6')
-// const getArgumentsObject = require('../../../shared/js/background/helpers/arguments-object')
+const getArgumentsObject = require('../../../shared/js/background/helpers/arguments-object')
 
 const batteryProtection = require('../../../shared/content-scope-scripts/src/features/fingerprinting-battery')
 const hardwareProtection = require('../../../shared/content-scope-scripts/src/features/fingerprinting-hardware')
 const screenProtection = require('../../../shared/content-scope-scripts/src/features/fingerprinting-screen-size')
-const contentScopeFeatures = require('../../../shared/content-scope-scripts/src/content-scope-features')
+const { isFeatureBroken } = require('../../../shared/content-scope-scripts/src/utils')
 
 const configReference = require('../../data/reference-tests/fingerprinting-protections/config_reference.json')
 const testSets = require('../../data/reference-tests/fingerprinting-protections/tests.json')
+const apiMocksInit = require('../../data/reference-tests/fingerprinting-protections/init')
 
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
@@ -38,27 +39,31 @@ for (const setName of Object.keys(testSets)) {
             }
 
             it(`${test.name}`, (done) => {
+                tabManager.delete(1)
+                tabManager.create({
+                    tabId: 1,
+                    url: test.siteURL
+                })
+
+                const args = getArgumentsObject(1, { url: test.siteURL, frameId: 0 }, test.siteURL, 'abc123')
                 const dom = new JSDOM('', {
                     url: 'https://example.com/',
                     runScripts: 'outside-only'
                 })
 
-                contentScopeFeatures.
-
                 // mock non-standard APIs not implemented by jsdom
-                const BatteryManager = function BatteryManager () {}
-
-                dom.window.BatteryManager = BatteryManager
-                dom.window.navigator.getBattery = () => Promise.resolve(new BatteryManager())
-
-                Object.defineProperty(dom.window.Navigator.prototype, 'deviceMemory', { get: () => 128, configurable: true })
-                Object.defineProperty(dom.window.Screen.prototype, 'availTop', { get: () => 12345, configurable: true })
-                Object.defineProperty(dom.window.Screen.prototype, 'availLeft', { get: () => 12345, configurable: true })
+                apiMocksInit(dom.window)
 
                 // init protections
-                batteryProtection.init({}, dom.window)
-                hardwareProtection.init({}, dom.window)
-                screenProtection.init({}, dom.window)
+                if (!isFeatureBroken(args, 'fingerprintingBattery')) {
+                    batteryProtection.init({}, dom.window)
+                }
+                if (!isFeatureBroken(args, 'fingerprintingHardware')) {
+                    hardwareProtection.init({}, dom.window)
+                }
+                if (!isFeatureBroken(args, 'fingerprintingScreenSize')) {
+                    screenProtection.init({}, dom.window)
+                }
 
                 // validate result
                 const result = dom.window.eval(test.property)
