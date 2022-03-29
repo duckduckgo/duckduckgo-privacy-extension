9769fa705e5560d4c1226f59821ab0a47d735c3f
Brad Slayter <slayterdevelopment@gmail.com>
2022-03-29 11:17:18 -0500

    Fix waitForConfig

--- integration-test/background/amp-protection.js
+++ integration-test/background/amp-protection.js
@@ -40,7 +40,6 @@ describe('Test AMP URL tracking protection', () => {
         ({ browser, bgPage, teardown } = await harness.setup())
         await backgroundWait.forAllConfiguration(bgPage)
 
-        bgPage.waitForTimeout(10000)
         // Overwrite the parts of the configuration needed for our tests.
         await loadTestConfig(bgPage, 'amp-protections.json')
     })
