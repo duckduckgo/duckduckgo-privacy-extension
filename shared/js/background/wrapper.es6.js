46914dd505eefb279bf674f0225ddcc766be4d23
Dave Vandyke <kzar@kzar.co.uk>
2022-05-20 14:45:54 +0100

    Fix the extension icons for Chrome MV3 (#1189)
    There are some differences with extension icons for Chrome MV3:
     - The API was renamed from browserAction.setIcon to action.setIcon.
     - Relative paths are handled differently.
     - SVG icons are not supported.
    
    Workaround those differences by expanding the setBadgeIcon wrapper,
    using absolute icon paths and using PNG icons.

--- shared/js/background/wrapper.es6.js
+++ shared/js/background/wrapper.es6.js
@@ -9,8 +9,12 @@ export function getExtensionVersion () {
     return manifest.version
 }
 
-export function setBadgeIcon (badgeData) {
-    browser.browserAction.setIcon(badgeData)
+export async function setBadgeIcon (badgeData) {
+    if (typeof browser.action === 'undefined') {
+        return await browser.browserAction.setIcon(badgeData)
+    }
+
+    return await browser.action.setIcon(badgeData)
 }
 
 export function syncToStorage (data) {
