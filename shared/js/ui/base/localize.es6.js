8b5815f04b538744e00ffab4068483e5f646b8d0
ladamski@duckduckgo.com <ladamski@duckduckgo.com>
2022-06-16 17:40:48 -0700

    set locale based upon chrome.18n, fallback to navigator.language

--- shared/js/ui/base/localize.es6.js
+++ shared/js/ui/base/localize.es6.js
@@ -3,7 +3,7 @@ import ICU from 'i18next-icu'
 
 const supportedLocales = require('../../../data/constants.js').supportedLocales
 
-function buildLocalesList (locales) {
+function buildLocalesList () {
     const resources = {}
     // hard coding for now till we figure out dynamic bundling
     resources.en = {
@@ -13,16 +13,24 @@ function buildLocalesList (locales) {
     return resources
 }
 
+function getLocale (supportedLocales) {
+    // truncate locale by dropping country suffix
+    let browserLocale = chrome.i18n ? chrome.i18n.getUILanguage() : navigator.language
+    browserLocale = browserLocale.split("-")[0]
+
+    return supportedLocales.includes[browserLocale] || "en" // probably redundant due to fallbackLng in init() below?
+}
+
 i18next
     .use(ICU)
     .init({
         debug: true,
         initImmediate: false,
         fallbackLng: 'en',
-        lng: 'en',
+        lng: getLocale(supportedLocales),
         ns: ['shared', 'options'],
         defaultNS: 'shared',
-        resources: buildLocalesList(supportedLocales)
+        resources: buildLocalesList()
     })
 
 module.exports = i18next
