231a59f58ad4fcdcddaf31abc19b227dd063ce44
ladamski@duckduckgo.com <ladamski@duckduckgo.com>
2022-06-29 15:19:52 -0700

    add all locales for options
    Also cleanup i18next init to follow dashboard pattern

--- shared/js/ui/base/localize.es6.js
+++ shared/js/ui/base/localize.es6.js
@@ -1,37 +1,30 @@
 import i18next from 'i18next'
 import ICU from 'i18next-icu'
-import config from '../../../data/constants.js'
 
-const supportedLocales = config.supportedLocales
+const localeResources = require('../../../locales/*/*.json', { mode: 'list' })
 
-function buildLocalesList () {
-    const resources = {}
-    // hard coding for now till we figure out dynamic bundling
-    resources.en = {
-        shared: require('../../../locales/en/shared.json'),
-        options: require('../../../locales/en/options.json')
-    }
-    return resources
-}
+const resources = localeResources.reduce((mapping, { name, module }) => {
+    const [locale, namespace] = name.split('/')
+    mapping[locale] = mapping[locale] || {}
+    mapping[locale][namespace] = module
+    return mapping
+}, {})
 
-function getLocale (supportedLocales) {
-    // truncate locale by dropping country suffix
+function getBrowserLocale () {
     let browserLocale = chrome.i18n ? chrome.i18n.getUILanguage() : navigator.language
-    browserLocale = browserLocale.split('-')[0]
-
-    return supportedLocales.includes[browserLocale] || 'en' // probably redundant due to fallbackLng in init() below?
+    return browserLocale.split('-')[0] // drop country suffix
 }
 
 i18next
     .use(ICU)
     .init({
-        // debug: true,
+        debug: true,
         initImmediate: false,
         fallbackLng: 'en',
-        lng: getLocale(supportedLocales),
+        lng: getBrowserLocale(),
         ns: ['shared', 'options'],
         defaultNS: 'shared',
-        resources: buildLocalesList()
+        resources: resources
     })
 
 module.exports = i18next
