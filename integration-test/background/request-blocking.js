9cab5c3d49366dd510972a6ccc93d1de34127e04
Dave Vandyke <kzar@kzar.co.uk>
2022-07-27 14:29:05 +0100

    Add request-blocking integration tests
    Add an integration test runner for the request blocking test page. To
    facilitate that, add support for request filtering and WebSocket
    connections to the logPageRequests helper.
    
    1 - https://privacy-test-pages.glitch.me/privacy-protections/request-blocking/

---
+++ integration-test/background/request-blocking.js
@@ -0,0 +1,77 @@
+const harness = require('../helpers/harness')
+const { logPageRequests } = require('../helpers/requests')
+const backgroundWait = require('../helpers/backgroundWait')
+const pageWait = require('../helpers/pageWait')
+
+const testSite = 'https://privacy-test-pages.glitch.me/privacy-protections/request-blocking/'
+
+let browser
+let bgPage
+let teardown
+
+describe('Test request blocking', () => {
+    beforeAll(async () => {
+        ({ browser, bgPage, teardown } = await harness.setup())
+        await backgroundWait.forAllConfiguration(bgPage)
+    })
+
+    afterAll(async () => {
+        await teardown()
+    })
+
+    it('Should block all the test tracking requests', async () => {
+        // Load the test page.
+        const page = await browser.newPage()
+        await pageWait.forGoto(page, testSite)
+
+        // Start logging network requests.
+        const pageRequests = []
+        logPageRequests(
+            page,
+            pageRequests,
+            ({ url }) => url.hostname === 'bad.third-party.site'
+        )
+
+        // Initiate the test requests and wait until they have all completed.
+        // Note:
+        //  - Waiting for network idle here does not work for some reason.
+        //  - ServiceWorker + WebWorker initiated requests are not yet logged by
+        //    logPageRequests.
+        await page.click('#start')
+        const testCount = await page.evaluate(
+            // eslint-disable-next-line no-undef
+            () => tests.filter(({ id }) => !id.includes('worker')).length
+        )
+        while (pageRequests.length < testCount) {
+            await backgroundWait.forTimeout(bgPage, 100)
+        }
+
+        // Verify that no logged requests were allowed.
+        for (const { url, method, type, status } of pageRequests) {
+            const description = `URL: ${url}, Method: ${method}, Type: ${type}`
+            expect(status).withContext(description).toEqual('blocked')
+        }
+
+        // Wait an additional second to ensure that the WebWorker +
+        // ServiceWorker initiated requests have finished. This can be removed
+        // once logPageRequests can intercept those requests.
+        await backgroundWait.forTimeout(bgPage, 1000)
+
+        // Also check that the test page itself agrees that no requests were
+        // allowed.
+        const pageResults = await page.evaluate(
+            () => results.results // eslint-disable-line no-undef
+        )
+        for (const { id, category, status } of pageResults) {
+            // ServiceWorker initiated request blocking is not yet supported.
+            if (id === 'serviceworker-fetch') {
+                continue
+            }
+
+            const description = `ID: ${id}, Category: ${category}`
+            expect(status).withContext(description).not.toEqual('loaded')
+        }
+
+        await page.close()
+    })
+})
