4f5ce862ab20de91ac919f8b44040de8cf76e0bd
Jonathan Kingston <jkingston@duckduckgo.com>
2022-08-25 01:12:32 +0100

    Enable reporting of MV2 to MV3 extension.
    Reuse onBeforeRequest reporting flow for MV3, this means we need to retain feature parity.
    
    It was deemed that the reporting APIs provided by MV3 were not suitable for our needs due to the cadence they come in.

--- unit-test/background/reference-tests/expire-1p-tracking-cookies.js
+++ unit-test/background/reference-tests/expire-1p-tracking-cookies.js
@@ -4,7 +4,7 @@ const tds = require('../../../shared/js/background/trackers.es6')
 const tdsStorageStub = require('../../helpers/tds.es6')
 const tdsStorage = require('../../../shared/js/background/storage/tds.es6')
 
-const { handleRequest } = require('../../../shared/js/background/redirect.es6')
+const { handleRequest } = require('../../../shared/js/background/before-request.es6')
 const tabManager = require('../../../shared/js/background/tab-manager.es6')
 const browserWrapper = require('../../../shared/js/background/wrapper.es6')
 const getArgumentsObject = require('../../../shared/js/background/helpers/arguments-object')
