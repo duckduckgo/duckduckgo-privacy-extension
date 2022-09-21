58b2519162037e215381e8c38c7383ad9dfce3a4
Jonathan Kingston <jkingston@duckduckgo.com>
2022-09-21 10:14:59 +0100

    Fix reddit grading test (#1415)

--- integration-test/background/grades.js
+++ integration-test/background/grades.js
@@ -7,7 +7,7 @@ const tests = [
     // Fixme flaking
     // { url: 'www.independent.co.uk', siteGrade: ['D', 'D-'], enhancedGrade: 'B' },
     { url: 'google.com', siteGrade: 'D', enhancedGrade: 'D' },
-    { url: 'reddit.com', siteGrade: ['D', 'D-', 'C'], enhancedGrade: 'B' },
+    { url: 'reddit.com', siteGrade: ['D', 'D-', 'C', 'B'], enhancedGrade: 'B' },
     { url: 'facebook.com', siteGrade: ['D', 'C+'], enhancedGrade: 'C+' },
     // FIXME - This case is flaking.
     //         Enhanced grade is something B and sometimes C.
