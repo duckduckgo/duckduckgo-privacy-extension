7243fb14baf32eb8302019f9c661a80469002680
Jonathan Kingston <jkingston@duckduckgo.com>
2022-11-02 03:49:43 +0000

    Switch integration over for content-scope-scripts

--- shared/js/background/events.es6.js
+++ shared/js/background/events.es6.js
@@ -491,18 +491,6 @@ browser.webNavigation.onCommitted.addListener(details => {
     if (tab.site.isBroken) {
         console.log('temporarily skip embedded object replacements for site: ' + details.url +
           'more info: https://github.com/duckduckgo/privacy-configuration')
-        return
-    }
-
-    if (utils.getClickToPlaySupport(tab)) {
-        browserWrapper.executeScript({
-            target: {
-                tabId: details.tabId,
-                frameIds: [details.frameId]
-            },
-            files: ['public/js/content-scripts/click-to-load.js'],
-            injectImmediately: true
-        })
     }
 })
 
