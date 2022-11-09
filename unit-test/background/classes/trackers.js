d90e1f87add9ec7be2fd925ecc6ad501f3c83f8d
Jonathan Kingston <jkingston@duckduckgo.com>
2022-11-09 13:05:30 +0000

    Move over unit test

---
+++ unit-test/background/classes/trackers.js
@@ -0,0 +1,267 @@
+const Trackers = require('../../../shared/js/background/classes/trackers')
+
+const trackers = new Trackers()
+trackers.setLists([
+    {
+        name: 'tds',
+        data: {
+            entities: {
+                Oath: {
+                    domains: [
+                        'yahoo.com',
+                        'aol.com',
+                        'advertising.com'
+                    ]
+                }
+            },
+            trackers: {
+                'yahoo.com': {
+                    domain: 'yahoo.com',
+                    default: 'block',
+                    owner: {
+                        name: 'Yahoo',
+                        url: 'yahoo.com'
+                    },
+                    rules: [
+                        {
+                            rule: 'geo\\.yahoo\\.com',
+                            options: {
+                                domains: ['example.com', 'aol.com']
+                            }
+                        },
+                        {
+                            rule: 'a\\.yahoo\\.com($|[?/])',
+                            surrogate: 'a.js',
+                            exceptions: {
+                                domains: ['example2.com'],
+                                types: ['image']
+                            }
+                        },
+                        {
+                            rule: 'b\\.yahoo\\.com\\/.*\\?ad=asdf',
+                            action: 'ignore'
+                        }
+                    ]
+                },
+                'example.com': {
+                    domain: 'example.com',
+                    default: 'ignore',
+                    owner: {
+                        name: 'Example',
+                        url: 'example.com'
+                    }
+                }
+            }
+        }
+    },
+    {
+        name: 'surrogates',
+        data: 'yahoo.com/a.js application/javascript\n(function() {})'
+    }
+])
+
+describe('getTrackerData', () => {
+    describe('find tracker data', () => {
+        const trackerTests = [
+            // matched rule with no exceptions or first party => block
+            {
+                action: 'block',
+                urlToCheck: 'https://geo.yahoo.com/asdf',
+                siteUrl: 'https://example.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'matched rule - block',
+                sameEntity: false,
+                expectedRule: 'geo\\.yahoo\\.com',
+                redirectUrl: false,
+                matchedRuleException: false
+            },
+            // request from a site owned by the same company => ignore
+            {
+                action: 'ignore',
+                urlToCheck: 'https://geo.yahoo.com/asdf',
+                siteUrl: 'https://news.aol.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'first party',
+                sameEntity: true,
+                expectedRule: 'geo\\.yahoo\\.com',
+                redirectUrl: false,
+                matchedRuleException: false
+            },
+            // matched a rule with a surrogate => rediect
+            {
+                action: 'redirect',
+                urlToCheck: 'https://ads.a.yahoo.com/?b=asdf',
+                siteUrl: 'https://example.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'matched rule - surrogate',
+                sameEntity: false,
+                expectedRule: 'a\\.yahoo\\.com($|[?/])',
+                redirectUrl: 'data:application/javascript;base64,KGZ1bmN0aW9uKCkge30p',
+                matchedRuleException: false
+            },
+            // request matches a rule exception => ignore
+            {
+                action: 'ignore',
+                urlToCheck: 'https://ads.a.yahoo.com/?b=asdf',
+                siteUrl: 'https://a.example2.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'matched rule - exception',
+                sameEntity: false,
+                expectedRule: 'a\\.yahoo\\.com($|[?/])',
+                redirectUrl: 'data:application/javascript;base64,KGZ1bmN0aW9uKCkge30p',
+                matchedRuleException: true
+            },
+            // request from the same domain => ignore
+            {
+                action: 'ignore',
+                urlToCheck: 'https://ads.a.yahoo.com/?b=asdf',
+                siteUrl: 'https://news.yahoo.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'first party',
+                sameEntity: true,
+                expectedRule: 'a\\.yahoo\\.com($|[?/])',
+                redirectUrl: 'data:application/javascript;base64,KGZ1bmN0aW9uKCkge30p',
+                matchedRuleException: false
+            },
+            // rule with a action 'ignore' => ignore
+            {
+                action: 'ignore',
+                urlToCheck: 'https://b.yahoo.com/ads?ad=asdf',
+                siteUrl: 'https://example.com',
+                requestType: 'image',
+                expectedOwner: 'Oath',
+                expectedReason: 'matched rule - ignore',
+                sameEntity: false,
+                expectedRule: 'b\\.yahoo\\.com\\/.*\\?ad=asdf',
+                redirectUrl: false,
+                matchedRuleException: false
+            },
+            // request without a rule, tracker definition default action is block => block
+            {
+                action: 'block',
+                urlToCheck: 'https://asdf.yahoo.com/tracker1/asdf',
+                siteUrl: 'https://example.com',
+                requestType: 'script',
+                expectedOwner: 'Oath',
+                expectedReason: 'default block',
+                sameEntity: false,
+                expectedRule: null,
+                redirectUrl: false,
+                matchedRuleException: false
+            },
+            // tracker with default ignore => ignore
+            {
+                action: 'ignore',
+                urlToCheck: 'https://example.com/tracker1/asdf',
+                siteUrl: 'https://aol.com',
+                requestType: 'script',
+                expectedOwner: 'Example',
+                expectedReason: 'default ignore',
+                sameEntity: false,
+                expectedRule: null,
+                redirectUrl: false,
+                matchedRuleException: false
+            }
+        ]
+
+        trackerTests.forEach((test) => {
+            it(`should block ${test.urlToCheck}`, () => {
+                const tracker = trackers.getTrackerData(
+                    test.urlToCheck,
+                    test.siteUrl,
+                    { url: test.urlToCheck, type: test.requestType })
+
+                expect(tracker.action).toEqual(test.action)
+                expect(tracker.reason).toEqual(test.expectedReason)
+
+                if (test.expectedRule) {
+                    expect(tracker.matchedRule.rule.toString()).toEqual(new RegExp(test.expectedRule, 'gi').toString())
+                }
+
+                expect(tracker.sameEntity).toEqual(test.sameEntity)
+                expect(tracker.redirectUrl).toEqual(test.redirectUrl)
+                expect(tracker.matchedRuleException).toEqual(test.matchedRuleException)
+            })
+        })
+
+        const malformedTests = [
+            // Malformed urls
+            {
+                urlToCheck: 'http://%20%20s.src%20%3D/',
+                siteUrl: 'https://example.com',
+                requestType: 'image'
+            },
+            // Special url schemes
+            {
+                urlToCheck: 'about:blank',
+                siteUrl: 'https://example.com',
+                requestType: 'image'
+            },
+            {
+                urlToCheck: 'chrome-extension://test-me',
+                siteUrl: 'https://example.com',
+                requestType: 'image'
+            },
+            {
+                urlToCheck: 'https://example.com',
+                siteUrl: 'chrome-extension://test-me',
+                requestType: 'image'
+            },
+            {
+                urlToCheck: 'https://example.com',
+                siteUrl: 'chrome-extension://test-me/test.html',
+                requestType: 'image'
+            },
+            {
+                urlToCheck: 'https://example.com',
+                siteUrl: 'about://test-me/test.html',
+                requestType: 'image'
+            },
+            {
+                urlToCheck: 'https://example.com',
+                siteUrl: 'ftp://blah.com/test.html',
+                requestType: 'image'
+            }
+        ]
+
+        malformedTests.forEach((test) => {
+            it(`should not block ${test.urlToCheck}`, () => {
+                const tracker = trackers.getTrackerData(
+                    test.urlToCheck,
+                    test.siteUrl,
+                    { url: test.urlToCheck, type: test.requestType })
+
+                expect(tracker).toEqual(null)
+            })
+        })
+
+        const nonTrackerTests = [
+            // Non tracker requests
+            {
+                urlToCheck: 'http://somecdn.com/jquery',
+                siteUrl: 'https://example.com',
+                requestType: 'script'
+            },
+            {
+                urlToCheck: 'https://somecdn.com/jquery',
+                siteUrl: 'https://example.com',
+                requestType: 'script'
+            }
+        ]
+        nonTrackerTests.forEach((test) => {
+            it(`should not block ${test.urlToCheck}`, () => {
+                const tracker = trackers.getTrackerData(
+                    test.urlToCheck,
+                    test.siteUrl,
+                    { url: test.urlToCheck, type: test.requestType })
+
+                expect(tracker.action).toEqual('none')
+            })
+        })
+    })
+})
