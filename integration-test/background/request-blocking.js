6e58beee793ef00f443c749286a4568eabcd0b67
Sam Macbeth <smacbeth@duckduckgo.com>
2023-02-07 08:27:44 +0100

    Extract common test parts

--- integration-test/background/request-blocking.js
+++ integration-test/background/request-blocking.js
@@ -11,6 +11,39 @@ let browser
 let bgPage
 let teardown
 
+async function runRequestBlockingTest (page) {
+    await pageWait.forGoto(page, testSite)
+
+    // Start logging network requests.
+    const pageRequests = []
+    await logPageRequests(
+        page,
+        pageRequests,
+        ({ url }) => url.hostname === 'bad.third-party.site'
+    )
+
+    // Initiate the test requests and wait until they have all completed.
+    // Note:
+    //  - Waiting for network idle here does not work for some reason.
+    //  - ServiceWorker + WebWorker initiated requests are not yet logged by
+    //    logPageRequests.
+    await page.click('#start')
+    const testCount = await page.evaluate(
+        // eslint-disable-next-line no-undef
+        () => tests.filter(({ id }) => !id.includes('worker')).length
+    )
+    while (pageRequests.length < testCount) {
+        await backgroundWait.forTimeout(bgPage, 100)
+    }
+
+    // Wait an additional second to ensure that the WebWorker +
+    // ServiceWorker initiated requests have finished. This can be removed
+    // once logPageRequests can intercept those requests.
+    await backgroundWait.forTimeout(bgPage, 1000)
+
+    return [testCount, pageRequests]
+}
+
 describe('Test request blocking', () => {
     beforeEach(async () => {
         ({ browser, bgPage, teardown } = await harness.setup())
@@ -25,40 +58,13 @@ describe('Test request blocking', () => {
     it('Should block all the test tracking requests', async () => {
         // Load the test page.
         const page = await browser.newPage()
-        await pageWait.forGoto(page, testSite)
-
-        // Start logging network requests.
-        const pageRequests = []
-        await logPageRequests(
-            page,
-            pageRequests,
-            ({ url }) => url.hostname === 'bad.third-party.site'
-        )
-
-        // Initiate the test requests and wait until they have all completed.
-        // Note:
-        //  - Waiting for network idle here does not work for some reason.
-        //  - ServiceWorker + WebWorker initiated requests are not yet logged by
-        //    logPageRequests.
-        await page.click('#start')
-        const testCount = await page.evaluate(
-            // eslint-disable-next-line no-undef
-            () => tests.filter(({ id }) => !id.includes('worker')).length
-        )
-        while (pageRequests.length < testCount) {
-            await backgroundWait.forTimeout(bgPage, 100)
-        }
+        const [testCount, pageRequests] = await runRequestBlockingTest(page)
 
         // Verify that no logged requests were allowed.
         for (const { url, method, type, status } of pageRequests) {
             const description = `URL: ${url}, Method: ${method}, Type: ${type}`
             expect(status).withContext(description).toEqual('blocked')
         }
-
-        // Wait an additional second to ensure that the WebWorker +
-        // ServiceWorker initiated requests have finished. This can be removed
-        // once logPageRequests can intercept those requests.
-        await backgroundWait.forTimeout(bgPage, 1000)
 
         // Also check that the test page itself agrees that no requests were
         // allowed.
@@ -116,30 +122,15 @@ describe('Test request blocking', () => {
                 value: config
             })
         }, testHost)
-        await pageWait.forGoto(page, testSite)
+        const [, pageRequests] = await runRequestBlockingTest(page)
 
-        // Start logging network requests.
-        const pageRequests = []
-        await logPageRequests(
-            page,
-            pageRequests,
-            ({ url }) => url.hostname === 'bad.third-party.site'
-        )
-        await page.click('#start')
-        const testCount = await page.evaluate(
-            // eslint-disable-next-line no-undef
-            () => tests.filter(({ id }) => !id.includes('worker')).length
-        )
-        while (pageRequests.length < testCount) {
-            await backgroundWait.forTimeout(bgPage, 100)
-        }
         // Verify that no logged requests were allowed.
         for (const { url, method, type, status } of pageRequests) {
             const description = `URL: ${url}, Method: ${method}, Type: ${type}`
             expect(status).withContext(description).toEqual('blocked')
         }
 
-        // Also check that the test page itself agrees that no requests were
+        // Check that the test page itself agrees that no requests were
         // allowed.
         const pageResults = await page.evaluate(
             () => results.results // eslint-disable-line no-undef
