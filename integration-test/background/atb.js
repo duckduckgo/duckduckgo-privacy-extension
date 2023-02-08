126193ce55e75e19ffdc975d19dc4002da3758a2
Sam Macbeth <smacbeth@duckduckgo.com>
2023-02-08 08:37:18 +0100

    Test for correct post install pathname

--- integration-test/background/atb.js
+++ integration-test/background/atb.js
@@ -31,11 +31,11 @@ describe('install workflow', () => {
             // if it never does, jasmine timeout will kick in
             while (!postInstallOpened) {
                 const urls = await Promise.all(browser.targets().map(target => target.url()))
-                postInstallOpened = urls.some(url => url.includes('duckduckgo.com/extension-success'))
+                postInstallOpened = urls.find(url => url.includes('duckduckgo.com/extension-success'))
                 await backgroundWait.forTimeout(bgPage, 100)
             }
-
             expect(postInstallOpened).toBeTruthy()
+            expect(new URL(postInstallOpened).pathname).toBe('/extension-success')
         })
     })
 
