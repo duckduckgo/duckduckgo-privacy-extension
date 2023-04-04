8ac025df0a0a918d24fb9b65d39c891115370dea
Dave Vandyke <kzar@kzar.co.uk>
2023-04-04 10:52:59 +0100

    Port YouTube Click to Load integration tests to Playwright (#1853)
    Move the Click to Load YouTube integration tests over to
    Playwright.
    
    Note: Some of the tests are disabled until the remaining
          content-scope-scripts changes land.

--- integration-test/helpers/pageWait.js
+++ integration-test/helpers/pageWait.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer')
 
+/** Legacy Puppeteer helpers **/
+
 async function waitForNetworkIdleInternal (page) {
     try {
         await page.waitForNetworkIdle({ idleTime: 1000, timeout: 15000 })
@@ -54,8 +56,51 @@ async function forReload (page) {
     }
 }
 
+/** Playwright helpers **/
+
+/**
+ * Helper to wait for network idle in Playwright tests.
+ * @param {import('@playwright/test').Page} page
+ *   The Playwright page to watch requests for.
+ * @param {number} idleTime
+ *   How long with no network connections until finished, in milliseconds.
+ */
+function waitForNetworkIdle (page, idleTime = 500) {
+    return new Promise(resolve => {
+        let timeout = null
+        let requestCount = 0
+
+        const onRequestEvent = requestCountChange => {
+            requestCount += requestCountChange
+
+            if (requestCount <= 0) {
+                timeout = setTimeout(finished, idleTime)
+            } else if (timeout) {
+                clearTimeout(timeout)
+                timeout = null
+            }
+        }
+        const onRequestOpen = onRequestEvent.bind(null, 1)
+        const onRequestClose = onRequestEvent.bind(null, -1)
+
+        const finished = () => {
+            page.removeListener('request', onRequestOpen)
+            page.removeListener('requestfinished', onRequestClose)
+            page.removeListener('requestfailed', onRequestClose)
+            resolve()
+        }
+
+        page.addListener('request', onRequestOpen)
+        page.addListener('requestfinished', onRequestClose)
+        page.addListener('requestfailed', onRequestClose)
+
+        onRequestEvent(0)
+    })
+}
+
 module.exports = {
     forGoto,
     forReload,
-    forNetworkIdle
+    forNetworkIdle,
+    waitForNetworkIdle
 }
