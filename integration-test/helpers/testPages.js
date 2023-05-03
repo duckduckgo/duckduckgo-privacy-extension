972af81264b69db628950b5da8c0e08e4fec5f45
Sam Macbeth <smacbeth@duckduckgo.com>
2023-05-03 09:30:10 +0200

    Support redirects for test page routing

--- integration-test/helpers/testPages.js
+++ integration-test/helpers/testPages.js
@@ -6,6 +6,7 @@ const testPageHosts = new Set([
     'bad.third-party.site',
     'www.search-company.site',
     'convert.ad-company.site',
+    'www.ad-company.site',
     'www.publisher-company.site',
     'www.payment-company.site'
 ])
@@ -32,12 +33,10 @@ export function routeFromLocalhost (page, overrideHandler) {
         const requestData = new Request(`http://localhost:3000${url.pathname}${url.search}${url.hash}`, {
             method: route.request().method(),
             body: route.request().postDataBuffer(),
-            headers
+            headers,
+            redirect: 'manual'
         })
         const response = await fetch(requestData)
-        if (!response.ok) {
-            console.log(url.href, requestData.url, response.ok)
-        }
         const responseHeaders = {}
         for (const [key, value] of response.headers.entries()) {
             responseHeaders[key] = value
@@ -47,7 +46,6 @@ export function routeFromLocalhost (page, overrideHandler) {
             // so we have to route this request to the real server.
             return route.continue()
         }
-        await new Promise(resolve => setTimeout(resolve, 500))
         return route.fulfill({
             status: response.status,
             body: await response.text(),
