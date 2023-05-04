ed5fa3236564d43adfa3ce84a11e74b96e04d837
Sam Macbeth <smacbeth@duckduckgo.com>
2023-05-04 08:30:25 +0200

    Playwright config tweaks

--- playwright.config.js
+++ playwright.config.js
@@ -27,9 +27,9 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: process.env.CI ? 'dot' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
