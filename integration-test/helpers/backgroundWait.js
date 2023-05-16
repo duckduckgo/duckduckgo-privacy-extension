1c36d2ec9a3268807911e3a93afcec1825fded05
Sam Macbeth <sammacbeth@users.noreply.github.com>
2023-05-16 21:05:12 +0200

    Remove legacy integration tests (#1961)
    * Remove legacy integration tests
    
    * Remove legacy test helpers
    
    * Port canvas verification to Playwright
    
    * Ignore image data perf test
    
    * Port fingerprint randomization test

--- integration-test/helpers/backgroundWait.js
+++ integration-test/helpers/backgroundWait.js
@@ -1,23 +1,6 @@
-const puppeteer = require('puppeteer')
+import { errors } from '@playwright/test'
 
-// Puppeteer does not yet (as of 14) provide a waitForTimeout method for
-// WebWorkers.
-function manuallyWaitForTimeout (timeout) {
-    return new Promise(resolve => {
-        setTimeout(resolve, timeout)
-    })
-}
-
-function forTimeout (bgPage, timeout) {
-    if (bgPage.waitForTimeout) {
-        return bgPage.waitForTimeout(timeout)
-    }
-
-    return manuallyWaitForTimeout(timeout)
-}
-
-// Puppeteer does not yet (as of 14) provide a waitForFunction method for
-// WebWorkers. The evaluate method is available though.
+// Helpers that aid in waiting for the background page's state.
 function manuallyWaitForFunction (bgPage, func, { polling, timeout }, ...args) {
     return new Promise((resolve, reject) => {
         const startTime = Date.now()
@@ -33,7 +16,7 @@ function manuallyWaitForFunction (bgPage, func, { polling, timeout }, ...args) {
             } else {
                 if (Date.now() - startTime > timeout) {
                     reject(
-                        new puppeteer.errors.TimeoutError(
+                        new errors.TimeoutError(
                             'Manually waiting for function timed out: ' +
                             func.toString()
                         )
@@ -47,65 +30,47 @@ function manuallyWaitForFunction (bgPage, func, { polling, timeout }, ...args) {
     })
 }
 
-// Helpers that aid in waiting for the background page's state.
-// Notes:
-//   - Puppeteer's `waitForFunction` uses `requestAnimationFrame` by default,
-//     but that appears to be broken for extension background pages. Polling
-//     every 10ms works however.
-//   - Jasmine's timeout is 20 seconds, so go with a timeout of 15 seconds
-//     (rather than the default of 30 seconds) to improve the error output on
-//     timeout.
-function forFunction (bgPage, func, ...args) {
+/**
+ * @param {import('@playwright/test').Page | import('@playwright/test').Worker} bgPage
+ * @param {*} func
+ * @param  {...any} args
+ * @returns {Promise<any>}
+ */
+export function forFunction (bgPage, func, ...args) {
     if (bgPage.waitForFunction && bgPage.routeFromHAR) {
         // In Playwright, the waitForFunction signature differs from the puppeteer one
         return bgPage.waitForFunction(func, ...args)
     }
-    const waitForFunction = bgPage.waitForFunction
-        ? bgPage.waitForFunction.bind(bgPage)
-        : manuallyWaitForFunction.bind(null, bgPage)
+    const waitForFunction = manuallyWaitForFunction.bind(null, bgPage)
     return waitForFunction(func, { polling: 10, timeout: 15000 }, ...args)
 }
 
-async function forSetting (bgPage, key) {
-    try {
-        return await forFunction(
-            bgPage, pageKey => globalThis.dbg?.settings?.getSetting(pageKey), key
-        )
-    } catch (e) {
-        if (e instanceof puppeteer.errors.TimeoutError) {
-            throw new puppeteer.errors.TimeoutError(
-                'Timed out waiting for setting: ' + key
-            )
-        } else {
-            throw e
+export async function forSetting (bgPage, key) {
+    return await forFunction(
+        bgPage, pageKey => globalThis.dbg?.settings?.getSetting(pageKey), key
+    )
+}
+
+export async function forAllConfiguration (bgPage) {
+    await forFunction(bgPage, async () => {
+        if (!globalThis.dbg?.https?.isReady ||
+            !globalThis.dbg?.settings?.ready ||
+            !globalThis.dbg?.startup?.ready ||
+            !globalThis.dbg?.tds?.ready) {
+            return false
         }
-    }
+
+        await Promise.all([
+            globalThis.dbg.settings.ready(),
+            globalThis.dbg.startup.ready(),
+            globalThis.dbg.tds.ready()
+        ])
+
+        return true
+    })
 }
 
-async function forAllConfiguration (bgPage) {
-    try {
-        await forFunction(bgPage, async () => {
-            if (!globalThis.dbg?.https?.isReady ||
-                !globalThis.dbg?.settings?.ready ||
-                !globalThis.dbg?.startup?.ready ||
-                !globalThis.dbg?.tds?.ready) {
-                return false
-            }
-
-            await Promise.all([
-                globalThis.dbg.settings.ready(),
-                globalThis.dbg.startup.ready(),
-                globalThis.dbg.tds.ready()
-            ])
-
-            return true
-        })
-    } catch (e) {
-        throw new Error('Failed to load extension configuration.')
-    }
-}
-
-async function forExtensionLoaded (context) {
+export async function forExtensionLoaded (context) {
     return /** @type {Promise<string>} */(new Promise((resolve) => {
         const listenForPostinstall = (page) => {
             if (page.url().startsWith('https://duckduckgo.com/extension-success')) {
@@ -117,9 +82,7 @@ async function forExtensionLoaded (context) {
     }))
 }
 
-module.exports = {
-    forTimeout,
-    forFunction,
+export default {
     forSetting,
     forAllConfiguration,
     forExtensionLoaded
