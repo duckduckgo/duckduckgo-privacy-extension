6e027bc2d8e00211c227ee4b2c7d6b426def6618
Shane Osbourne <shane.osbourne8@gmail.com>
2023-05-27 14:27:19 +0100

    increase newtab stats to 24hr (#1953)
    * update new tab tracker stats to 24hr
    
    * PR feedback
    
    * add a check for clock adjustments
    
    * move debug API to devbuild.js
    
    ---------
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

--- shared/js/background/classes/tracker-stats.js
+++ shared/js/background/classes/tracker-stats.js
@@ -1,192 +1,247 @@
 import z from 'zod'
 
+const SEC = 1000
+const MIN = SEC * 60
+const HOUR = MIN * 60
+const DAY = HOUR * 24
+
 /**
- * A single place to collect information about the number of requests
- * we have blocked, grouped by the owner's display name.
+ * @typedef {Record<string, number>} PlainEntries
+ * @typedef {{key: string; count: number}[]} SortedOutput
  */
+
+/**
+ * @typedef Pack - a pack is a collection of entries that are packed into a single object
+ * @property {number} start
+ * @property {number} end
+ * @property {PlainEntries} entries
+ */
+
 export class TrackerStats {
-    maxAgeMs = 1000 * 60 * 60 // 1hr
     /**
-     * A time-based mechanism
-     * @type {Map<string, number[]>}
-     */
-    entries = new Map()
-    /**
-     * A running total for (hopefully) the lifespan of the extension being installed
+     * A running total for the lifespan of the extension being installed
      * @type {number}
      */
     totalCount = 0
+
     /**
-     * Given a key eg: 'displayName', store current timestamp +
-     * increment the 'since-install-time' value
-     *
+     * A collection of packs, each one representing an hour of data
+     * @type {Pack[]}
+     */
+    packs = []
+
+    /**
+     * @type {{entries: Map<string, number>, start: number, end: number}}
+     */
+    current = {
+        start: 0,
+        end: 0,
+        entries: new Map()
+    }
+
+    /**
      * @param {string} key
-     * @param {number} [timestamp] - optional timestamp
+     * @param {number} [now] optional timestamp to use as the current time
      */
-    increment (key, timestamp = Date.now()) {
-        // ensure we always have an array for this incoming key
-        const prev = this.entries.get(key) || []
-
-        // now add this entry to the end
-        prev.push(timestamp)
-
-        // re-store the array of timestamps
-        this.entries.set(key, prev)
-
-        // increment the total count
+    increment (key, now = Date.now()) {
         this.totalCount += 1
+
+        // if current is empty, start it
+        if (this.current.start === 0) {
+            this.current.start = now
+        } else {
+            // if 'now' was *less* than the previous start time
+            // it means the user could have changed their system clock
+            // so we just should clear any current values to be sure
+            // and re-set the start time
+            if (now < this.current.start) {
+                this.clearCurrent()
+                this.current.start = now
+            }
+        }
+
+        const count = this.current.entries.get(key) ?? 0
+        this.current.entries.set(key, count + 1)
     }
 
     /**
-     * Ensure we're not holding onto stale data.
-     *
-     * Note: entries are skipped at read-time anyway, but this ensures
-     * that we don't maintain a growing list of timestamp entries over time.
-     *
-     * This may be called by an alarm or at any other period
-     *
-     * @param {number} [now] - the timestamp to compare entries against
-     * @returns {this}
+     * Every hour, pack up the current entries and start a new pack
+     * @param {number} [now] optional timestamp to use as the current time
      */
-    evictExpired (now = Date.now()) {
-        for (const [key, timestamps] of this.entries) {
-            // note: there's an assumption here that this *won't* be a perf
-            // bottleneck. `.filter(...)` is not always ideal since it allocates a new array
-            // but in this instance we're not in a 'hot' path as pruning only happens
-            // in response to things like timed-alarm.
-            const filteredTimestamps = timestamps.filter(previousTimestamp => {
-                const delta = now - previousTimestamp
-                return delta <= this.maxAgeMs
+    pack (now = Date.now()) {
+        const delta = now - this.current.start
+        const currentIsUnsetOrOverAnHourOld = this.current.start === 0 || (delta) >= HOUR
+        const currentIsUnsetOrOverADayOld = this.current.start === 0 || (delta) >= DAY
+
+        // if current is over a day old, just clear it
+        if (currentIsUnsetOrOverADayOld) {
+            // clear current
+            this.clearCurrent()
+        } else if (currentIsUnsetOrOverAnHourOld && this.current.entries.size > 0) {
+            // when 'current' is over an hour old, pack it
+            /** @type {Pack} */
+            const next = {
+                start: this.current.start,
+                end: now,
+                entries: Object.fromEntries(this.current.entries)
+            }
+
+            this.packs.push(next)
+            this.clearCurrent()
+        } else {
+            // Here we do nothing because 'current' was neither dropped nor packed.
+            // This comments remains here to highlight this 3rd state, where we don't alter 'current'
+            // but instead we allow it to keep collecting data
+        }
+
+        // prune expired packs, only accepting the last 23 to account for clock drift/adjustments
+        this.packs = this.packs.slice(-23).filter(pack => {
+            const packIsYoungerThanADay = (now - pack.end) <= DAY
+            return packIsYoungerThanADay
+        })
+    }
+
+    /**
+     * @param {number} [now] - optional timestamp to use as the current time
+     * @return {PlainEntries}
+     */
+    data (now = Date.now()) {
+        /** @type {Record<string, number>} */
+        const output = {}
+
+        // aggregate all packs
+        for (const pack of this.packs) {
+            const packIsYoungerThanADay = (now - pack.end) < DAY
+            if (!packIsYoungerThanADay) continue
+            for (const [key, count] of Object.entries(pack.entries)) {
+                output[key] = (output[key] ?? 0) + count
+            }
+        }
+
+        // aggregate current, but only if it's younger than a day
+        const currentIsYoungerThanADay = (now - this.current.start) < DAY
+        if (currentIsYoungerThanADay) {
+            for (const [key, count] of this.current.entries) {
+                output[key] = (output[key] ?? 0) + count
+            }
+        }
+
+        return output
+    }
+
+    /**
+     * @param {number} [now] optional timestamp to use as the current time
+     * @returns {SortedOutput}
+     */
+    sorted (now = Date.now()) {
+        const data = this.data(now)
+
+        /** @type {SortedOutput} */
+        const keys = Object.keys(data)
+            .map(key => {
+                return { key, count: data[key] }
             })
+            .sort((a, b) => b.count - a.count)
 
-            // evict the entry entirely if there are no valid timestamps
-            if (filteredTimestamps.length === 0) {
-                this.entries.delete(key)
-            } else {
-                // otherwise just re-set this entry to have only valid ones.
-                this.entries.set(key, filteredTimestamps)
-            }
-        }
-
-        return this
+        return keys
     }
 
     /**
-     * Produce a simplified 'sorted' view of the underlying data.
-     *
-     * We store data in the following format:
-     *
-     * {
-     *     "Google": [1673438914778],
-     *     "Facebook: [1673438914778, 1673438914778, 1673438914792]
-     * }
-     *
-     * We have to keep track of those timestamps, but only internally. When a consumer
-     * wants access to our data, we collapse it down into the following format:
-     *
-     * {
-     *     results: { Facebook: 3, Google: 1 },
-     *     overflow: 5
-     * }
-     *
-     * Where 'overflow' represents the number of requests for companies
-     * outside the `maxCount`.
-     *
-     * @param {number} [maxCount] - how many companies to display
-     * @param {number} [now] - an optional timestamp, defaults to call-time
-     * @returns {{
-     *     results: { key: string, count: number }[],
-     *     overflow: number
-     * }}
-     */
-    sorted (maxCount = 10, now = Date.now()) {
-        /** @type {{key: string, count: number}[]} */
-        const stats = []
-
-        // For every entry, only count the ones within the last hour.
-        for (const [key, timestamps] of this.entries) {
-            let count = 0
-            for (const timestamp of timestamps) {
-                const delta = now - timestamp
-                if (delta <= this.maxAgeMs) {
-                    count += 1
-                }
-            }
-            if (count > 0) {
-                stats.push({ key, count })
-            }
-        }
-
-        // sort them, so that the highest count's are first
-        const sorted = stats.sort((a, b) => b.count - a.count)
-
-        // now it's sorted, we need to partition the array based on the maxCount
-        const results = sorted.slice(0, maxCount)
-
-        // everything else will make up the 'overflow' count that can be used to
-        // display a category like 'Other'
-        const overflowItems = sorted.slice(maxCount)
-        const overflow = overflowItems.reduce((sum, item) => sum + item.count, 0)
-
-        return {
-            results,
-            overflow
-        }
-    }
-
-    /**
-     * Allow our internal data to be 'set'.
-     *
-     * We are very strict about the incoming data here, mostly treating
-     * it as untrusted.
-     *
-     * It should be in this format:
-     *
-     * {
-     *     "totalCount": 12,
-     *     "entries": {
-     *         "Google": [1673438914778],
-     *         "Facebook": [1673438914778, 1673438914778, 1673438914792]
-     *     }
-     * }
-     *
-     * This is normally produced by `.serialize()` on this instance (see below)
-     *
      * @param {object} params
      * @param {number} params.totalCount
-     * @param {Record<string, number[]>} params.entries
-     * @throws
+     * @param {Pack} params.current
+     * @param {Pack[]} params.packs
      */
     deserialize (params) {
-        // define what shape incoming data must be in
-        const storageSchema = z.object({
-            totalCount: z.number(),
-            entries: z.record(z.string(), z.array(z.number()))
+        // this is the schema for a single pack. It's used for the current pack and for the packs array
+        const packSchema = z.object({
+            start: z.number().default(0),
+            end: z.number().default(0),
+            entries: z.record(z.string(), z.number())
         })
 
-        // try to validate the data
+        // this is the schema for the entire storage object
+        const storageSchema = z.object({
+            totalCount: z.number(),
+            current: packSchema.default({
+                start: 0,
+                end: 0,
+                entries: {}
+            }),
+            packs: z.array(packSchema).default([])
+        })
+
+        // try to validate the data, but without throwing errors
         const result = storageSchema.safeParse(params)
 
-        // if any errors occur, bail and don't use this data at all (start a fresh)
+        // if any errors occur, bail and don't use this data at all (essentially starting a fresh)
         if (!result.success) {
             console.warn('could not accept the incoming data because of schema errors', result.error.errors.length)
         } else {
-            // but if we get here, we can use the data internally, we 'trust' it.
+            // but if we get here, we can use the data internally, we 'trust' it at this point.
             this.totalCount = result.data.totalCount
-            this.entries = new Map(Object.entries(result.data.entries))
+            this.current = {
+                start: result.data.current.start,
+                end: result.data.current.end,
+                entries: new Map(Object.entries(result.data.current.entries))
+            }
+            this.packs = result.data.packs
         }
+
+        // Because 'deserialize' will happen every time the extension is loaded, we need to pack
+        // the restored data to prune expired packs and ensure this.totalCount is accurate
+        this.pack()
     }
 
     /**
-     * Produce a data format that can be stored and restored from.
+     * Produce a JSON-compatible object that can be stored in `chrome.storage.local`
+     * NewTabTrackerStats will call this when it's time to save the data
      *
-     * The data produced by this method will be stored and should be used
-     * in calls to 'deserialize' (above)
+     * @return {{current: Pack, totalCount: number, packs: Pack[], entries: null}}
      */
     serialize () {
-        return {
-            entries: Object.fromEntries(this.entries),
-            totalCount: this.totalCount
+        const output = {
+            totalCount: this.totalCount,
+            current: {
+                ...this.current,
+                entries: Object.fromEntries(this.current.entries)
+            },
+            packs: this.packs,
+            /**
+             * This was the key used in the first implementation. To avoid
+             * confusion with the different data types we are no longer using this key name.
+             * To ensure no lingering data, setting this to `null` deletes any unused data
+             */
+            entries: null
         }
+        return output
+    }
+
+    /**
+     * NewTabTrackerStats will call this via things like alarms. It's an opportunity
+     * to look at internal data and drop what we no-longer need. This prevents
+     * the data from growing too large whilst also ensuring we don't hang onto stale entries.
+     *
+     * @param {number} [now] optional timestamp to use as the current time
+     */
+    evictExpired (now = Date.now()) {
+        this.pack(now)
+    }
+
+    clearCurrent () {
+        this.current.entries.clear()
+        this.current.start = 0
+        this.current.end = 0
+    }
+
+    clear () {
+        this.clearCurrent()
+        this.packs = []
+    }
+
+    clearAll () {
+        this.clearCurrent()
+        this.packs = []
+        this.totalCount = 0
     }
 }
