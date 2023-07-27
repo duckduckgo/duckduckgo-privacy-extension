d3642dfcbbdaa8158af3a3e75d75d61053accc11
Sam Macbeth <smacbeth@duckduckgo.com>
2023-07-27 08:49:00 +0200

    Remove url-parse-lax dependency

--- packages/privacy-grade/src/utils.js
+++ packages/privacy-grade/src/utils.js
@@ -1,6 +1,5 @@
 const entityMap = require('../data/generated/entity-map')
 const tldts = require('tldts')
-const parseUrl = require('url-parse-lax')
 
 // pull off subdomains and look for parent companies
 const findParent = (url) => {
@@ -17,29 +16,13 @@ const findParent = (url) => {
 
 const getDomain = (url) => {
     if (!url) return ''
-
-    let domain = tldts.getDomain(url)
-
-    if (!domain) {
-        // handle e.g. underscore URLs which tldts normally chokes on
-        // note this is really slow and we don't want to do it unless necessary
-        const hostname = parseUrl(url).hostname || ''
-        const hostnameParts = hostname.split('.')
-
-        while (!domain && hostnameParts.length > 2) {
-            hostnameParts.shift()
-
-            domain = tldts.getDomain(hostnameParts.join('.'))
-        }
-    }
-
-    return domain || ''
+    return tldts.getDomain(url) || ''
 }
 
 const extractHostFromURL = (url) => {
     if (!url) return ''
 
-    let hostname = parseUrl(url).hostname || ''
+    let hostname = tldts.parse(url).hostname || ''
     hostname = hostname.replace(/^www\./, '')
 
     return hostname
