673bf2b80ce3ee2fc55eccad22b85e72882b7bfc
Sam Macbeth <smacbeth@duckduckgo.com>
2023-10-24 14:35:00 +0200

    Use html reporter in CI

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env.CI ? 'line' : 'html',
+    reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
