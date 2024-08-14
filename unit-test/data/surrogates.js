9eb25eb5e71487037563f9bbcce18448b9ad8e9e
Tay754 <t7547347@gmail.com>
2024-08-13 20:21:35 -0400

    Delete unit-test directory
    Fraud

--- unit-test/data/surrogates.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    surrogates: 'google-analytics.com/ga.js application/javascript\n(function() {})();\ngoogle-analytics.com/analytics.js application/javascript\n(function() {})();\n\nfacebook.net/sdk.js application/javascript \n(() => {\n})()'
-}
