d15687659c4a1ce086eacf8f84fe3840baa21442
Sam Macbeth <smacbeth@duckduckgo.com>
2025-01-03 17:34:12 +0100

    TDS Override via contentBlocking subfeature

--- shared/js/background/components/tds.js
+++ shared/js/background/components/tds.js
@@ -6,6 +6,9 @@ import constants from '../../../data/constants';
  * @typedef {import('./remote-config.js').default} RemoteConfig
  */
 
+const TDS_OVERRIDE_SETTINGS_KEY = 'tdsOverride';
+const CONTENT_BLOCKING = 'contentBlocking';
+
 export default class TDSStorage {
     /**
      * @param {{
@@ -14,6 +17,7 @@ export default class TDSStorage {
      * }} opts
      */
     constructor({ settings, remoteConfig }) {
+        this.settings = settings;
         this.remoteConfig = remoteConfig;
         /** @deprecated config is an alias of remoteConfig */
         this.config = this.remoteConfig;
@@ -28,14 +32,52 @@ export default class TDSStorage {
         this.tds = new ResourceLoader(
             {
                 name: 'tds',
-                remoteUrl: constants.tdsLists[1].url,
+                remoteUrl: this.getTDSUrl.bind(this),
                 updateIntervalMinutes: 15,
             },
             { settings },
         );
+        this.remoteConfig.onUpdate(this.checkShouldOverrideTDS.bind(this));
     }
 
     ready() {
         return Promise.all([this.tds.ready, this.surrogates.ready, this.remoteConfig.ready]);
+    }
+
+    async getTDSUrl() {
+        await this.config.ready;
+        const overridePath = this.settings.getSetting(TDS_OVERRIDE_SETTINGS_KEY);
+        if (overridePath) {
+            return `https://staticcdn.duckduckgo.com/trackerblocking/${overridePath}`;
+        }
+        return constants.tdsLists[1].url;
+    }
+
+    checkShouldOverrideTDS() {
+        const contentBlockingSubFeatures = this.config.config?.features[CONTENT_BLOCKING].features || {};
+        const enabledBlocklistOverrides = Object.keys(contentBlockingSubFeatures).filter(
+            (k) => k.startsWith('TDS') && this.config.isSubFeatureEnabled(CONTENT_BLOCKING, k),
+        );
+        if (enabledBlocklistOverrides.length > 0) {
+            const subFeatureName = enabledBlocklistOverrides[0]
+            const overrideSubFeature = contentBlockingSubFeatures[subFeatureName]
+            // If this is enabled via an experiment, the override URL is defined as `${cohortName}Url`, otherwise, for a normal rollout use `nextUrl`.
+            const settingsKey = `${this.remoteConfig.getCohortName(CONTENT_BLOCKING, subFeatureName) || 'next'}Url`
+            const overridePath = overrideSubFeature.settings && overrideSubFeature.settings[settingsKey]
+            if (!overridePath) {
+                console.warn(`Couldn't find TDS override path in subfeature settings.`)
+                return
+            }
+            if (overridePath !== this.settings.getSetting(TDS_OVERRIDE_SETTINGS_KEY)) {
+                console.log('TDS URL override changed to ', overridePath);
+                this.settings.updateSetting(TDS_OVERRIDE_SETTINGS_KEY, overridePath);
+                this.tds.checkForUpdates(true);
+            }
+            this.remoteConfig.markExperimentEnrolled(CONTENT_BLOCKING, subFeatureName)
+        } else if (this.settings.getSetting(TDS_OVERRIDE_SETTINGS_KEY)) {
+            // User removed from experiment/rollout, reset TDS override and fetch default list
+            this.settings.removeSetting(TDS_OVERRIDE_SETTINGS_KEY)
+            this.tds.checkForUpdates(true);
+        }
     }
 }
