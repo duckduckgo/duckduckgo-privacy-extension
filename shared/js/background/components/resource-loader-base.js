1c1a54e6413979e97c2969d74e1274e577bcd378
Maxim Tsoy <maks.tsoy@gmail.com>
2026-01-20 15:48:02 +0100

    Extract base resource loader logic

---
+++ shared/js/background/components/resource-loader-base.js
@@ -0,0 +1,201 @@
+import { alarms } from 'webextension-polyfill';
+import { createAlarm } from '../wrapper';
+
+/**
+ * @typedef {'tds' | 'surrogates' | 'config'} ResourceName
+ *
+ * @typedef {object} ResourceConfig
+ * @property {ResourceName} name
+ * @property {number} [updateIntervalMinutes]
+ * @property {'json'|'text'} [format]
+ *
+ * @typedef {(resourceName: ResourceName, etag: string, value: any) => Promise<any> | void} OnUpdatedCallback
+ * @typedef {import('../settings.js')} Settings
+ */
+
+const AFTER_UPDATE_EVENT_NAME = 'afterUpdate';
+
+/**
+ * Abstract base class for resource loading with common interface and refresh logic.
+ * Subclasses must implement `_fetchResource()` to provide the actual data fetching mechanism.
+ */
+export default class ResourceLoaderBase extends EventTarget {
+    /**
+     * @param {ResourceConfig} config
+     * @param {{
+     *  settings?: Settings
+     * }} opts
+     */
+    constructor(config, { settings } = {}) {
+        super();
+        this.settings = settings || null;
+        this.name = config.name;
+        this.updateIntervalMinutes = config.updateIntervalMinutes || 0;
+        this.format = config.format || 'json';
+        this.data = null;
+        this._onUpdateProcessing = [];
+        this._etag = '';
+        this._lastUpdate = 0;
+
+        /**
+         * @type {Promise<void>} Promise that resolves once this resource has been loaded
+         * (i.e. `this.data` contains the resource contents)
+         */
+        this.ready = this.checkForUpdates();
+        /**
+         * @type {Promise} Promise that resolves after resource is loaded, and all `onUpdate`
+         * callbacks have resolved.
+         */
+        this.allLoadingFinished = new Promise((resolve) => {
+            this.addEventListener(AFTER_UPDATE_EVENT_NAME, resolve);
+        });
+
+        if (this.updateIntervalMinutes) {
+            const alarmName = `updateResource: ${this.name}`;
+            createAlarm(alarmName, { periodInMinutes: this.updateIntervalMinutes });
+            alarms.onAlarm.addListener((alarmEvent) => {
+                if (alarmEvent.name === alarmName) {
+                    this.checkForUpdates();
+                }
+            });
+        }
+    }
+
+    get lastUpdate() {
+        if (this.settings) {
+            return this.settings.getSetting(`${this.name}-lastUpdate`) || 0;
+        }
+        return this._lastUpdate;
+    }
+
+    set lastUpdate(value) {
+        if (this.settings) {
+            this.settings.updateSetting(`${this.name}-lastUpdate`, value);
+        } else {
+            this._lastUpdate = value;
+        }
+    }
+
+    get etag() {
+        if (this.settings) {
+            return this.settings.getSetting(`${this.name}-etag`) || '';
+        }
+        return this._etag;
+    }
+
+    set etag(value) {
+        if (this.settings) {
+            this.settings.updateSetting(`${this.name}-etag`, value);
+        } else {
+            this._etag = value;
+        }
+    }
+
+    /**
+     * Check for updates and load the resource.
+     * Subclasses must implement this method.
+     * @param {boolean} [force] - Force update even if cache is fresh
+     * @returns {Promise<void>}
+     * @abstract
+     */
+    async checkForUpdates(force) {
+        throw new Error('checkForUpdates must be implemented by subclass');
+    }
+
+    /**
+     * Update the resource data and dispatch update events.
+     * @param {{
+     *  contents: any;
+     *  etag?: string;
+     * }} result
+     * @param {boolean} waitForUpdateProcessing
+     * @protected
+     */
+    async _updateData(result, waitForUpdateProcessing = false) {
+        console.log(`Loaded ${this.name}:`, result);
+        const updated = !!(this.data === null || this.etag);
+        this.data = result.contents;
+
+        if (result.etag) {
+            await this._persistData(result);
+            this.etag = result.etag;
+            this.lastUpdate = Date.now();
+        }
+        if (updated) {
+            this.dispatchEvent(
+                new CustomEvent('update', {
+                    detail: {
+                        name: this.name,
+                        etag: this.etag,
+                        data: this.data,
+                    },
+                }),
+            );
+            // After dispatchEvent, the return values of all onUpdate listeners will have been
+            // added to this._onUpdateProcessing, so we can wait for those promises to resolve.
+            const updateProcessing = Promise.all(this._onUpdateProcessing).then(() => {
+                this._onUpdateProcessing = [];
+                this.dispatchEvent(new Event(AFTER_UPDATE_EVENT_NAME));
+            });
+
+            // When configurations are being overridden, e.g. from the developer
+            // tools, it's useful to then wait for the update processing to have
+            // finished. That way, for MV3 builds the returned Promise will only
+            // resolve after the corresponding ruleset changes have been made.
+            if (waitForUpdateProcessing) {
+                await updateProcessing;
+            }
+        }
+    }
+
+    /**
+     * Persist data to storage. Override in subclasses that need persistence.
+     * @param {{contents: any, etag?: string}} result
+     * @protected
+     */
+    async _persistData(result) {
+        // Default: no persistence. Subclasses can override.
+    }
+
+    /**
+     * @param {OnUpdatedCallback} cb
+     */
+    onUpdate(cb) {
+        this.addEventListener('update', (ev) => {
+            if (ev instanceof CustomEvent) {
+                const { name, etag, data } = ev.detail;
+                const result = cb(name, etag, data);
+                if (result instanceof Promise) {
+                    this._onUpdateProcessing.push(result);
+                }
+            }
+        });
+    }
+
+    /**
+     * Manually set the value of this resource
+     * @param {any} data
+     */
+    async overrideDataValue(data) {
+        // create an etag hash based on the content
+        const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(JSON.stringify(data)));
+        const etag = [...new Uint8Array(hash)].map((x) => x.toString(16).padStart(2, '0')).join('');
+        await this._updateData(
+            {
+                contents: data,
+                etag,
+            },
+            true,
+        );
+    }
+
+    /**
+     * Modify resource data in-place
+     * @param {(data: any) => any} func
+     */
+    async modify(func) {
+        await this.ready;
+        const newValue = func(this.data);
+        await this.overrideDataValue(newValue);
+    }
+}
--- shared/js/background/components/resource-loader.js
+++ shared/js/background/components/resource-loader.js
@@ -1,89 +1,58 @@
 import Dexie from 'dexie';
-import { alarms } from 'webextension-polyfill';
 import bundledEtags from '../../../data/etags.json';
-import { createAlarm } from '../wrapper';
+import ResourceLoaderBase from './resource-loader-base.js';
+
 /**
- * @typedef {'tds' | 'surrogates' | 'config'} ResourceName
+ * @typedef {import('./resource-loader-base.js').ResourceName} ResourceName
+ * @typedef {import('./resource-loader-base.js').OnUpdatedCallback} OnUpdatedCallback
+ * @typedef {import('./resource-loader-base.js').Settings} Settings
  *
- * @typedef {object} ResourceConfig
+ * @typedef {object} NetworkResourceConfig
  * @property {ResourceName} name
  * @property {string | (() => Promise<string>)} [remoteUrl]
  * @property {string} [localUrl]
  * @property {number} [updateIntervalMinutes]
  * @property {'json'|'text'} [format]
- *
- * @typedef {(resourceName: ResourceName, etag: string, value: any) => Promise<any> | void} OnUpdatedCallback
- * @typedef {import('../settings.js')} Settings
  */
 
-const AFTER_UPDATE_EVENT_NAME = 'afterUpdate';
-
-export default class ResourceLoader extends EventTarget {
+/**
+ * Resource loader that fetches data from network URLs with IndexedDB caching.
+ */
+export default class ResourceLoader extends ResourceLoaderBase {
     /** @type {Dexie?} */
     static dbc = null;
 
     /**
-     * @param {ResourceConfig} config
+     * @param {NetworkResourceConfig} config
      * @param {{
      *  settings: Settings
      * }} opts
      */
     constructor(config, { settings }) {
-        super();
-        this.settings = settings;
-        this.name = config.name;
-        this.remoteUrl = config.remoteUrl;
-        this.localUrl = config.localUrl;
-        this.updateIntervalMinutes = config.updateIntervalMinutes || 0;
-        this.format = config.format || 'json';
-        this.data = null;
-        this._onUpdateProcessing = [];
-
-        if (!this.remoteUrl && !this.localUrl) {
+        if (!config.remoteUrl && !config.localUrl) {
             throw new Error('invalid config: need a remote or local URL (or both)');
         }
 
-        /**
-         * @type {Promise<void>} Promise that resolves once this resouce has been loaded
-         * (i.e. `this.data` contains the resource contents)
-         */
-        this.ready = this.checkForUpdates();
-        /**
-         * @type {Promise} Promise that resolves after resource is loaded, and all `onUpdate`
-         * callbacks have resolved.
-         */
-        this.allLoadingFinished = new Promise((resolve) => {
-            this.addEventListener(AFTER_UPDATE_EVENT_NAME, resolve);
-        });
+        super(
+            {
+                name: config.name,
+                updateIntervalMinutes: config.updateIntervalMinutes,
+                format: config.format,
+            },
+            { settings },
+        );
 
-        if (this.updateIntervalMinutes) {
-            const alarmName = `updateResource: ${this.name}`;
-            createAlarm(alarmName, { periodInMinutes: this.updateIntervalMinutes });
-            alarms.onAlarm.addListener((alarmEvent) => {
-                if (alarmEvent.name === alarmName) {
-                    this.checkForUpdates();
-                }
-            });
-        }
+        this.remoteUrl = config.remoteUrl;
+        this.localUrl = config.localUrl;
     }
 
-    get lastUpdate() {
-        return this.settings.getSetting(`${this.name}-lastUpdate`) || 0;
-    }
-
-    set lastUpdate(value) {
-        this.settings.updateSetting(`${this.name}-lastUpdate`, value);
-    }
-
-    get etag() {
-        return this.settings.getSetting(`${this.name}-etag`) || '';
-    }
-
-    set etag(value) {
-        this.settings.updateSetting(`${this.name}-etag`, value);
-    }
-
+    /**
+     * @param {boolean} [force]
+     */
     async checkForUpdates(force) {
+        if (!this.settings) {
+            throw new Error('ResourceLoader requires settings');
+        }
         await this.settings.ready();
         const remoteUrl = this.remoteUrl instanceof Function ? await this.remoteUrl() : this.remoteUrl;
         const loadFromDb = this._loadFromDB.bind(this);
@@ -157,89 +126,12 @@ export default class ResourceLoader extends EventTarget {
     }
 
     /**
-     * @param {{
-     *  contents: any;
-     *  etag?: string;
-     * }} result
-     * @param {boolean} waitForUpdateProcessing
+     * Persist data to IndexedDB
+     * @param {{contents: any, etag?: string}} result
+     * @protected
      */
-    async _updateData(result, waitForUpdateProcessing = false) {
-        console.log(`Loaded ${this.name}:`, result);
-        const updated = !!(this.data === null || this.etag);
-        this.data = result.contents;
-
-        if (result.etag) {
-            const dbc = await this._getDb();
-            await dbc.table('tdsStorage').put({ name: this.name, data: result.contents });
-            this.etag = result.etag;
-            this.lastUpdate = Date.now();
-        }
-        if (updated) {
-            this.dispatchEvent(
-                new CustomEvent('update', {
-                    detail: {
-                        name: this.name,
-                        etag: this.etag,
-                        data: this.data,
-                    },
-                }),
-            );
-            // After dispatchEvent, the return values of all onUpdate listeners will have been
-            // added to this._onUpdateProcessing, so we can wait for those promises to resolve.
-            const updateProcessing = Promise.all(this._onUpdateProcessing).then(() => {
-                this._onUpdateProcessing = [];
-                this.dispatchEvent(new Event(AFTER_UPDATE_EVENT_NAME));
-            });
-
-            // When configurations are being overridden, e.g. from the developer
-            // tools, it's useful to then wait for the update processing to have
-            // finished. That way, for MV3 builds the returned Promise will only
-            // resolve after the corresponding ruleset changes have been made.
-            if (waitForUpdateProcessing) {
-                await updateProcessing;
-            }
-        }
-    }
-
-    /**
-     * @param {OnUpdatedCallback} cb
-     */
-    onUpdate(cb) {
-        this.addEventListener('update', (ev) => {
-            if (ev instanceof CustomEvent) {
-                const { name, etag, data } = ev.detail;
-                const result = cb(name, etag, data);
-                if (result instanceof Promise) {
-                    this._onUpdateProcessing.push(result);
-                }
-            }
-        });
-    }
-
-    /**
-     * Manually set the value of this resource
-     * @param {any} data
-     */
-    async overrideDataValue(data) {
-        // create an etag hash based on the content
-        const hash = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(JSON.stringify(data)));
-        const etag = [...new Uint8Array(hash)].map((x) => x.toString(16).padStart(2, '0')).join('');
-        await this._updateData(
-            {
-                contents: data,
-                etag,
-            },
-            true,
-        );
-    }
-
-    /**
-     * Modify resource data in-place
-     * @param {(data: any) => any} func
-     */
-    async modify(func) {
-        await this.ready;
-        const newValue = func(this.data);
-        await this.overrideDataValue(newValue);
+    async _persistData(result) {
+        const dbc = await this._getDb();
+        await dbc.table('tdsStorage').put({ name: this.name, data: result.contents });
     }
 }
