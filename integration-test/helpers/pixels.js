771dd2791e65fd5904616251489473df3a69291d
Dave Vandyke <kzar@kzar.co.uk>
2026-02-17 11:56:05 +0000

    Refactor the Playwright request helpers (#3367)
    The Playwright integration tests have some helpers for observing the requests
    made by the test pages. They are used for things like checking tracking requests
    are blocked correctly.
    
    The (work in progress) Firefox Playwright harness will intercept requests in a
    different way, needing access to the extension's background context, so let's
    start passing the background context in ready. Let's also re-use the
    logPageRequests helper where possible, rather than duplicating the request logging
    logic. That way, when we add the Firefox harness, which will need requests to be
    handled differently, we can just update the logPageRequests abstraction.
    
    While making these changes several improvements were necessary:
    - Fixed how redirected requests are recorded, that wasn't working correctly.
    - There's a Playwright/Chrome issue whereby requests that are redirected and
      then blocked, are sometimes just reported as blocked. So let's add a
      workaround for that.
    - A couple of the requests on the request blocking test page aren't reported
      reliably, so let's avoid failing when that happens (whilst still checking all
      the known requests were handled correctly.)
    - Let's improve the output when the (usually reliable) requests for the request
      blocking test page really _have_ timed out.
    - Ensure the tests stop listening for request events once they are done.
    - Add missing test host domains, which help the tests run more reliably, by
      ensuring requests to those domains are routed properly.
    
    Some of the individual tests needed to be improved as well:
    - Get the click attribution tests passing more reliably by ensuring unrelated
      pixel requests aren't counted, and by waiting for the extension to finish
      loading before the tests run.
    - Get the Facebook Click to Load tests passing more reliably by ensuring that
      we wait for the blocked iframe request events, before moving on to the test
      that requests are allowed when the Load buttons are clicked. Otherwise, the
      blocked events sometimes fire late, causing the later tests to fail.

--- integration-test/helpers/pixels.js
+++ integration-test/helpers/pixels.js
@@ -9,14 +9,14 @@ function formatPixelRequest(request) {
     return _formatPixelRequestForTesting(request.url);
 }
 
-export function logPixels(page, pixelRequests, filter) {
-    return logPageRequests(page, pixelRequests, requestIsPixel, formatPixelRequest, filter);
+export function logPixels(backgroundPage, page, pixelRequests, filter) {
+    return logPageRequests(backgroundPage, page, pixelRequests, requestIsPixel, formatPixelRequest, filter);
 }
 
-export function listenForBreakageReport(backgroundNetworkContext) {
+export function listenForBreakageReport(backgroundPage, backgroundNetworkContext) {
     // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve) => {
-        const cleanup = await logPixels(backgroundNetworkContext, [], (pixel) => {
+        const cleanup = await logPixels(backgroundPage, backgroundNetworkContext, [], (pixel) => {
             if (pixel.name.startsWith('epbf_')) {
                 resolve(pixel);
                 cleanup();
