717517d8c1d897bd3cd9578729e2d130e3b7bce0
Maxim Tsoy <mtsoy@duckduckgo.com>
2026-02-24 16:57:06 +0100

    Embedded release automation (#3413)
    * add optional parameter to the version bump script
    
    * Add a workflow to update embedded extension in the apple repo
    
    * Add a comment about PR parsing
    
    * ignore autoconsent in dependabot (we bump it manually from another workflow)

--- scripts/bumpVersion.js
+++ scripts/bumpVersion.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
+const allBrowsers = ['chrome', 'firefox', 'chrome-mv2', 'embedded'];
 const newVersion = process.argv[2];
+const browserFilter = process.argv[3];
 
-for (const browser of ['chrome', 'firefox', 'chrome-mv2']) {
+const browsers = browserFilter ? [browserFilter] : allBrowsers;
+
+for (const browser of browsers) {
     const manifest = fs.readFileSync(`browsers/${browser}/manifest.json`, 'utf8');
     const manifestData = JSON.parse(manifest);
     manifestData.version = newVersion;
