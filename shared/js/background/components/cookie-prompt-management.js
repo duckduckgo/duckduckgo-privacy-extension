f83a875e9266ae27c08d649c8ea99ed064f8f05c
Maxim Tsoy <maks.tsoy@gmail.com>
2026-03-20 14:01:32 +0100

    Clear scheduled alarms manually to wrokaround WebKit bug

--- shared/js/background/components/cookie-prompt-management.js
+++ shared/js/background/components/cookie-prompt-management.js
@@ -122,10 +122,12 @@ export default class CookiePromptManagement {
         this.getCpmState();
 
         // Set up alarm listener for summary pixel
-        browser.alarms.onAlarm.addListener((alarm) => {
+        browser.alarms.onAlarm.addListener(async (alarm) => {
             if (alarm.name === CookiePromptManagement.SUMMARY_ALARM_NAME) {
+                // WebKit has a bug where the alarm is not cleared automatically, so we have to clear it ourselves
+                browser.alarms.clear(CookiePromptManagement.SUMMARY_ALARM_NAME);
+                await this.sendSummaryPixel();
                 this.cpmMessaging.logMessage(`alarm triggered: ${JSON.stringify(alarm)}`);
-                this.sendSummaryPixel();
             }
         });
 
